acb9baa66d1c0a517d283669583da61e05fb5412
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-08-04 10:52:20 +0200

    translation: Setup mlir module.

--- lib/vast/Translation/FromSource.cpp
+++ lib/vast/Translation/FromSource.cpp
@@ -1,12 +1,6 @@
 // Copyright (c) 2021-present, Trail of Bits, Inc.
 
-#include "clang/Frontend/FrontendAction.h"
-#include <clang/AST/ASTConsumer.h>
-#include <clang/AST/DeclBase.h>
-#include <clang/Tooling/Tooling.h>
-#include <cstdio>
-#include <cstdlib>
-#include <fstream>
+#include <mlir/IR/Location.h>
 #include <mlir/IR/MLIRContext.h>
 #include <mlir/IR/BuiltinOps.h>
 #include <mlir/Translation.h>
@@ -15,36 +9,60 @@
 #include <clang/AST/RecursiveASTVisitor.h>
 #include <clang/Frontend/CompilerInstance.h>
 #include <clang/Frontend/ASTConsumers.h>
-#include <clang/Frontend/FrontendActions.h>
+#include "clang/Frontend/FrontendAction.h"
+#include <clang/AST/ASTConsumer.h>
+#include <clang/AST/DeclBase.h>
+#include <clang/Tooling/Tooling.h>
 
 #include <llvm/Support/SourceMgr.h>
 #include <llvm/Support/MemoryBuffer.h>
+
+#include <vast/Dialect/VastDialect.hpp>
 
 #include <iostream>
 #include <filesystem>
 #include <memory>
 #include <stdexcept>
 #include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
 
 namespace vast
 {
 
     struct ASTVisitor : clang::RecursiveASTVisitor<ASTVisitor>
     {
+        ASTVisitor(mlir::ModuleOp module)
+            : module(module)
+        {}
+
         virtual ~ASTVisitor() = default;
 
         virtual bool VisitDecl(clang::Decl *decl)
         {
             return decl->dump(), true;
         }
+
+        mlir::ModuleOp module;
     };
 
     static mlir::OwningModuleRef from_source_parser(const llvm::MemoryBuffer *input, mlir::MLIRContext *ctx)
     {
-        ASTVisitor visitor;
+        ctx->loadDialect< VastDialect >();
+
+        mlir::OwningModuleRef module(
+            mlir::ModuleOp::create(
+                mlir::FileLineColLoc::get(input->getBufferIdentifier(), /* line */ 0, /* column */ 0, ctx)
+            )
+        );
+
+        ASTVisitor visitor(*module);
         auto ast = clang::tooling::buildASTFromCode(input->getBuffer());
         visitor.TraverseDecl(ast->getASTContext().getTranslationUnitDecl());
-        return {};
+
+        // TODO(Heno): verify module
+        return module;
     }
 
     mlir::LogicalResult registerFromSourceParser()
