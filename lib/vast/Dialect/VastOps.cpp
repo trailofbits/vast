ac5814621ee09757464094395d7219755dc07c6e
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-08-05 11:14:32 +0200

    vast: Generate simple function op.

--- lib/vast/Dialect/VastOps.cpp
+++ lib/vast/Dialect/VastOps.cpp
@@ -1,8 +1,27 @@
 // Copyright (c) 2021-present, Trail of Bits, Inc.
 
 #include "vast/Dialect/VastOps.hpp"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/OperationSupport.h"
+#include "mlir/IR/SymbolTable.h"
 #include "vast/Dialect/VastDialect.hpp"
 #include "mlir/IR/OpImplementation.h"
+
+namespace vast
+{
+    using builder = mlir::OpBuilder;
+
+    void VastFuncOp::build(builder &bld, mlir::OperationState &st, llvm::StringRef name)
+    {
+        st.addRegion();
+        st.addAttribute(mlir::SymbolTable::getSymbolAttrName(), bld.getStringAttr(name));
+    }
+
+} // namespace vast
+
+//===----------------------------------------------------------------------===//
+// TableGen generated logic.
+//===----------------------------------------------------------------------===//
 
 #define GET_OP_CLASSES
 #include "vast/Dialect/VastOps.cpp.inc"
