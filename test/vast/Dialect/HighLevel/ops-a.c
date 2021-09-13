1468823016bf28def80a742627056ff875d1ed82
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-09-13 13:18:51 +0200

    test: Add addition test.

---
+++ test/vast/Dialect/HighLevel/ops-a.c
@@ -0,0 +1,36 @@
+// RUN: vast-cc --from-source %s | FileCheck %s
+
+int add1(int a, int b)
+{
+    // CHECK: [[V1:%[0-9]+]] = hl.declref( @a ): !hl.int
+    // CHECK: [[V2:%[0-9]+]] = hl.implicit_cast( [[V1]] ): !hl.int
+    // CHECK: [[V3:%[0-9]+]] = hl.declref( @b ): !hl.int
+    // CHECK: [[V4:%[0-9]+]] = hl.implicit_cast( [[V3]] ): !hl.int
+    // CHECK: hl.addi [[V4]], [[V2]] : !hl.int
+    return a + b;
+}
+
+int add2(int a, int b)
+{
+    // CHECK: [[V1:%[0-9]+]] = hl.declref( @a ): !hl.int
+    // CHECK: [[V2:%[0-9]+]] = hl.implicit_cast( [[V1]] ): !hl.int
+    // CHECK: [[V3:%[0-9]+]] = hl.declref( @b ): !hl.int
+    // CHECK: [[V4:%[0-9]+]] = hl.implicit_cast( [[V3]] ): !hl.int
+    // CHECK: [[V5:%[0-9]+]] = hl.addi [[V4]], [[V2]] : !hl.int
+    // CHECK: [[V6:%[0-9]+]] = hl.var( r, [[V5]] ): !hl.int
+    int r = a + b;
+
+    // CHECK: [[V7:%[0-9]+]] = hl.declref( @r ): !hl.int
+    // CHECK: [[V8:%[0-9]+]] = hl.implicit_cast( [[V7]] ): !hl.int
+    // CHECK: return [[V8]] : !hl.int
+    return r;
+}
+
+void add3()
+{
+    // CHECK: [[V1:%[0-9]+]]  = hl.constant( 1 : i32 ): !hl.int
+    // CHECK: [[V2:%[0-9]+]] = hl.constant( 2 : i32 ): !hl.int
+    // CHECK: [[V3:%[0-9]+]] = hl.addi %1, %0 : !hl.int
+    // CHECK: [[V4:%[0-9]+]] = hl.var( v, %2 ): !hl.int
+    int v = 1 + 2;
+}
