a568ee24ff716723120ea4c49ef7dc1f56d49ffb
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-09-15 15:49:03 +0200

    test: Add scope generation tests.

---
+++ test/vast/Dialect/HighLevel/scope-a.c
@@ -0,0 +1,69 @@
+// RUN: vast-cc --from-source %s | FileCheck %s
+
+// CHECK-LABEL: func private @test1() -> !hl.int
+int test1()
+{
+    // CHECK: hl.scope
+    // CHECK: hl.scope-end
+    {
+        int a = 0;
+    }
+
+    int a = 0;
+    // CHECK: return [[C1:%[0-9]+]] : !hl.int
+    return a;
+}
+
+// CHECK-LABEL: func private @test2() -> !hl.void
+void test2()
+{
+    // CHECK: hl.scope
+    // CHECK: hl.var( a ): !hl.int
+    // CHECK: hl.scope-end
+    {
+        int a;
+    }
+
+    // CHECK: hl.scope
+    // CHECK: hl.var( a ): !hl.int
+    // CHECK: hl.scope-end
+    {
+        int a;
+    }
+
+    // CHECK-NOT: hl.scope
+    // CHECK: hl.var( a ): !hl.int
+    {
+        int a;
+    }
+    // CHECK: return [[C2:%[0-9]+]] : !hl.void
+}
+
+// CHECK-LABEL: func private @test3() -> !hl.int
+int test3()
+{
+    // CHECK: hl.var( b ): !hl.int
+    int b;
+
+    // CHECK: hl.scope
+    // CHECK: hl.scope-end
+    {
+        // CHECK: hl.var( a ): !hl.int
+        int a;
+    }
+
+    // CHECK-NOT: hl.scope
+    int a;
+    // CHECK: return [[C3:%[0-9]+]] : !hl.int
+    return 0;
+}
+
+// CHECK-LABEL: func private @test4() -> !hl.int
+int test4()
+{
+    // CHECK-NOT: hl.scope
+    {
+        int a = 0;
+        return a;
+    }
+}
