748ebb1f3ff4fda16ac8aea9ecb4620ccec42822
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-09-20 14:59:43 +0200

    hl: Add teminator helpers.

--- lib/vast/Dialect/HighLevel/HighLevelOps.cpp
+++ lib/vast/Dialect/HighLevel/HighLevelOps.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2021-present, Trail of Bits, Inc.
 
+#include "mlir/Support/LLVM.h"
 #include "mlir/Support/LogicalResult.h"
 #include "vast/Dialect/HighLevel/HighLevel.hpp"
 
@@ -7,11 +8,26 @@
 #include "mlir/IR/OperationSupport.h"
 #include "mlir/IR/SymbolTable.h"
 #include "mlir/IR/OpImplementation.h"
+#include "llvm/Support/ErrorHandling.h"
 
 namespace vast::hl
 {
-    using builder = mlir::OpBuilder;
-    using logical_result = mlir::LogicalResult;
+    void terminate_body(Builder &bld, Location loc)
+    {
+        bld.create< ScopeEndOp >(loc);
+    }
+
+    void ensure_terminator(Region *region, Builder &bld, Location loc)
+    {
+        if (region->empty())
+            bld.createBlock(region);
+
+        auto &block = region->back();
+        if (!block.empty() && block.back().isKnownTerminator())
+            return;
+        bld.setInsertionPoint(&block, block.end());
+        bld.create< ScopeEndOp >(loc);
+    }
 }
 
 //===----------------------------------------------------------------------===//
