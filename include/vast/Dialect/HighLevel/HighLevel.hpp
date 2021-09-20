c2045886975f27db552c223b0d63e8b39be1af0b
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-09-20 14:59:43 +0200

    hl: Add teminator helpers.

--- include/vast/Dialect/HighLevel/HighLevel.hpp
+++ include/vast/Dialect/HighLevel/HighLevel.hpp
@@ -5,6 +5,7 @@
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
+#include "mlir/IR/OperationSupport.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 
 #include "vast/Dialect/HighLevel/HighLevelTypes.hpp"
@@ -14,6 +15,16 @@ namespace vast::hl
 {
     // fixes missing namespace in the tablegen code
     using Value = mlir::Value;
+
+    using Region    = mlir::Region;
+    using Builder   = mlir::OpBuilder;
+    using Location  = mlir::Location;
+    using State     = mlir::OperationState;
+    using TypeRange = mlir::TypeRange;
+
+    using BuilderCallback = llvm::function_ref< void(Builder &, Location) >;
+
+    void terminate_body(Builder &bld, Location loc);
 
 } // namespace vast::hl
 
