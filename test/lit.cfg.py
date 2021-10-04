7adb766ce24c25b662512f5279b5c608daa5a6b6
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-10-04 10:33:59 +0200

    test: Fix lit paths.

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -21,7 +21,7 @@ config.name = 'VAST'
 config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
 
 # suffixes: A list of file extensions to treat as test files.
-config.suffixes = ['.mlir', '.c', '.cpp']
+config.suffixes = ['.mlir', '.c', '.cpp', '.ll']
 
 # test_source_root: The root path where tests are located.
 config.test_source_root = os.path.dirname(__file__)
@@ -35,8 +35,6 @@ config.substitutions.append(('%shlibext', config.llvm_shlib_ext))
 llvm_config.with_system_environment(
     ['HOME', 'INCLUDE', 'LIB', 'TMP', 'TEMP'])
 
-llvm_config.use_default_substitutions()
-
 # excludes: A list of directories to exclude from the testsuite. The 'Inputs'
 # subdirectories contain auxiliary inputs for various tests in their parent
 # directories.
@@ -49,13 +47,9 @@ config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = os.path.join(config.vast_obj_root, 'test')
 config.vast_tools_dir = os.path.join(config.vast_obj_root, 'bin')
 
-# Tweak the PATH to include the tools dir.
-llvm_config.with_environment('PATH', config.llvm_tools_dir, append_path=True)
-
-tool_dirs = [config.vast_tools_dir, config.llvm_tools_dir]
 tools = [
     'vast-opt',
     'vast-cc'
 ]
 
-llvm_config.add_tool_substitutions(tools, tool_dirs)
+llvm_config.add_tool_substitutions(tools, config.vast_tools_dir)
