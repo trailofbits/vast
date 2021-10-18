fbad3beb3d9952f0d8090a119fbddde2a95a2845
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-10-18 17:56:18 +0200

    test: Update while tests.

--- test/vast/Dialect/HighLevel/while-a.c
+++ test/vast/Dialect/HighLevel/while-a.c
@@ -3,45 +3,45 @@
 // CHECK-LABEL: func private @while_empty
 void while_empty()
 {
+    // CHECK: hl.while
     // CHECK: [[V1:%[0-9]+]] = hl.constant( true )
-    // CHECK-NEXT: hl.while [[V1]]
-    while (true)
-    {
-        // CHECK: hl.scope.end
+    // CHECK: hl.cond.yield [[V1]]
+    while (true) {
+        // CHECK: } do {
     }
-    // CHECK: hl.scope.end
 }
 
 // CHECK-LABEL: func private @while_simple
 void while_simple(int a)
 {
+    // CHECK: hl.while
     // CHECK: [[V1:%[0-9]+]] = hl.cmp sgt
-    // CHECK-NEXT: hl.while [[V1]]
+    // CHECK: hl.cond.yield [[V1]]
     while (a > 0)
     {
+        // CHECK: } do {
         a = a - 1;
         // CHECK: [[V2:%[0-9]+]] = hl.sub
         // CHECK: hl.assign [[V2]]
-        // CHECK: hl.scope.end
     }
-    // CHECK: hl.scope.end
 }
 
 // CHECK-LABEL: func private @while_nested
 void while_nested(int a, int b)
 {
+    // CHECK: hl.while
     // CHECK: [[V1:%[0-9]+]] = hl.cmp sgt
-    // CHECK-NEXT: hl.while [[V1]]
+    // CHECK: hl.cond.yield [[V1]]
     while (a > 0) {
+        // CHECK: } do {
         int c = b;
+        // CHECK: hl.while
         // CHECK: [[V2:%[0-9]+]] = hl.cmp sgt
-        // CHECK-NEXT: hl.while [[V2]]
+        // CHECK: hl.cond.yield [[V2]]
         while (c > 0) {
+            // CHECK: } do {
             c = c - 1;
-            // CHECK: hl.scope.end
         }
         a = a - 1;
-        // CHECK: hl.scope.end
     }
-    // CHECK: hl.scope.end
 }
