55ec02f6f019dde70a8b64b0c5cfa4848f69430e
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-10-27 14:29:59 +0200

    test: Add test on void return value.

--- test/vast/Dialect/HighLevel/return-a.c
+++ test/vast/Dialect/HighLevel/return-a.c
@@ -1,6 +1,10 @@
 // RUN: vast-cc --from-source %s | FileCheck %s
 
-int a() { return 7; }
 // CHECK-LABEL: func private @a() -> !hl.int
-// CHECK: hl.constant 7 : !hl.int
-// CHECK: return %0 : !hl.int
+// CHECK: [[V1:%[0-9]+]] = hl.constant 7 : !hl.int
+// CHECK: hl.return [[V1]] : !hl.int
+int a() { return 7; }
+
+// CHECK-LABEL: func private @b() -> !hl.void
+// CHECK: hl.return
+void b() { return; }
