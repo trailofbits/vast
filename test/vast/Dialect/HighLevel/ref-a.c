32d1cb608fd8faffd0d4519cc6f1997df9251909
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-10-27 13:18:35 +0200

    test: Update expected assembly format of high level tests.

--- test/vast/Dialect/HighLevel/ref-a.c
+++ test/vast/Dialect/HighLevel/ref-a.c
@@ -5,7 +5,7 @@ int main()
     // CHECK: [[V1:%[0-9]+]] = hl.constant( 0 : i32 ): !hl.int
     // CHECK: hl.var( x, [[V1]] ): !hl.int
     int x = 0;
-    // CHECK: [[V2:%[0-9]+]] = hl.declref( @x ): !hl.int
+    // CHECK: [[V2:%[0-9]+]] = hl.declref @x : !hl.int
     // CHECK: [[V3:%[0-9]+]] = hl.implicit_cast( [[V2]] ) LValueToRValue: !hl.int
     // CHECK: hl.var( y, [[V3]] ): !hl.int
     int y = x;
