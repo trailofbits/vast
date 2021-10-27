9f304d59ed7cb4234e2da9c413f35b958726a0a4
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-10-27 14:54:37 +0200

    test: Update expected format of hl casts.

--- test/vast/Dialect/HighLevel/cast-a.c
+++ test/vast/Dialect/HighLevel/cast-a.c
@@ -5,9 +5,9 @@ void cast_cstyle()
 {
     int i = 0;
     short s = (short)i;
-    // CHECK: [[V1:%[0-9]+]] = hl.declref @i
-    // CHECK: [[V2:%[0-9]+]] = hl.implicit_cast( [[V1]] ) LValueToRValue: !hl.int
-    // CHECK: [[V3:%[0-9]+]] = hl.implicit_cast( [[V2]] ) IntegralCast: !hl.short
-    // CHECK: [[V4:%[0-9]+]] = hl.cstyle_cast( [[V3]] ) NoOp: !hl.short
+    // CHECK: [[V1:%[0-9]+]] = hl.declref @i : !hl.int
+    // CHECK: [[V2:%[0-9]+]] = hl.implicit_cast [[V1]] LValueToRValue : !hl.int -> !hl.int
+    // CHECK: [[V3:%[0-9]+]] = hl.implicit_cast [[V2]] IntegralCast : !hl.int -> !hl.short
+    // CHECK: [[V4:%[0-9]+]] = hl.cstyle_cast [[V3]] NoOp : !hl.short -> !hl.short
     // CHECK: hl.var( s, [[V4]] ): !hl.short
 }
