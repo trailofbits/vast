04e689f01624f04c2f275bb514f7b187d28610a7
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-11-05 12:23:50 +0100

    hl: Implement high level types parsers.

--- lib/vast/Dialect/HighLevel/HighLevelTypes.cpp
+++ lib/vast/Dialect/HighLevel/HighLevelTypes.cpp
@@ -2,22 +2,8 @@
 
 #include "vast/Dialect/HighLevel/HighLevel.hpp"
 
-#include <mlir/IR/DialectImplementation.h>
-
-#include <llvm/ADT/TypeSwitch.h>
-#include <llvm/ADT/StringRef.h>
 
 namespace vast::hl
 {
-    std::string to_string(integer_kind kind) noexcept
-    {
-        switch (kind) {
-            case integer_kind::Char:     return "char";
-            case integer_kind::Short:    return "short";
-            case integer_kind::Int:      return "int";
-            case integer_kind::Long:     return "long";
-            case integer_kind::LongLong: return "longlong";
-        }
-    }
 
 } // namespace vast::hl
