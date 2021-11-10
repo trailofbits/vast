93b10399e90fe40f96968b4b20e8f5c68cc58ce3
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-11-10 14:35:35 +0100

    test: Add simple direct call tests.

---
+++ test/vast/Dialect/HighLevel/calls-a.c
@@ -0,0 +1,31 @@
+// RUN: vast-cc --from-source %s | FileCheck %s
+// RUN: vast-cc --from-source %s > %t && vast-opt %t | diff -B %t -
+
+int constant() { return 7; }
+
+
+void noop() {}
+
+
+int add(int a, int b) { return a + b; }
+
+
+int forward_decl(int a);
+
+
+int main()
+{
+    // CHECK: hl.call @constant() : () -> !hl.int
+    int c = constant();
+
+    // CHECK: hl.call @noop() : () -> !hl.void
+    noop();
+
+    // CHECK: hl.call @add([[V1:%[0-9]+]], [[V2:%[0-9]+]]) : (!hl.int, !hl.int) -> !hl.int
+    int v = add(1, 2);
+
+    // CHECK: hl.call @forward_decl([[V3:%[0-9]+]])
+    forward_decl(7);
+}
+
+int forward_decl(int a) { return a; }
