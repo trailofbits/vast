7fd5f062da06037bcb08c918041749b647af8782
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-11-24 13:48:22 +0100

    test: Add indirect call test.

---
+++ test/vast/Dialect/HighLevel/calls-c.c
@@ -0,0 +1,29 @@
+// RUN: vast-cc --from-source %s | FileCheck %s
+// RUN: vast-cc --from-source %s > %t && vast-opt %t | diff -B %t -
+
+// CHECK: hl.typedef @operation : !hl.ptr<(!hl.int, !hl.int) -> !hl.int>
+typedef int ( *operation ) ( int, int );
+
+int apply( operation op, int a, int b )
+{
+    // CHECK: [[OP:%[0-9]+]] = hl.declref @op : !hl.ptr<(!hl.int, !hl.int) -> !hl.int>
+    // CHECK: [[FN:%[0-9]+]] = hl.implicit_cast [[OP]] LValueToRValue
+    // CHECK: [[R:%[0-9]+]] = hl.indirect_call [[FN]]([[A:%[0-9]+]], [[B:%[0-9]+]])
+    return op( a, b );
+}
+
+int add( int a, int b ) { return a + b; }
+int mul( int a, int b ) { return a * b; }
+
+int main()
+{
+    // CHECK: [[F1:%[0-9]+]] = hl.declref @add
+    // CHECK: [[P1:%[0-9]+]] = hl.addressof [[F1]]
+    // CHECK: hl.call @apply([[P1]], [[A1:%[0-9]+]], [[A2:%[0-9]+]])
+    apply( &add, 1, 2 );
+
+    // CHECK: [[F2:%[0-9]+]] = hl.declref @mul
+    // CHECK: [[P2:%[0-9]+]] = hl.addressof [[F2]]
+    // CHECK: hl.call @apply([[P2]], [[A1:%[0-9]+]], [[A2:%[0-9]+]])
+    apply( &mul, 3, 4 );
+}
