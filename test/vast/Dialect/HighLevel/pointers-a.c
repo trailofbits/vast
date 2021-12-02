68c8f0ca9f1b9484fd3bb25a677d307f7221a983
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-12-02 22:00:23 +0100

    test: Update pointer types.

--- test/vast/Dialect/HighLevel/pointers-a.c
+++ test/vast/Dialect/HighLevel/pointers-a.c
@@ -1,47 +1,47 @@
 // RUN: vast-cc --from-source %s | FileCheck %s
 // RUN: vast-cc --from-source %s > %t && vast-opt %t | diff -B %t -
 
-// CHECK: hl.ptr<void>
+// CHECK: !hl.ptr<!hl.void>
 void * vp = 0;
 
-// CHECK: hl.ptr<ptr<void>>
+// CHECK: !hl.ptr<!hl.ptr<!hl.void>>
 void ** vpp = 0;
 
-// CHECK: hl.ptr<int>
+// CHECK: !hl.ptr<!hl.int>
 int * ip = 0;
 
-// CHECK: hl.ptr<int<const>>
+// CHECK: !hl.ptr<!hl.int<const>>
 int const * icp = 0;
 
-// CHECK: hl.ptr<int<const>>
+// CHECK: !hl.ptr<!hl.int<const>>
 const int * cip = 0;
 
-// CHECK: hl.ptr<int, const>
+// CHECK: !hl.ptr<!hl.int, const>
 int * const ipc = 0;
 
-// CHECK: hl.ptr<int<const>, const>
+// CHECK: !hl.ptr<!hl.int<const>, const>
 int const * const icpc = 0;
 
-// CHECK: hl.ptr<int<const>, const>
+// CHECK: !hl.ptr<!hl.int<const>, const>
 const int * const cipc = 0;
 
-// CHECK: hl.ptr<ptr<int>>
+// CHECK: !hl.ptr<!hl.ptr<!hl.int>>
 int ** ipp = 0;
 
-// CHECK: hl.ptr<ptr<int>, const>
+// CHECK: !hl.ptr<!hl.ptr<!hl.int>, const>
 int ** const ippc = 0;
 
-// CHECK: hl.ptr<ptr<int, const>>
+// CHECK: !hl.ptr<!hl.ptr<!hl.int, const>>
 int * const * ipcp = 0;
 
-// CHECK: hl.ptr<ptr<int<const>>>
+// CHECK: !hl.ptr<!hl.ptr<!hl.int<const>>>
 int const ** icpp = 0;
 
-// CHECK: hl.ptr<ptr<int, const>, const>
+// CHECK: !hl.ptr<!hl.ptr<!hl.int, const>, const>
 int * const * const ipcpc = 0;
 
-// CHECK: hl.ptr<ptr<ptr<int>>>
+// CHECK: !hl.ptr<!hl.ptr<!hl.ptr<!hl.int>>>
 int *** ippp = 0;
 
-// CHECK: hl.ptr<int<unsigned>>
+// CHECK: !hl.ptr<!hl.int<unsigned>>
 unsigned int *uip = 0;
