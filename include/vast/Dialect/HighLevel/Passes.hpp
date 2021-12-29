b50a43d8fa0ddf83335b65ee55207e691824f8c0
Lukas Korencik <lukas.korencik@trailofbits.com>
2021-12-29 21:15:44 +0100

    hl:toll: Initial stub of pass that handles hl -> ll.
    hl:toll: Add VarOp, ReturnOp and ConstanIntOp conversions.
    
    hl:toll: Remove obsolete code.
    
    hl:toll: Add AddIOp and SubIOp conversions.

--- include/vast/Dialect/HighLevel/Passes.hpp
+++ include/vast/Dialect/HighLevel/Passes.hpp
@@ -5,6 +5,7 @@
 #include "vast/Util/Warnings.hpp"
 
 VAST_RELAX_WARNINGS
+#include <mlir/IR/Operation.h>
 #include <mlir/Pass/Pass.h>
 VAST_UNRELAX_WARNINGS
 
@@ -12,6 +13,8 @@ VAST_UNRELAX_WARNINGS
 
 namespace vast::hl
 {
+    std::unique_ptr< mlir::Pass > createLowerHLToLLPass();
+
     std::unique_ptr< mlir::Pass > createLowerHighLevelTypesPass();
 
     std::unique_ptr< mlir::Pass > createStructsToTuplesPass();
