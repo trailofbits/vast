ea24be8cb0415c139444e92c74de38102adf8053
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-01-13 18:45:41 +0100

    test: Add loop.c, vars-c.c to type lowering tests.

---
+++ test/vast/Transform/HLLowerTypes/loop.c
@@ -0,0 +1,13 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-lower-types | FileCheck %s
+
+void loop_simple()
+{
+    // CHECK: [[V0:%[0-9]+]] = hl.declref @i : i32
+    // CHECK: [[V1:%[0-9]+]] = hl.implicit_cast [[V0]] LValueToRValue : i32 -> i32
+    // CHECK: [[V2:%[0-9]+]] = hl.constant.int 100 : i32
+    // CHECK: [[V3:%[0-9]+]] = hl.cmp slt i32 [[V1]], [[V2]] -> i1
+    // CHECK: hl.cond.yield [[V3]] : i1
+    for (int i = 0; i < 100; ++i) {
+        /* ... */
+    }
+}
---
+++ test/vast/Transform/HLLowerTypes/vars-c.c
@@ -0,0 +1,18 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-lower-types | FileCheck %s
+
+// CHECK-LABEL: func @main() -> i32
+int main()
+{
+    // CHECK: hl.var @x : i32 =  {
+    // CHECK:   [[V1:%[0-9]+]] = hl.constant.int 0 : i32
+    // CHECK:   hl.value.yield [[V1]] : i32
+    int x = 0;
+    // CHECK: hl.var @cx : i32 =  {
+    // CHECK:   [[V2:%[0-9]+]] = hl.constant.int 0 : i32
+    // CHECK:   hl.value.yield [[V2]] : i32
+    const int cx = 0;
+    // CHECK: hl.var @cvx : i32 =  {
+    // CHECK:   [[V3:%[0-9]+]] = hl.constant.int 0 : i32
+    // CHECK:   hl.value.yield [[V3]] : i32
+    const volatile int cvx = 0;
+}
