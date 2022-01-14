df6ea4f2ae530d45c8b9c351be0f9b5e51c56952
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-01-14 18:18:23 +0100

    util: Use vast defined assert macro.

--- include/vast/Util/Maybe.hpp
+++ include/vast/Util/Maybe.hpp
@@ -85,7 +85,7 @@ namespace vast
         explicit operator bool() const { return has_value(); }
         bool has_value() const
         {
-            assert(contains_value == static_cast< bool >(self));
+            ASSERT(contains_value == static_cast< bool >(self));
             return contains_value;
         }
     };
