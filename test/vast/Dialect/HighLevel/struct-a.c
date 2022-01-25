119d8881d6cb49be69493c998a4789f37a464416
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-01-25 17:11:07 +0100

    test: Update record codegen tests.

--- test/vast/Dialect/HighLevel/struct-a.c
+++ test/vast/Dialect/HighLevel/struct-a.c
@@ -1,12 +1,12 @@
 // RUN: vast-cc --ccopts -xc --from-source %s | FileCheck %s
 // RUN: vast-cc --ccopts -xc --from-source %s > %t && vast-opt %t | diff -B %t -
 
-// CHECK: hl.typedecl @empty
+// CHECK: hl.type.decl @empty
 // CHECK: hl.typedef @empty : !hl.record<>
 struct empty {};
 
-// CHECK: hl.typedecl @pair
-// CHECK: hl.typedef @pair : !hl.record<a: !hl.int, b: !hl.int>
+// CHECK: hl.type.decl @pair
+// CHECK: hl.typedef @pair : !hl.record<a : !hl.int, b : !hl.int>
 struct pair {
   int a, b;
 };
@@ -14,16 +14,16 @@ struct pair {
 // CHECK: hl.global @p : !hl.named_type<@pair>
 struct pair p;
 
-// CHECK: hl.typedecl @forward
+// CHECK: hl.type.decl @forward
 struct forward;
 
-// CHECK: hl.typedef @forward : !hl.record<a: !hl.int>
+// CHECK: hl.typedef @forward : !hl.record<a : !hl.int>
 struct forward {
   int a;
 };
 
-// CHECK: hl.typedecl @wrap
-// CHECK: hl.typedef @wrap : !hl.record<v: !hl.int>
+// CHECK: hl.type.decl @wrap
+// CHECK: hl.typedef @wrap : !hl.record<v : !hl.int>
 typedef struct wrap {
   int v;
 } wrap;
@@ -31,12 +31,12 @@ typedef struct wrap {
 // CHECK: hl.global @w : !hl.named_type<@wrap>
 wrap w;
 
-// CHECK: hl.typedecl @nested
+// CHECK: hl.type.decl @nested
 // CHECK: hl.typedef @nested : !hl.record<>
 struct nested {};
 
-// CHECK: hl.typedecl @compound
-// CHECK: hl.typedef @compound : !hl.record<n: !hl.named_type<@nested>, w: !hl.named_type<@wrap>>
+// CHECK: hl.type.decl @compound
+// CHECK: hl.typedef @compound : !hl.record<n : !hl.named_type<@nested>, w : !hl.named_type<@wrap>>
 struct compound {
   struct nested n;
   wrap w;
