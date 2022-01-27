1e9c1e4102ef3465bf14ca9ce3021a6cba9daaf7
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-01-27 20:10:19 +0100

    test: Update struct codegen tests.

---
+++ test/vast/Dialect/HighLevel/struct-c.c
@@ -0,0 +1,11 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | FileCheck %s
+// RUN: vast-cc --ccopts -xc --from-source %s > %t && vast-opt %t | diff -B %t -
+
+// CHECK: hl.type.decl @struct.anonymous.0
+// CHECK: hl.record @struct.anonymous.0 : {
+// CHECK:  hl.field @data : !hl.int
+// CHECK: }
+// CHECK: hl.global @named : !hl.named_type<@struct.anonymous.0>
+struct {
+  int data;
+} named;
