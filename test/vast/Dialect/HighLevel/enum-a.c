e6bc42369139726ba1a2469f5c69b80144078834
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-01-28 13:30:17 +0100

    test: Update enum typedef tests.

--- test/vast/Dialect/HighLevel/enum-a.c
+++ test/vast/Dialect/HighLevel/enum-a.c
@@ -1,14 +1,21 @@
 // RUN: vast-cc --ccopts -xc --from-source %s | FileCheck %s
 // RUN: vast-cc --ccopts -xc --from-source %s > %t && vast-opt %t | diff -B %t -
 
-// CHECK: hl.enum.decl @color : !hl.int<unsigned>
+// CHECK: hl.enum.decl @enum.color : !hl.int<unsigned> {
 // CHECK:  hl.enum.const @RED = 0 : si32
 // CHECK:  hl.enum.const @GREEN = 1 : si32
 // CHECK:  hl.enum.const @BLUE = 2 : si32
+// CHECK: }
 enum color { RED, GREEN, BLUE };
 
-// CHECK: hl.global @c : !hl.named_type<@color>
+// CHECK: hl.global @c : !hl.named_type<@enum.color>
 // CHECK: [[V1:%[0-9]+]] = hl.declref @GREEN : !hl.int
-// CHECK: [[V2:%[0-9]+]] = hl.implicit_cast [[V1]] IntegralCast : !hl.int -> !hl.named_type<@color>
-// CHECK: hl.value.yield [[V2]] : !hl.named_type<@color>
+// CHECK: [[V2:%[0-9]+]] = hl.implicit_cast [[V1]] IntegralCast : !hl.int -> !hl.named_type<@enum.color>
+// CHECK: hl.value.yield [[V2]] : !hl.named_type<@enum.color>
 enum color c = GREEN;
+
+// CHECK: hl.typedef @color : !hl.named_type<@enum.color>
+typedef enum color color;
+
+// CHECK: hl.global @tc : !hl.named_type<@color>
+color tc;
--- test/vast/Dialect/HighLevel/enum-b.c
+++ test/vast/Dialect/HighLevel/enum-b.c
@@ -4,15 +4,21 @@
 int puts(const char *str);
 
 int main() {
-    // CHECK: hl.enum.decl @color : !hl.int<unsigned>
+    // CHECK: hl.enum.decl @enum.color : !hl.int<unsigned>  {
     // CHECK:  hl.enum.const @RED = 0 : si32
     // CHECK:  hl.enum.const @GREEN = 1 : si32
     // CHECK:  hl.enum.const @BLUE = 2 : si32
-    // CHECK: hl.var @r : !hl.named_type<@color>
+    // CHECK: }
+
+    // CHECK: hl.var @r : !hl.named_type<@enum.color> = {
+    // CHECK:  [[V1:%[0-9]+]] = hl.declref @RED : !hl.int
+    // CHECK:  [[V2:%[0-9]+]] = hl.implicit_cast %1 IntegralCast : !hl.int -> !hl.named_type<@enum.color>
+    // CHECK:  hl.value.yield [[V2]] : !hl.named_type<@enum.color>
+    // CHECK: }
     enum color { RED, GREEN, BLUE } r = RED;
 
     // CHECK: hl.switch cond
-    // CHECK:  hl.declref @r : !hl.named_type<@color>
+    // CHECK:  hl.declref @r : !hl.named_type<@enum.color>
     switch(r) {
     // CHECK: hl.case
     // CHECK:  hl.declref @RED : !hl.int
