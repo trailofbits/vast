2bd6339045c1ca21266c257b02c34db642c9eb1a
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-02-04 16:33:26 +0100

    test: Fix comparisons returning integers.

--- test/vast/Transform/HLLowerTypes/loop.c
+++ test/vast/Transform/HLLowerTypes/loop.c
@@ -5,8 +5,8 @@ void loop_simple()
     // CHECK: [[V0:%[0-9]+]] = hl.declref @i : i32
     // CHECK: [[V1:%[0-9]+]] = hl.implicit_cast [[V0]] LValueToRValue : i32 -> i32
     // CHECK: [[V2:%[0-9]+]] = hl.constant.int 100 : i32
-    // CHECK: [[V3:%[0-9]+]] = hl.cmp slt i32 [[V1]], [[V2]] -> i1
-    // CHECK: hl.cond.yield [[V3]] : i1
+    // CHECK: [[V3:%[0-9]+]] = hl.cmp slt i32 [[V1]], [[V2]] -> i32
+    // CHECK: hl.cond.yield [[V3]] : i32
     for (int i = 0; i < 100; ++i) {
         /* ... */
     }
