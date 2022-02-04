f1d1cc74bb73e1ed18443424a7edbd7a0a466fcc
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-02-04 17:59:00 +0100

    test: Add alignof codegen tests.

---
+++ test/vast/Dialect/HighLevel/alignof-a.c
@@ -0,0 +1,7 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | FileCheck %s
+// RUN: vast-cc --ccopts -xc --from-source %s > %t && vast-opt %t | diff -B %t -
+
+int main() {
+    // CHECK: hl.alignof.type !hl.int -> !hl.long<unsigned>
+    unsigned long ai = _Alignof(int);
+}
---
+++ test/vast/Dialect/HighLevel/alignof-b.c
@@ -0,0 +1,14 @@
+// RUN: vast-cc --from-source %s | FileCheck %s
+// RUN: vast-cc --from-source %s > %t && vast-opt %t | diff -B %t -
+
+int main() {
+    // CHECK: hl.alignof.type !hl.int -> !hl.long<unsigned>
+    unsigned long si = alignof(int);
+
+    int v;
+
+    // CHECK: hl.var @sv : !hl.long<unsigned>
+    // CHECK: hl.alignof.expr -> !hl.long<unsigned>
+    // CHECK:  hl.declref @v : !hl.int
+    unsigned long sv = alignof v;
+}
