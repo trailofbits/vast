dc7c8e10e4359c2db7dddb7854b0fa9dd9e863d9
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-02-04 17:17:43 +0100

    test: Add sizeof codegen test.

---
+++ test/vast/Dialect/HighLevel/sizeof-a.c
@@ -0,0 +1,14 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | FileCheck %s
+// RUN: vast-cc --ccopts -xc --from-source %s > %t && vast-opt %t | diff -B %t -
+
+int main() {
+    // CHECK: hl.sizeof.type !hl.int -> !hl.long<unsigned>
+    unsigned long si = sizeof(int);
+
+    int v;
+
+    // CHECK: hl.var @sv : !hl.long<unsigned>
+    // CHECK: hl.sizeof.expr -> !hl.long<unsigned>
+    // CHECK:  hl.declref @v : !hl.int
+    unsigned long sv = sizeof v;
+}
