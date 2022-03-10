3fab9db380c96be6b2a9275761833e9ae0361ecc
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-03-10 14:52:49 +0100

    test: Add few query tests.

---
+++ test/query/symbols.c
@@ -0,0 +1,38 @@
+// RUN: vast-cc --ccopts -xc --from-source %s > %t && \
+// RUN: vast-query --show-symbols=all %t | \
+// RUN: FileCheck %s -check-prefix=FOO-VAR -check-prefix=MAIN-VAR -check-prefix=FUN
+
+// RUN: vast-cc --ccopts -xc --from-source %s > %t && \
+// RUN: vast-query --show-symbols=vars %t | \
+// RUN: FileCheck %s -check-prefix=FOO-VAR -check-prefix=MAIN-VAR
+
+// RUN: vast-cc --ccopts -xc --from-source %s > %t && \
+// RUN: vast-query --show-symbols=vars %t --scope=foo | \
+// RUN: FileCheck %s -check-prefix=FOO-VAR
+
+// RUN: vast-cc --ccopts -xc --from-source %s > %t && \
+// RUN: vast-query --show-symbols=vars %t --scope=main | \
+// RUN: FileCheck %s -check-prefix=MAIN-VAR
+
+// RUN: vast-cc --ccopts -xc --from-source %s > %t && \
+// RUN: vast-query --show-symbols=functions %t | \
+// RUN: FileCheck %s -check-prefix=FUN
+
+// FOO-VAR-DAG: hl.var : a
+// FUN-DAG: func : foo
+int foo() {
+    int a;
+    return a;
+}
+
+// MAIN-VAR-DAG: hl.var : a
+// MAIN-VAR-DAG: hl.var : b
+// MAIN-VAR-DAG: hl.var : c
+// FUN-DAG: func : main
+int main() 
+{
+    int a = 1, b = 1;
+    {
+        int c = a + b;
+    }
+}
---
+++ test/query/users.c
@@ -0,0 +1,29 @@
+// RUN: vast-cc --ccopts -xc --from-source %s > %t && \
+// RUN: vast-query --symbol-users=a %t | \
+// RUN: FileCheck %s -check-prefix=MAIN -check-prefix=FOO
+
+// RUN: vast-cc --ccopts -xc --from-source %s > %t && \
+// RUN: vast-query --symbol-users=a --scope=main %t | \
+// RUN: FileCheck %s -check-prefix=MAIN
+
+// RUN: vast-cc --ccopts -xc --from-source %s > %t && \
+// RUN: vast-query --symbol-users=a --scope=foo %t | \
+// RUN: FileCheck %s -check-prefix=FOO
+
+// FOO: hl.declref @a : !hl.int
+int foo() {
+    int a;
+    return a;
+}
+
+// MAIN: hl.declref @a : !hl.int
+// MAIN: hl.declref @a : !hl.int
+int main() 
+{
+    int a = 1, b = 1;
+    {
+        int c = a + b;
+    }
+
+    int d = a + 7;   
+}
