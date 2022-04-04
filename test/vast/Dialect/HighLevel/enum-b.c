b4d6f356c936556e12c3d67124bee89fce7fcf82
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-04-04 17:20:14 +0200

    test: Update enum tests.

--- test/vast/Dialect/HighLevel/enum-b.c
+++ test/vast/Dialect/HighLevel/enum-b.c
@@ -4,34 +4,34 @@
 int puts(const char *str);
 
 int main() {
-    // CHECK: hl.enum.decl @enum.color : !hl.int<unsigned>  {
-    // CHECK:  hl.enum.const @RED = 0 : si32
-    // CHECK:  hl.enum.const @GREEN = 1 : si32
-    // CHECK:  hl.enum.const @BLUE = 2 : si32
+    // CHECK: hl.enum.decl "enum color" : !hl.int<unsigned>  {
+    // CHECK:  hl.enum.const "RED" = 0 : si32
+    // CHECK:  hl.enum.const "GREEN" = 1 : si32
+    // CHECK:  hl.enum.const "BLUE" = 2 : si32
     // CHECK: }
 
-    // CHECK: hl.var "r" : !hl.named_type<@enum.color> = {
-    // CHECK:  [[V1:%[0-9]+]] = hl.declref "RED" : !hl.lvalue<!hl.int>
-    // CHECK:  [[V2:%[0-9]+]] = hl.implicit_cast %1 IntegralCast : !hl.int -> !hl.named_type<@enum.color>
-    // CHECK:  hl.value.yield [[V2]] : !hl.named_type<@enum.color>
+    // CHECK: hl.var "r" : !hl.lvalue<!hl.named_type<"enum color">> =  {
+    // CHECK:  [[V1:%[0-9]+]] = hl.enumref "RED" : !hl.int
+    // CHECK:  [[V2:%[0-9]+]] = hl.implicit_cast [[V1]] IntegralCast : !hl.int -> !hl.named_type<"enum color">
+    // CHECK:  hl.value.yield [[V2]] : !hl.named_type<"enum color">
     // CHECK: }
     enum color { RED, GREEN, BLUE } r = RED;
 
-    // CHECK: hl.switch cond
-    // CHECK:  hl.declref "r" : !hl.named_type<@enum.color>
+    // CHECK: hl.switch
+    // CHECK:  hl.declref %0 : !hl.lvalue<!hl.named_type<"enum color">>
     switch(r) {
     // CHECK: hl.case
-    // CHECK:  hl.declref "RED" : !hl.lvalue<!hl.int>
+    // CHECK:  hl.enumref "RED" : !hl.int
     case RED:
         puts("red");
         break;
     // CHECK: hl.case
-    // CHECK:  hl.declref "GREEN" : !hl.lvalue<!hl.int>
+    // CHECK:  hl.enumref "GREEN" : !hl.int
     case GREEN:
         puts("green");
         break;
     // CHECK: hl.case
-    // CHECK:  hl.declref "BLUE" : !hl.lvalue<!hl.int>
+    // CHECK:  hl.enumref "BLUE" : !hl.int
     case BLUE:
         puts("blue");
         break;
