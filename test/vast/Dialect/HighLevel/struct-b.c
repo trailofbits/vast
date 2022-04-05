30006e501ca80a07d700eea0ededdafc0f19c63b
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-04-05 09:50:16 +0200

    test: Update record tests.

--- test/vast/Dialect/HighLevel/struct-b.c
+++ test/vast/Dialect/HighLevel/struct-b.c
@@ -1,11 +1,10 @@
 // RUN: vast-cc --ccopts -xc --from-source %s | FileCheck %s
 // RUN: vast-cc --ccopts -xc --from-source %s > %t && vast-opt %t | diff -B %t -
 
-// CHECK: hl.type.decl @struct.node
-// CHECK: hl.record @struct.node : {
-// CHECK:  hl.field @data : !hl.int
-// CHECK:  hl.field @left : !hl.ptr<!hl.named_type<@struct.node>>
-// CHECK:  hl.field @right : !hl.ptr<!hl.named_type<@struct.node>>
+// CHECK: hl.record "struct node" : {
+// CHECK:  hl.field "data" : !hl.int
+// CHECK:  hl.field "left" : !hl.ptr<!hl.named_type<"struct node">>
+// CHECK:  hl.field "right" : !hl.ptr<!hl.named_type<"struct node">>
 // CHECK: }
 struct node {
   int data;
--- test/vast/Dialect/HighLevel/struct-c.c
+++ test/vast/Dialect/HighLevel/struct-c.c
@@ -1,11 +1,10 @@
 // RUN: vast-cc --ccopts -xc --from-source %s | FileCheck %s
 // RUN: vast-cc --ccopts -xc --from-source %s > %t && vast-opt %t | diff -B %t -
 
-// CHECK: hl.type.decl @struct.anonymous.0
-// CHECK: hl.record @struct.anonymous.0 : {
-// CHECK:  hl.field @data : !hl.int
+// CHECK: hl.record "struct anonymous.0" : {
+// CHECK:  hl.field "data" : !hl.int
 // CHECK: }
-// CHECK: hl.var "named" : !hl.named_type<@struct.anonymous.0>
+// CHECK: hl.var "named" : !hl.lvalue<!hl.named_type<"struct anonymous.0">>
 struct {
   int data;
 } named;
