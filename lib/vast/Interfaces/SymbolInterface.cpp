4b9a7974dd04f75d91f1fed7f555608e8ef7acb9
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-04-10 23:41:58 +0200

    treewide: Setup vast interface structure.

---
+++ lib/vast/Interfaces/SymbolInterface.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2022-present, Trail of Bits, Inc.
+
+#include "vast/Interfaces/SymbolInterface.hpp"
+
+//===----------------------------------------------------------------------===//
+// Symbol Interfaces
+//===----------------------------------------------------------------------===//
+
+/// Include the generated symbol interfaces.
+#include "vast/Interfaces/SymbolInterface.cpp.inc"
