5f1b083d4fc44757cf8df74b758cb06283401ea2
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-04-11 14:02:16 +0200

    test: Update type lowering tests.

--- test/vast/Transform/HLLowerTypes/array-a.c
+++ test/vast/Transform/HLLowerTypes/array-a.c
@@ -1,22 +1,22 @@
 // RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-lower-types | FileCheck %s
 
-// CHECK: hl.var "ai" : memref<10xi32>
+// CHECK: hl.var "ai" : !hl.lvalue<memref<10xi32>>
 int ai[10];
 
-// CHECK: hl.var "aci" : memref<5xi32>
+// CHECK: hl.var "aci" : !hl.lvalue<memref<5xi32>>
 const int aci[5];
 
-// CHECK: hl.var "avi" : memref<5xi32>
+// CHECK: hl.var "avi" : !hl.lvalue<memref<5xi32>>
 volatile int avi[5];
 
-// CHECK: hl.var "acvi" : memref<5xi32>
+// CHECK: hl.var "acvi" : !hl.lvalue<memref<5xi32>>
 const volatile int acvi[5];
 
-// CHECK: hl.var "acvui" : memref<5xi32>
+// CHECK: hl.var "acvui" : !hl.lvalue<memref<5xi32>>
 const volatile unsigned int acvui[5];
 
-// CHECK: hl.var "af" : memref<10xf32>
+// CHECK: hl.var "af" : !hl.lvalue<memref<10xf32>>
 float af[10];
 
-// CHECK: hl.var "a3d" : memref<2x4x3xf32>
+// CHECK: hl.var "a3d" : !hl.lvalue<memref<2x4x3xf32>>
 float a3d[2][4][3];
--- test/vast/Transform/HLLowerTypes/loop.c
+++ test/vast/Transform/HLLowerTypes/loop.c
@@ -2,11 +2,13 @@
 
 void loop_simple()
 {
-    // CHECK: [[V0:%[0-9]+]] = hl.declref "i" : i32
-    // CHECK: [[V1:%[0-9]+]] = hl.implicit_cast [[V0]] LValueToRValue : i32 -> i32
-    // CHECK: [[V2:%[0-9]+]] = hl.constant.int 100 : i32
-    // CHECK: [[V3:%[0-9]+]] = hl.cmp slt i32 [[V1]], [[V2]] -> i32
-    // CHECK: hl.cond.yield [[V3]] : i32
+    // CHECK: [[I:%[0-9]+]] = hl.var "i" : !hl.lvalue<i32>
+    // CHECK: hl.for
+    // CHECK:   [[V0:%[0-9]+]] = hl.declref [[I]] : !hl.lvalue<i32>
+    // CHECK:   [[V1:%[0-9]+]] = hl.implicit_cast [[V0]] LValueToRValue : !hl.lvalue<i32> -> i32
+    // CHECK:   [[V2:%[0-9]+]] = hl.constant.int 100 : i32
+    // CHECK:   [[V3:%[0-9]+]] = hl.cmp slt i32 [[V1]], [[V2]] -> i32
+    // CHECK:   hl.cond.yield [[V3]] : i32
     for (int i = 0; i < 100; ++i) {
         /* ... */
     }
