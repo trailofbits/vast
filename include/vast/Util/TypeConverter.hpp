6b84721a1b183a667141891f4054e3f501669914
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-04-18 20:33:41 +0200

    util: Add separate class that injects type conversion helpers via inheritance.

--- include/vast/Util/TypeConverter.hpp
+++ include/vast/Util/TypeConverter.hpp
@@ -7,8 +7,39 @@ VAST_UNRELAX_WARNINGS
 
 #include "vast/Util/Maybe.hpp"
 
-namespace vast::hl
+namespace vast::util
 {
+    template< typename Self >
+    struct TCHelpers
+    {
+        using types_t = mlir::SmallVector< mlir::Type >;
+        using maybe_type_t = llvm::Optional< mlir::Type >;
+        using maybe_types_t = llvm::Optional< types_t >;
+
+        Self &self() { return static_cast< Self & >(*this); }
+
+        auto convert_type() { return [&](auto t) { return self().do_conversion(t); }; }
+        auto convert_type_to_type()
+        {
+            return [&](auto t) { return self().convert_type_to_type(t); };
+        }
+
+        maybe_types_t convert_type_to_types(mlir::Type t, std::size_t count = 1)
+        {
+            return Maybe(t).and_then(self().convert_type())
+                           .keep_if([&](const auto &ts) { return ts->size() == count; })
+                           .template take_wrapped< maybe_types_t >();
+        }
+
+        maybe_type_t convert_type_to_type(mlir::Type t)
+        {
+            return Maybe(t).and_then([&](auto t){ return self().convert_type_to_types(t, 1); })
+                           .and_then([&](auto ts){ return *ts->begin(); })
+                           .template take_wrapped< maybe_type_t >();
+        }
+    };
+
+    // Comment out
     template< typename Impl >
     struct TypeConverterWrapper
     {
@@ -23,32 +54,12 @@ namespace vast::hl
         Impl *operator->() { return &impl; }
         const Impl *operator->() const { return &impl; }
 
-        maybe_types_t convert_type(mlir::Type t)
+        maybe_types_t do_conversion(mlir::Type t)
         {
             types_t out;
             if (mlir::succeeded(impl.convertTypes(t, out)))
                 return { std::move( out ) };
             return {};
         }
-
-        auto convert_type() { return [&](auto t) { return this->convert_type(t); }; }
-        auto convert_type_to_type()
-        {
-            return [&](auto t) { return this->convert_type_to_type(t); };
-        }
-
-        maybe_types_t convert_type_to_types(mlir::Type t, std::size_t count = 1)
-        {
-            return Maybe(t).and_then(convert_type())
-                           .keep_if([&](const auto &ts) { return ts->size() == count; })
-                           .template take_wrapped< maybe_types_t >();
-        }
-
-        maybe_type_t convert_type_to_type(mlir::Type t)
-        {
-            return Maybe(t).and_then([&](auto t){ return this->convert_type_to_types(t, 1); })
-                           .and_then([&](auto ts){ return *ts->begin(); })
-                           .template take_wrapped< maybe_type_t >();
-        }
     };
-} // namespace vast::hl
+} // namespace vast::util
