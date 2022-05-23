5f2e8e9a2bb46f0eb4ce7fa5b244058732d3cb71
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-05-23 17:12:25 +0200

    test: Add annotation attribute codegen test.

---
+++ test/vast/Dialect/HighLevel/attr-a.c
@@ -0,0 +1,10 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | FileCheck %s
+// RUN: vast-cc --ccopts -xc --from-source %s > %t && vast-opt %t | diff -B %t -
+
+// CHECK: hl.typedef "u64" : !hl.longlong<unsigned>
+typedef unsigned long long u64;
+
+# define __nocast	__attribute__((annotate("nocast")))
+
+// CHECK: hl.typedef "cputime_t" {annotation = #hl.annotation<"nocast">} : !hl.named_type<"u64">
+typedef u64 __nocast cputime_t;
