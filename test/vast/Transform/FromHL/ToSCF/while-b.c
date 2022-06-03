00a56cf6558611350a176214a1c33a10593b7fd2
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-06-03 19:36:56 +0200

    test:hltoscf: Add tests of while cycle conversion.

---
+++ test/vast/Transform/FromHL/ToSCF/while-b.c
@@ -0,0 +1,21 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-lower-types --vast-hl-structs-to-tuples --vast-hl-lower-cf | FileCheck %s
+
+// CHECK:   func @fn(%arg0: !hl.lvalue<i32>) -> none {
+void fn(int x)
+{
+    // CHECK: scf.while : () -> () {
+    // CHECK:   [[V0:%[0-9]+]] = hl.decl.ref %arg0 : !hl.lvalue<i32>
+    // CHECK:   [[V1:%[0-9]+]] = hl.implicit_cast [[V0]] LValueToRValue : !hl.lvalue<i32> -> i32
+    // CHECK:   [[V2:%[0-9]+]] = hl.constant.int 0 : i32
+    // CHECK:   [[V3:%[0-9]+]] = hl.cmp ne i32 [[V1]], [[V2]] -> i32
+    // CHECK:   [[V4:%[0-9]+]] = hl.implicit_cast [[V3]] IntegralCast : i32 -> i1
+    // CHECK:   scf.condition([[V4]])
+    // CHECK: } do {
+    // CHECK:   [[V10:%[0-9]+]] = hl.decl.ref %arg0 : !hl.lvalue<i32>
+    // CHECK:   [[V11:%[0-9]+]] = hl.pre.dec [[V10]] : !hl.lvalue<i32>
+    // CHECK:   scf.yield
+    // CHECK: }
+    // CHECK: hl.return
+    while (x != 0)
+        --x;
+}
