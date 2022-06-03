b1dd28a2b857a84c2e0c9c63229d27b0180243fa
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-06-03 19:25:26 +0200

    test:hltocsf: Add tests of if statements.

---
+++ test/vast/Transform/FromHL/ToSCF/if-a-b.c
@@ -0,0 +1,25 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-lower-types --vast-hl-structs-to-tuples --vast-hl-lower-cf | FileCheck %s
+
+void fn()
+{
+    // CHECK: [[V0:%[0-9]+]] = hl.var "x" : !hl.lvalue<i32> = {
+    // CHECK:   [[V14:%[0-9]+]] = hl.constant.int 12 : i32
+    // CHECK:   hl.value.yield [[V14]] : i32
+    // CHECK: }
+    // CHECK: [[V1:%[0-9]+]] = hl.decl.ref [[V0]] : !hl.lvalue<i32>
+    // CHECK: [[V2:%[0-9]+]] = hl.implicit_cast [[V1]] LValueToRValue : !hl.lvalue<i32> -> i32
+    // CHECK: [[V3:%[0-9]+]] = hl.implicit_cast [[V2]] IntegralCast : i32 -> i1
+    // CHECK: scf.if [[V3]] {
+    // CHECK:   [[V24:%[0-9]+]] = hl.var "y" : !hl.lvalue<i32> = {
+    // CHECK:     [[V25:%[0-9]+]] = hl.constant.int 45 : i32
+    // CHECK:     hl.value.yield [[V25]] : i32
+    // CHECK:   }
+    // CHECK: }
+    // CHECK: hl.return
+
+    int x = 12;
+    if (x)
+    {
+        int y = 45;
+    }
+}
---
+++ test/vast/Transform/FromHL/ToSCF/if-b-a.c
@@ -0,0 +1,29 @@
+// RUN: vast-cc --from-source %s | vast-opt --vast-hl-lower-types --vast-hl-structs-to-tuples --vast-hl-lower-cf | FileCheck %s
+
+void fn()
+{
+    int x = 12;
+
+    // CHECK: [[V1:%[0-9]+]] = hl.decl.ref [[V0]] : !hl.lvalue<i32>
+    // CHECK: [[V2:%[0-9]+]] = hl.implicit_cast [[V1]] LValueToRValue : !hl.lvalue<i32> -> i32
+    // CHECK: [[V3:%[0-9]+]] = hl.implicit_cast [[V2]] IntegralToBoolean : i32 -> i1
+    // CHECK: scf.if [[V3]] {
+    // CHECK:   [[V14:%[0-9]+]] = hl.var "y" : !hl.lvalue<i32> = {
+    // CHECK:     [[V15:%[0-9]+]] = hl.constant.int 45 : i32
+    // CHECK:     hl.value.yield [[V15]] : i32
+    // CHECK:   }
+    // CHECK: } else {
+    // CHECK:   [[V24:%[0-9]+]] = hl.var "y" : !hl.lvalue<i32> = {
+    // CHECK:     [[V25:%[0-9]+]] = hl.constant.int 55 : i32
+    // CHECK:     hl.value.yield [[V25]] : i32
+    // CHECK:   }
+    // CHECK: }
+    // CHECK: hl.return
+
+    if (x)
+    {
+        int y = 45;
+    } else {
+        int y = 55;
+    }
+}
