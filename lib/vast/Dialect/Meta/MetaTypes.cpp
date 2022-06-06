4b672728f21eb2254b677177059dc7daf2fffd9b
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-06-06 16:50:48 +0200

    meta: Setup dialect.

---
+++ lib/vast/Dialect/Meta/MetaTypes.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2022-present, Trail of Bits, Inc.
+
+#include "vast/Dialect/Meta/MetaTypes.hpp"
+
+using StringRef = llvm::StringRef; // to fix missing namespace in generated file
+
+#define  GET_TYPEDEF_CLASSES
+#include "vast/Dialect/Meta/MetaTypes.cpp.inc"
