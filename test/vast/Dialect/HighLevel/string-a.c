8f45bf1de64aac5021a1f87a60add3825b84674d
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-06-06 11:03:58 +0200

    test:hl: Update high-level array tests.

--- test/vast/Dialect/HighLevel/string-a.c
+++ test/vast/Dialect/HighLevel/string-a.c
@@ -2,6 +2,6 @@
 // RUN: vast-cc --ccopts -xc --from-source %s > %t && vast-opt %t | diff -B %t -
 
 // CHECK: hl.var "gstr" : !hl.lvalue<!hl.ptr<!hl.char<const>>>
-// CHECK: hl.constant.string "global" : !hl.const.array<7, !hl.char>
-// CHECK: ArrayToPointerDecay : !hl.const.array<7, !hl.char> -> !hl.ptr<!hl.char>
+// CHECK: hl.constant.string "global" : !hl.array<7, !hl.char>
+// CHECK: ArrayToPointerDecay : !hl.array<7, !hl.char> -> !hl.ptr<!hl.char>
 const char *gstr = "global";
