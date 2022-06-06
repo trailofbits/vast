397e1e94c99383e7afa782a6b0e10661c5417a16
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-06-06 14:09:40 +0200

    test: Update invocations of vast-opt.

--- test/vast/Transform/FromHL/ToLL/hl-assign-a.c
+++ test/vast/Transform/FromHL/ToLL/hl-assign-a.c
@@ -1,4 +1,4 @@
-// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-lower-types --vast-hl-structs-to-tuples --vast-hl-lower-to-ll | FileCheck %s
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-lower-types --vast-hl-structs-to-tuples --vast-hl-to-ll | FileCheck %s
 
 void count(int arg)
 {
