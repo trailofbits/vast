04d13712f81956f9032a81690e8af6195558e6ed
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-06-21 14:28:42 +0200

    hl:fnexport: Export sizes of types.

--- lib/vast/Dialect/HighLevel/Transforms/ExportFnInfo.cpp
+++ lib/vast/Dialect/HighLevel/Transforms/ExportFnInfo.cpp
@@ -3,6 +3,8 @@
 #include "vast/Dialect/HighLevel/Passes.hpp"
 
 VAST_RELAX_WARNINGS
+#include <mlir/Analysis/DataLayoutAnalysis.h>
+
 #include <mlir/Target/LLVMIR/Dialect/All.h>
 
 #include <llvm/Support/FileSystem.h>
@@ -24,25 +26,28 @@ namespace vast::hl
         void runOnOperation() override
         {
             auto &mctx = this->getContext();
-            mlir::ModuleOp module = this->getOperation();
+            mlir::ModuleOp mod = this->getOperation();
 
             std::error_code ec;
             llvm::raw_fd_ostream out("exported_fn_info.json", ec, llvm::sys::fs::OF_Text);
             VAST_ASSERT(!ec);
 
             llvm::json::Object top;
-            for (auto &op : *module.getBody())
+            for (auto &op : *mod.getBody())
             {
                 auto fn = mlir::dyn_cast< mlir::FuncOp >(op);
                 if (!fn)
                     continue;
+                const auto &dl_analysis = this->getAnalysis< mlir::DataLayoutAnalysis >();
+                const auto &dl = dl_analysis.getAtOrAbove(mod);
+
                 llvm::json::Array args;
                 for (auto &arg_type : fn.getArgumentTypes())
-                    args.push_back(parse_typeinfo(arg_type));
+                    args.push_back(parse_typeinfo(dl, arg_type));
 
                 llvm::json::Array rets;
                 for (auto &ret_type : fn.getResultTypes())
-                    rets.push_back(parse_typeinfo(ret_type));
+                    rets.push_back(parse_typeinfo(dl, ret_type));
 
                 llvm::json::Object current;
                 current["rets"] = std::move(rets);
@@ -53,24 +58,28 @@ namespace vast::hl
             out << llvm::formatv("{0:2}",llvm::json::Value(std::move(top)));
         }
 
-        llvm::json::Object parse_typeinfo(mlir::Type hl_type)
+        llvm::json::Object parse_typeinfo(
+                const mlir::DataLayout &dl,
+                mlir::Type hl_type)
         {
             auto ptr_type = [&](auto ptr_type)
             {
                 llvm::json::Object out;
                 out["type"] = "ptr";
-                out["element_type"] = parse_typeinfo(ptr_type.getElementType());
+                out["size"] = dl.getTypeSizeInBits(ptr_type);
+                out["element_type"] = parse_typeinfo(dl, ptr_type.getElementType());
                 return out;
             };
 
             auto lvalue_type = [&](auto lvalue_ty)
             {
-                return parse_typeinfo(lvalue_ty.getElementType());
+                return parse_typeinfo(dl, lvalue_ty.getElementType());
             };
 
             auto void_type = [&](auto void_ty)
             {
                 llvm::json::Object out;
+                out["size"] = 0;
                 out["type"] = void_ty.getMnemonic();
                 return out;
             };
@@ -78,6 +87,7 @@ namespace vast::hl
             auto scalars = [&](auto scalar)
             {
                 llvm::json::Object out;
+                out["size"] = dl.getTypeSizeInBits(scalar);
                 out["type"] = scalar.getMnemonic();
                 out["const"] = scalar.isConst();
                 out["volatile"] = scalar.isVolatile();
