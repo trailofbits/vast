76ad5368a797defa3b8eeac5bcfc19e9bfb8377b
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-06-26 12:09:49 +0200

    meta: Setup types and attributes.

--- lib/vast/Dialect/Meta/MetaTypes.cpp
+++ lib/vast/Dialect/Meta/MetaTypes.cpp
@@ -4,5 +4,14 @@
 
 using StringRef = llvm::StringRef; // to fix missing namespace in generated file
 
+namespace vast::meta {
+    void MetaDialect::registerTypes() {
+        addTypes<
+            #define GET_TYPEDEF_LIST
+            #include "vast/Dialect/Meta/MetaTypes.cpp.inc"
+        >();
+    }
+} // namespace vast::meta
+
 #define  GET_TYPEDEF_CLASSES
 #include "vast/Dialect/Meta/MetaTypes.cpp.inc"
