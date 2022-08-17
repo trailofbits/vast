64cbb58644e62686f7107b531e4d7de2f902bdb7
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-08-17 14:19:15 +0200

    ll: Add a bunch of missing files to have a minimal working dialect.

---
+++ include/vast/Dialect/LowLevel/LowLevelDialect.hpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2022-present, Trail of Bits, Inc.
+
+#pragma once
+
+#include "vast/Util/Warnings.hpp"
+
+VAST_RELAX_WARNINGS
+#include <mlir/IR/BuiltinOps.h>
+#include <mlir/IR/BuiltinTypes.h>
+#include <mlir/IR/Dialect.h>
+#include <mlir/IR/OperationSupport.h>
+#include <mlir/Interfaces/SideEffectInterfaces.h>
+VAST_RELAX_WARNINGS
+
+
+// Pull in the dialect definition.
+#include "vast/Dialect/LowLevel/LowLevelDialect.h.inc"
