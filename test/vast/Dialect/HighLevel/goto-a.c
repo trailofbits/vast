41768e433ad6864fb4f92c281010eda23d8cea5c
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-08-24 10:25:06 +0200

    test: Add goto tests.

---
+++ test/vast/Dialect/HighLevel/goto-a.c
@@ -0,0 +1,15 @@
+// RUN: vast-cc --from-source %s | FileCheck %s
+// RUN: vast-cc --from-source %s > %t && vast-opt %t | diff -B %t -
+
+int main() {
+    // CHECK: [[L:%[0-9]+]] = hl.label.decl "end" : !hl.label
+
+    // CHECK: hl.var "x" : !hl.lvalue<!hl.int>
+    int x;
+
+    // CHECK: hl.goto [[L]]
+    goto end;
+
+    // CHECK: hl.label [[L]]
+    end:;
+}
