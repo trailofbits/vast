9b135cde0bc05139ed882dad2a14b573819e1c07
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-08-24 15:51:57 +0200

    test:hltollvm: Update tests to reflect changes in name.

--- test/vast/Transform/FromHL/ToLL/fn-a.c
+++
@@ -1,9 +0,0 @@
-// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-lower-types --vast-hl-structs-to-tuples --vast-hl-to-ll | FileCheck %s
-// REQUIRES: to-ll
-
-// CHECK: llvm.func @fn() {
-void fn()
-{
-    // CHECK: llvm.return
-}
-// CHECK : }
--- test/vast/Transform/FromHL/ToLLVM/hl-assign-b.c
+++ test/vast/Transform/FromHL/ToLLVM/hl-assign-b.c
@@ -1,4 +1,4 @@
-// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-lower-types --vast-hl-structs-to-tuples --vast-hl-to-ll | FileCheck %s
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-lower-types --vast-hl-structs-to-tuples --vast-hl-to-llvm | FileCheck %s
 // REQUIRES: to-ll
 
 // CHECK: llvm.func @count([[ARG:%arg[0-9]+]]: i32)
