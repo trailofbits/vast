3806edcb170c0c0d9e017863fb9a18438e1f53d1
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-08-24 10:25:34 +0200

    test: Add vararg test.

---
+++ test/vast/Dialect/HighLevel/vararg-a.c
@@ -0,0 +1,12 @@
+// RUN: vast-cc --from-source %s | FileCheck %s
+// RUN: vast-cc --from-source %s > %t && vast-opt %t | diff -B %t -
+// REQUIRES: libc
+
+#include <stdarg.h>
+
+void format(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+
+    va_end(args);
+}
