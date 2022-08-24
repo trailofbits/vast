8eda9c1d1b8a10f582c975a1cc121a79b45cab02
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-08-24 15:04:04 +0200

    util: Add extended BasePattern class with type converter support.

--- include/vast/Util/DialectConversion.hpp
+++ include/vast/Util/DialectConversion.hpp
@@ -40,8 +40,42 @@ namespace vast::util
                 typename operation_t::Adaptor ops,
                 mlir::ConversionPatternRewriter &rewriter) const override
         {
-            return Impl< operation_t >(op, ops, rewriter).convert();
+            // Required, because even if the method is overloaded in one of the
+            // children, this method must still compile.
+            if constexpr( std::is_constructible_v< Impl< operation_t >,
+                    decltype(op),
+                    decltype(ops),
+                    decltype(rewriter) >)
+            {
+                return Impl< operation_t >(op, ops, rewriter).convert();
+            } else {
+                return mlir::failure();
+            }
         }
+    };
+
+    // TODO(lukas) Unfortunately, extending the class is rather tedious, might
+    //             need to rethink how pieces fit together.
+    template< typename Op, typename TC, template< typename > class Impl >
+    struct TypeConvertingPattern : BasePattern< Op, Impl >
+    {
+        using parent_t = BasePattern< Op, Impl >;
+        using operation_t = typename parent_t::operation_t;
+
+        TC &tc;
+
+        TypeConvertingPattern(TC &tc, mlir::MLIRContext *mctx)
+            : parent_t(mctx), tc(tc)
+        {}
+
+        mlir::LogicalResult matchAndRewrite(
+                operation_t op,
+                typename operation_t::Adaptor ops,
+                mlir::ConversionPatternRewriter &rewriter) const override
+        {
+            return Impl< operation_t >(tc, op, ops, rewriter).convert();
+        }
+
     };
 
 } // namespace vast::util
