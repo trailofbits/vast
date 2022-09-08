f2a1b1fd8d3966ca44be96fecaad54d71d7e5f4c
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-09-08 12:08:48 +0200

    test: Update unary return types.

--- test/vast/Dialect/HighLevel/assign-a.c
+++ test/vast/Dialect/HighLevel/assign-a.c
@@ -8,7 +8,7 @@ void foo() {
     // CHECK: [[A:%[0-9]+]] = hl.decl.ref [[AG]] : !hl.lvalue<!hl.int>
     // CHECK: [[BG:%[0-9]+]] = hl.global.ref "b" : !hl.lvalue<!hl.int>
     // CHECK: [[B1:%[0-9]+]] = hl.decl.ref [[BG]] : !hl.lvalue<!hl.int>
-    // CHECK: [[B2:%[0-9]+]] = hl.pre.inc [[B1]] : !hl.lvalue<!hl.int>
-    // CHECK: hl.assign [[B2]] to [[A]] : !hl.lvalue<!hl.int>, !hl.lvalue<!hl.int> -> !hl.lvalue<!hl.int>
+    // CHECK: [[B2:%[0-9]+]] = hl.pre.inc [[B1]] : !hl.lvalue<!hl.int> -> !hl.int
+    // CHECK: hl.assign [[B2]] to [[A]] : !hl.int, !hl.lvalue<!hl.int> -> !hl.int
     a = ++b;
 }
---
+++ test/vast/Dialect/HighLevel/unary-b.c
@@ -0,0 +1,11 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | FileCheck %s
+// RUN: vast-cc --from-source %s > %t && vast-opt %t | diff -B %t -
+
+int main() {
+    // CHECK: [[A:%[0-9]+]] = hl.var "a" : !hl.lvalue<!hl.int>
+    int a = 1;
+    // CHECK: [[B:%[0-9]+]] = hl.var "b" : !hl.lvalue<!hl.int>
+    // CHECK:   [[AR:%[0-9]+]] = hl.decl.ref [[A]] : !hl.lvalue<!hl.int>
+    // CHECK:   hl.pre.dec [[AR]] : !hl.lvalue<!hl.int> -> !hl.int
+    int b = --a;
+}
