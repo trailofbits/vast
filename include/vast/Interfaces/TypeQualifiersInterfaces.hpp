3ce9eb415d9d2e94523e96e02c8dfc853aed2a9d
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-09-22 16:30:27 +0200

    hl: Implement type qualifiers interfaces.

---
+++ include/vast/Interfaces/TypeQualifiersInterfaces.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2022-present, Trail of Bits, Inc.
+
+#pragma once
+
+#include "vast/Util/Warnings.hpp"
+
+VAST_RELAX_WARNINGS
+#include <mlir/IR/Types.h>
+VAST_RELAX_WARNINGS
+
+/// Include the generated interface declarations.
+#include "vast/Interfaces/TypeQualifiersInterfaces.h.inc"
---
+++ lib/vast/Interfaces/TypeQualifiersInterfaces.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2022-present, Trail of Bits, Inc.
+
+#include "vast/Interfaces/TypeQualifiersInterfaces.hpp"
+
+//===----------------------------------------------------------------------===//
+// Type Qualifiers Interfaces
+//===----------------------------------------------------------------------===//
+
+/// Include the generated type qualifiers interfaces.
+#include "vast/Interfaces/TypeQualifiersInterfaces.cpp.inc"
