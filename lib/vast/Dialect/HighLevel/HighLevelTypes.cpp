b3fcb154bd250d9753933cadec830b6b06b8f17e
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-09-22 16:33:08 +0200

    hl: Clean up type definitions and uniformly integrate qualifiers.

--- lib/vast/Dialect/HighLevel/HighLevelTypes.cpp
+++ lib/vast/Dialect/HighLevel/HighLevelTypes.cpp
@@ -13,6 +13,22 @@ VAST_RELAX_WARNINGS
 
 namespace vast::hl
 {
+    Type LValueType::replaceImmediateSubElements(llvm::ArrayRef<mlir::Attribute>, llvm::ArrayRef<mlir::Type>) const {
+        VAST_UNIMPLEMENTED;
+    }
+
+    Type ElaboratedType::replaceImmediateSubElements(llvm::ArrayRef<mlir::Attribute>, llvm::ArrayRef<mlir::Type>) const {
+        VAST_UNIMPLEMENTED;
+    }
+
+    Type PointerType::replaceImmediateSubElements(llvm::ArrayRef<mlir::Attribute>, llvm::ArrayRef<mlir::Type>) const {
+        VAST_UNIMPLEMENTED;
+    }
+
+    Type ArrayType::replaceImmediateSubElements(llvm::ArrayRef<mlir::Attribute>, llvm::ArrayRef<mlir::Type>) const {
+        VAST_UNIMPLEMENTED;
+    }
+
     mlir::FunctionType getFunctionType(PointerType functionPointer)
     {
         return functionPointer.getElementType().cast< mlir::FunctionType >();
@@ -55,9 +71,10 @@ namespace vast::hl
             return builtin_type.isSigned();
 
         VAST_ASSERT(isIntegerType(type));
-        return util::dispatch< integer_types, bool >(type, [] (auto ty) {
-            return ty.isSigned();
-        });
+        return false;
+        // return util::dispatch< integer_types, bool >(type, [] (auto ty) {
+        //     return ty.isSigned();
+        // });
     }
 
     bool isUnsigned(mlir::Type type)
@@ -69,120 +86,6 @@ namespace vast::hl
     {
         return util::is_one_of< high_level_types >(type);
     }
-
-    template< typename CVType >
-    Type parse_cv_type(Context *ctx, DialectParser &parser)
-    {
-        if (failed(parser.parseOptionalLess())) {
-            return CVType::get(ctx);
-        }
-
-        bool c = succeeded(parser.parseOptionalKeyword("const"));
-        bool v = succeeded(parser.parseOptionalKeyword("volatile"));
-
-        if (failed(parser.parseGreater())) {
-            return Type();
-        }
-
-        return CVType::get(ctx, c, v);
-    }
-
-    template< typename CVType >
-    void print_cv_type(const CVType &type, DialectPrinter &printer)
-    {
-        if ( !(type.getIsVolatile() || type.getIsConst()) ) {
-            return;
-        }
-
-        bool first = true;
-        auto print = [&] (auto qual) {
-            printer << (!first ? " " : "") << qual;
-            first = false;
-        };
-
-        printer << "<";
-        if (type.isConst())    { print("const"); }
-        if (type.isVolatile()) { print("volatile"); }
-        printer << ">";
-    }
-
-    template< typename IntegerType >
-    Type parse_integer_type(Context *ctx, DialectParser &parser)
-    {
-        if (failed(parser.parseOptionalLess())) {
-            return IntegerType::get(ctx);
-        }
-
-        bool u = succeeded(parser.parseOptionalKeyword("unsigned"));
-        bool c = succeeded(parser.parseOptionalKeyword("const"));
-        bool v = succeeded(parser.parseOptionalKeyword("volatile"));
-
-        if (failed(parser.parseGreater())) {
-            return Type();
-        }
-
-        return IntegerType::get(ctx, u, c, v);
-    }
-
-    template< typename IntegerType >
-    void print_integer_type(const IntegerType &type, DialectPrinter &printer)
-    {
-        if ( !(type.getIsVolatile() || type.getIsConst() || type.getIsUnsigned()) ) {
-            return;
-        }
-
-        bool first = true;
-        auto print = [&] (auto qual) {
-            printer << (!first ? " " : "") << qual;
-            first = false;
-        };
-
-        printer << "<";
-        if (type.isUnsigned()) { print("unsigned"); }
-        if (type.isConst())    { print("const"); }
-        if (type.isVolatile()) { print("volatile"); }
-        printer << ">";
-    }
-
-    Type parse_pointer_type(Context *ctx, DialectParser &parser) {
-        if (failed(parser.parseLess())) {
-            return Type();
-        }
-
-        Type element;
-        if (failed(parser.parseType(element))) {
-            auto loc = parser.getCurrentLocation();
-            parser.emitError(loc, "expected pointer element type");
-            return Type();
-        }
-
-        bool c = false, v = false;
-        if (succeeded(parser.parseOptionalComma())) {
-            c = succeeded(parser.parseOptionalKeyword("const"));
-            v = succeeded(parser.parseOptionalKeyword("volatile"));
-        }
-
-        if (failed(parser.parseGreater())) {
-            return Type();
-        }
-
-        return PointerType::get(ctx, element, c, v);
-    }
-
-    void print_pointer_type(const PointerType &type, DialectPrinter &printer)
-    {
-        printer << "<";
-        printer.printType(type.getElementType());
-
-        if ( type.getIsVolatile() || type.getIsConst() ) {
-            printer << ",";
-            if (type.isConst())    { printer << " const"; }
-            if (type.isVolatile()) { printer << " volatile"; }
-        }
-
-        printer << ">";
-    }
-
 
 } // namespace vast::hl
 
@@ -203,6 +106,11 @@ namespace vast::hl
         tys( this->getElementType() );
     }
 
+    void ElaboratedType::walkImmediateSubElements(walk_attrs, walk_types tys) const {
+        tys( this->getElementType() );
+    }
+
+
     void PointerType::walkImmediateSubElements(walk_attrs, walk_types tys) const {
         tys( this->getElementType() );
     }
@@ -217,9 +125,9 @@ namespace vast::hl
         // If this ever is generalised investigate if `SubElementTypeInterface` can be used
         // do this recursion?
         auto collect = [&](ArrayType arr, auto &self) -> mlir::Type {
-            dims.push_back(arr.getSize());
-            if (auto nested = arr.getElementType().dyn_cast< ArrayType >())
-                return self(nested, self);
+            // dims.push_back(arr.getSize());
+            // if (auto nested = arr.getElementType().dyn_cast< ArrayType >())
+            //     return self(nested, self);
             return arr.getElementType();
         };
         return { std::move(dims), collect(*this, collect) };
