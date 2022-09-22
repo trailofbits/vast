c67a22e3ebae198ef3847f175c70f0e4aaa178d5
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-09-22 17:23:35 +0200

    codegen: Clean up type visitor and generate type qualifiers.

--- include/vast/Translation/CodeGenTypeVisitor.hpp
+++ include/vast/Translation/CodeGenTypeVisitor.hpp
@@ -8,7 +8,9 @@ VAST_RELAX_WARNINGS
 #include <clang/AST/TypeVisitor.h>
 VAST_UNRELAX_WARNINGS
 
+#include "vast/Util/Common.hpp"
 #include "vast/Translation/CodeGenMeta.hpp"
+#include "vast/Translation/CodeGenBuilder.hpp"
 #include "vast/Translation/CodeGenVisitorBase.hpp"
 #include "vast/Translation/CodeGenVisitorLens.hpp"
 
@@ -23,6 +25,7 @@ namespace vast::hl {
     struct CodeGenTypeVisitorMixin
         : clang::TypeVisitor< CodeGenTypeVisitorMixin< Derived >, Type >
         , CodeGenVisitorLens< CodeGenTypeVisitorMixin< Derived >, Derived >
+        , CodeGenBuilderMixin< CodeGenTypeVisitorMixin< Derived >, Derived >
     {
         using base_type = clang::TypeVisitor< CodeGenTypeVisitorMixin< Derived >, Type >;
 
@@ -35,35 +38,147 @@ namespace vast::hl {
         using LensType::mcontext;
         using LensType::visit;
 
-        Type Visit(clang::QualType ty) {
-            auto [underlying, quals] = ty.split();
-            auto res = visit(underlying);
+        using qualifiers   = clang::Qualifiers;
 
-            // TODO(Heno): add qualifiers
-            // if (ty.isConstQualified()) {
+        template< typename high_level_type >
+        auto type_builder() {
+            return this->template make_type< high_level_type >().bind(&mcontext());
+        };
 
-            // }
+        auto with_cv_qualifiers(auto &&state, qualifiers quals) {
+            return std::move(state)
+                .bind(quals.hasConst())
+                .bind(quals.hasVolatile());
+        };
 
-            // if (ty.isRestrictQualified()) {
+        auto with_cvr_qualifiers(auto &&state, qualifiers quals) {
+            return with_cv_qualifiers(std::move(state), quals)
+                .bind(quals.hasRestrict());
+        };
 
-            // }
-
-            // if (ty.isVolatileQualified()) {
-
-            // }
-
-            return res;
+        auto with_qualifiers(auto &&state, const clang::BuiltinType *ty, qualifiers quals) -> mlir_type {
+            return with_cv_qualifiers(std::move(state), quals).freeze();
         }
 
-        Type VisitElaboratedType(const clang::ElaboratedType *ty) {
-            return visit(ty->getNamedType());
+        template< typename high_level_type >
+        auto with_qualifiers(const clang::BuiltinType *ty, qualifiers quals) -> mlir_type;
+
+        template<>
+        auto with_qualifiers< VoidType >(const clang::BuiltinType *ty, qualifiers quals) -> mlir_type {
+            return with_cv_qualifiers(type_builder< VoidType >(), quals).freeze();
         }
 
-        Type VisitLValueType(clang::QualType ty) {
+        template<>
+        auto with_qualifiers< BoolType >(const clang::BuiltinType *ty, qualifiers quals) -> mlir_type {
+            return with_qualifiers(type_builder< BoolType >(), ty, quals);
+        }
+
+        template< high_level_integer_type type >
+        auto with_qualifiers(const clang::BuiltinType *ty, qualifiers quals) -> mlir_type {
+            assert(ty->isIntegerType());
+            return with_qualifiers(type_builder< type >().bind(ty->isUnsignedIntegerType()), ty, quals);
+        }
+
+        template< high_level_floating_type type >
+        auto with_qualifiers(const clang::BuiltinType *ty, qualifiers quals) -> mlir_type {
+            return with_qualifiers(type_builder< type >(), ty, quals);
+        }
+
+        auto with_qualifiers(const clang::PointerType *ty, qualifiers quals) -> mlir_type {
+            auto pointee = visit(ty->getPointeeType());
+            return with_cvr_qualifiers(type_builder< PointerType >().bind(pointee), quals).freeze();
+        }
+
+        auto with_qualifiers(const clang::ArrayType *ty, qualifiers quals) -> mlir_type {
+            // return ArrayType::get(&ctx, get_size_attr(ty, ctx), element_type);
+            auto element_type = visit(ty->getElementType());
+            return with_cvr_qualifiers(type_builder< ArrayType >().bind(element_type), quals).freeze();
+        }
+
+        auto make_name_attr(string_ref name) {
+            return mlir::StringAttr::get(&mcontext(), name);
+        }
+
+        auto with_qualifiers(const clang::RecordType *ty, qualifiers quals) -> mlir_type {
+            auto name = make_name_attr( context().decl_name(ty->getDecl()) );
+            return with_cv_qualifiers( type_builder< RecordType >().bind(name), quals ).freeze();
+        }
+
+        auto with_qualifiers(const clang::EnumType *ty, qualifiers quals) -> mlir_type {
+            auto name = make_name_attr( context().decl_name(ty->getDecl()) );
+            return with_cv_qualifiers( type_builder< RecordType >().bind(name), quals ).freeze();
+        }
+
+        auto with_qualifiers(const clang::TypedefType *ty, qualifiers quals) -> mlir_type {
+            auto name = make_name_attr( ty->getDecl()->getName() );
+            return with_cvr_qualifiers( type_builder< TypedefType >().bind(name), quals ).freeze();
+        }
+
+        auto with_qualifiers(const clang::ElaboratedType *ty, qualifiers quals) -> mlir_type {
+            auto element_type = visit(ty->getNamedType());
+            return with_cvr_qualifiers(type_builder< ElaboratedType >().bind(element_type), quals).freeze();
+        }
+
+        auto Visit(clang::QualType ty) -> mlir_type {
+            auto underlying = ty.getTypePtr();
+            auto quals      = ty.getQualifiers();
+            if (auto t = llvm::dyn_cast< clang::BuiltinType >(underlying)) {
+                return VisitBuiltinType(t, quals);
+            }
+
+            if (auto t = llvm::dyn_cast< clang::PointerType >(underlying)) {
+                return VisitPointerType(t, quals);
+            }
+
+            if (auto t = llvm::dyn_cast< clang::ArrayType >(underlying)) {
+                return VisitArrayType(t, quals);
+            }
+
+            if (auto t = llvm::dyn_cast< clang::ElaboratedType >(underlying)) {
+                return VisitElaboratedType(t, quals);
+            }
+
+            if (auto t = llvm::dyn_cast< clang::RecordType >(underlying)) {
+                return VisitRecordType(t, quals);
+            }
+
+            if (auto t = llvm::dyn_cast< clang::EnumType >(underlying)) {
+                return VisitEnumType(t, quals);
+            }
+
+            if (auto t = llvm::dyn_cast< clang::TypedefType >(underlying)) {
+                return VisitTypedefType(t, quals);
+            }
+
+            if (auto t = llvm::dyn_cast< clang::ParenType >(underlying)) {
+                return VisitParenType(t, quals);
+            }
+
+            if (auto t = llvm::dyn_cast< clang::FunctionProtoType >(underlying)) {
+                return VisitFunctionProtoType(t, quals);
+            }
+
+            if (auto t = llvm::dyn_cast< clang::FunctionNoProtoType >(underlying)) {
+                return VisitFunctionNoProtoType(t, quals);
+            }
+
+            VAST_UNREACHABLE("unsupported qualified type");
+            return Type{};
+        }
+
+        auto VisitElaboratedType(const clang::ElaboratedType *ty, qualifiers quals) -> mlir_type {
+            return with_qualifiers(ty, quals);
+        }
+
+        auto VisitElaboratedType(const clang::ElaboratedType *ty) -> mlir_type {
+            return VisitElaboratedType(ty, ty->desugar().getQualifiers());
+        }
+
+        auto VisitLValueType(clang::QualType ty) -> mlir_type {
             return LValueType::get(&mcontext(), visit(ty));
         }
 
-        Type VisitFunctionType(const clang::FunctionType *ty) {
+        auto VisitFunctionType(const clang::FunctionType *ty) -> mlir_type {
             llvm::SmallVector< Type > args;
 
             if (auto prototype = clang::dyn_cast< clang::FunctionProtoType >(ty)) {
@@ -77,78 +192,106 @@ namespace vast::hl {
             return mlir::FunctionType::get(mctx, args, rty);
         }
 
-        Type VisitBuiltinType(const clang::BuiltinType *ty) {
-            auto *mctx = &mcontext();
+        auto VisitBuiltinType(const clang::BuiltinType *ty) -> mlir_type {
+            return VisitBuiltinType(ty, ty->desugar().getQualifiers());
+        }
 
+        auto VisitBuiltinType(const clang::BuiltinType *ty, qualifiers quals) -> mlir_type {
             if (ty->isVoidType()) {
-                return VoidType::get(mctx);
+                return with_qualifiers< VoidType >(ty, quals);
             }
 
             if (ty->isBooleanType()) {
-                return BoolType::get(mctx);
+                return with_qualifiers< BoolType >(ty, quals);
             }
 
             if (ty->isIntegerType()) {
-                auto u = ty->isUnsignedIntegerType();
-
                 switch (get_integer_kind(ty)) {
-                    case IntegerKind::Char:     return CharType::get(mctx, u);
-                    case IntegerKind::Short:    return ShortType::get(mctx, u);
-                    case IntegerKind::Int:      return IntType::get(mctx, u);
-                    case IntegerKind::Long:     return LongType::get(mctx, u);
-                    case IntegerKind::LongLong: return LongLongType::get(mctx, u);
-                    case IntegerKind::Int128:   return Int128Type::get(mctx, u);
+                    case IntegerKind::Char:     return with_qualifiers< CharType >(ty, quals);
+                    case IntegerKind::Short:    return with_qualifiers< ShortType >(ty, quals);
+                    case IntegerKind::Int:      return with_qualifiers< IntType >(ty, quals);
+                    case IntegerKind::Long:     return with_qualifiers< LongType >(ty, quals);
+                    case IntegerKind::LongLong: return with_qualifiers< LongLongType >(ty, quals);
+                    case IntegerKind::Int128:   return with_qualifiers< Int128Type >(ty, quals);
                 }
             }
 
             if (ty->isFloatingType()) {
                 switch (get_floating_kind(ty)) {
-                    case FloatingKind::Half:       return HalfType::get(mctx);
-                    case FloatingKind::BFloat16:   return BFloat16Type::get(mctx);
-                    case FloatingKind::Float:      return FloatType::get(mctx);
-                    case FloatingKind::Double:     return DoubleType::get(mctx);
-                    case FloatingKind::LongDouble: return LongDoubleType::get(mctx);
-                    case FloatingKind::Float128:   return Float128Type::get(mctx);
+                    case FloatingKind::Half:       return with_qualifiers< HalfType >(ty, quals);
+                    case FloatingKind::BFloat16:   return with_qualifiers< BFloat16Type >(ty, quals);
+                    case FloatingKind::Float:      return with_qualifiers< FloatType >(ty, quals);
+                    case FloatingKind::Double:     return with_qualifiers< DoubleType >(ty, quals);
+                    case FloatingKind::LongDouble: return with_qualifiers< LongDoubleType >(ty, quals);
+                    case FloatingKind::Float128:   return with_qualifiers< Float128Type >(ty, quals);
                 }
             }
 
             return Type{};
         }
 
-        Type VisitPointerType(const clang::PointerType *ty) {
-            return PointerType::get(&mcontext(), visit(ty->getPointeeType()));
+        auto VisitPointerType(const clang::PointerType *ty, qualifiers quals) -> mlir_type {
+            return with_qualifiers(ty, quals);
         }
 
-        Type VisitArrayType(const clang::ArrayType *ty) {
-            auto &ctx = mcontext();
-            auto element_type = visit(ty->getElementType());
-            return ArrayType::get(&ctx, get_size_attr(ty, ctx), element_type);
+        auto VisitPointerType(const clang::PointerType *ty) -> mlir_type {
+            return VisitPointerType(ty, ty->desugar().getQualifiers());
         }
 
-        Type VisitTagType(const clang::TagDecl *decl) {
-            auto name = context().decl_name(decl);
-            return NamedType::get(&mcontext(), name);
+        auto VisitArrayType(const clang::ArrayType *ty, qualifiers quals) -> mlir_type {
+            return with_qualifiers(ty, quals);
         }
 
-        Type VisitRecordType(const clang::RecordType *ty) {
-            return VisitTagType(ty->getDecl());
+        auto VisitArrayType(const clang::ArrayType *ty) -> mlir_type {
+            return VisitArrayType(ty, qualifiers());
         }
 
-        Type VisitEnumType(const clang::EnumType *ty) {
-            return VisitTagType(ty->getDecl());
+        auto VisitRecordType(const clang::RecordType *ty, qualifiers quals) -> mlir_type {
+            return with_qualifiers(ty, quals);
         }
 
-        Type VisitTypedefType(const clang::TypedefType *ty) {
-            auto name = ty->getDecl()->getName();
-            return NamedType::get(&mcontext(), name);
+        auto VisitRecordType(const clang::RecordType *ty) -> mlir_type {
+            return VisitRecordType(ty, ty->desugar().getQualifiers());
         }
 
-        Type VisitFunctionNoProtoType(const clang::FunctionNoProtoType *type) {
-            return VisitFunctionType(type);
+        auto VisitEnumType(const clang::EnumType *ty, qualifiers quals) -> mlir_type {
+            return with_qualifiers(ty, quals);
         }
 
-        Type VisitFunctionProtoType(const clang::FunctionProtoType *type) {
-            return VisitFunctionType(type);
+        auto VisitEnumType(const clang::EnumType *ty) -> mlir_type {
+            return VisitEnumType(ty, ty->desugar().getQualifiers());
+        }
+
+        auto VisitTypedefType(const clang::TypedefType *ty, qualifiers quals) -> mlir_type {
+            return with_qualifiers(ty, quals);
+        }
+
+        auto VisitTypedefType(const clang::TypedefType *ty) -> mlir_type {
+            return VisitTypedefType(ty, ty->desugar().getQualifiers());
+        }
+
+        auto VisitParenType(const clang::ParenType *ty, qualifiers /* quals */) -> mlir_type {
+            return ParenType::get(&mcontext(), visit(ty->getInnerType()));
+        }
+
+        auto VisitParenType(const clang::ParenType *ty) -> mlir_type {
+            return VisitParenType(ty, ty->desugar().getQualifiers());
+        }
+
+        auto VisitFunctionNoProtoType(const clang::FunctionNoProtoType *ty, qualifiers /* quals */) -> mlir_type {
+            return VisitFunctionType(ty);
+        }
+
+        auto VisitFunctionNoProtoType(const clang::FunctionNoProtoType *ty) -> mlir_type {
+            return VisitFunctionNoProtoType(ty, ty->desugar().getQualifiers());
+        }
+
+        auto VisitFunctionProtoType(const clang::FunctionProtoType *ty, qualifiers /* quals */) -> mlir_type {
+            return VisitFunctionType(ty);
+        }
+
+        auto VisitFunctionProtoType(const clang::FunctionProtoType *ty) -> mlir_type {
+            return VisitFunctionProtoType(ty, ty->desugar().getQualifiers());
         }
     };
 
@@ -164,14 +307,14 @@ namespace vast::hl {
         using LensType::context;
         using LensType::acontext;
 
-        bool is_forward_declared(const clang::Type *ty) const {
+        bool is_forward_declared(const clang_type *ty) const {
             if (auto tag = ty->getAsTagDecl()) {
                 return !tag->getDefinition();
             }
             return false;
         }
 
-        Type StoreDataLayout(const clang::Type *orig, Type out) {
+        auto StoreDataLayout(const clang_type *orig, mlir_type out) -> mlir_type {
             if (!orig->isFunctionType() && !is_forward_declared(orig)) {
                 context().data_layout().try_emplace(out, orig, acontext());
             }
@@ -179,11 +322,11 @@ namespace vast::hl {
             return out;
         }
 
-        Type Visit(const clang::Type *ty) {
+        auto Visit(const clang_type *ty) -> mlir_type {
             return StoreDataLayout(ty, Base::Visit(ty));
         }
 
-        Type Visit(clang::QualType ty) {
+        auto Visit(clang::QualType ty) -> mlir_type {
             auto [underlying, quals] = ty.split();
             return StoreDataLayout(underlying, Base::Visit(ty));
         }
