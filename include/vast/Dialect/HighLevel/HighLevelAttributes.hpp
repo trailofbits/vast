994f5f220735c83d10a36639bd192ce81b22da49
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-09-23 16:30:30 +0200

    hl: Add typed constant attributes.

--- include/vast/Dialect/HighLevel/HighLevelAttributes.hpp
+++ include/vast/Dialect/HighLevel/HighLevelAttributes.hpp
@@ -2,7 +2,12 @@
 
 #pragma once
 
+#include "vast/Util/Warnings.hpp"
+
 #include "vast/Dialect/HighLevel/HighLevelDialect.hpp"
+#include "vast/Interfaces/TypedAttrInterface.hpp"
+
+#include "mlir/IR/BuiltinAttributes.h"
 
 #define GET_ATTRDEF_CLASSES
 #include "vast/Dialect/HighLevel/HighLevelAttributes.h.inc"
