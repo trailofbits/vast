b37b6033246ce66ccf8eb03a8cb7bfa9ab8c0dcd
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-09-28 11:17:20 +0200

    test: Add high-level function signature qualifiers test.

---
+++ test/vast/Dialect/HighLevel/qualifiers-j.c
@@ -0,0 +1,11 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | FileCheck %s
+// RUN: vast-cc --ccopts -xc --from-source %s > %t && vast-opt %t | diff -B %t -
+
+// CHECK: hl.var "fp" : !hl.lvalue<!hl.ptr<!hl.paren<(!hl.lvalue<!hl.int>) -> !hl.int>>>
+int (*fp) (int);
+
+// CHECK: hl.var "cfp" : !hl.lvalue<!hl.ptr<!hl.paren<(!hl.lvalue<!hl.int>) -> !hl.int< const >>>>
+const int (*cfp)(int);
+
+// CHECK: hl.var "fpc" : !hl.lvalue<!hl.ptr<!hl.paren<(!hl.lvalue<!hl.int< const >>) -> !hl.int>>>
+int (*fpc)(const int);
