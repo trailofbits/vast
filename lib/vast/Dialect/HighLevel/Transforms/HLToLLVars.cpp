bb523961584792a09517ecc3af347236a9ebfb0c
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-10-07 14:33:55 +0200

    hl:tollvars: Implement lowering of hl vars into ll.

---
+++ lib/vast/Dialect/HighLevel/Transforms/HLToLLVars.cpp
@@ -0,0 +1,125 @@
+// Copyright (c) 2021-present, Trail of Bits, Inc.
+
+#include "vast/Dialect/HighLevel/Passes.hpp"
+
+VAST_RELAX_WARNINGS
+#include <mlir/Analysis/DataLayoutAnalysis.h>
+#include <mlir/IR/PatternMatch.h>
+#include <mlir/Transforms/GreedyPatternRewriteDriver.h>
+#include <mlir/Transforms/DialectConversion.h>
+
+#include <mlir/Conversion/LLVMCommon/Pattern.h>
+VAST_UNRELAX_WARNINGS
+
+#include "PassesDetails.hpp"
+
+#include "vast/Dialect/HighLevel/HighLevelOps.hpp"
+#include "vast/Dialect/LowLevel/LowLevelOps.hpp"
+
+#include "vast/Util/DialectConversion.hpp"
+#include "vast/Util/LLVMTypeConverter.hpp"
+#include "vast/Util/Symbols.hpp"
+
+namespace vast::hl
+{
+    namespace pattern
+    {
+        // Inline the region that is responsible for initialization
+        //  * `rewriter` insert point is invalidated (although documentation of called
+        //    methods does not state it, experimentally it is corrupted)
+        //  * terminator is returned to be used & erased by caller.
+        template< typename T >
+        T inline_init_region(auto src, auto &rewriter)
+        {
+            auto &init_region = src.getInitializer();
+            auto &init_block = init_region.back();
+
+            auto terminator = mlir::dyn_cast< T >(init_block.getTerminator());
+
+            rewriter.inlineRegionBefore(init_region, src->getBlock());
+            auto ip = std::next(mlir::Block::iterator(src));
+            VAST_ASSERT(ip != src->getBlock()->end());
+
+            rewriter.mergeBlockBefore(&init_block, &*ip);
+            return terminator;
+        }
+
+        using TypeConverter = util::tc::LLVMTypeConverter;
+
+        template< typename O >
+        struct BasePattern : mlir::ConvertOpToLLVMPattern< O >
+        {
+            using Base = mlir::ConvertOpToLLVMPattern< O >;
+            using TC_t = util::TypeConverterWrapper< TypeConverter >;
+
+            TypeConverter &tc;
+
+            BasePattern(TypeConverter &tc_) : Base(tc_), tc(tc_) {}
+            TypeConverter &type_converter() const { return tc; }
+        };
+
+        struct vardecl_op : BasePattern< hl::VarDeclOp >
+        {
+            using op_t = hl::VarDeclOp;
+            using Base = BasePattern< op_t >;
+            using Base::Base;
+
+            mlir::LogicalResult matchAndRewrite(
+                        op_t op, typename op_t::Adaptor ops,
+                        mlir::ConversionPatternRewriter &rewriter) const override
+            {
+                auto trg_type = op.getType();
+
+                auto uninit_var = rewriter.create< ll::UninitializedVar >(op.getLoc(),
+                                                                          trg_type);
+
+                auto yield = inline_init_region< hl::ValueYieldOp >(op, rewriter);
+                rewriter.setInsertionPointAfter(yield);
+                auto initialize = rewriter.create< ll::InitializeVar >(
+                        yield.getLoc(),
+                        trg_type,
+                        uninit_var, yield.getResult());
+
+                rewriter.replaceOp(op, {initialize});
+                rewriter.eraseOp(yield);
+
+
+                return mlir::success();
+            }
+        };
+
+
+    } // namespace pattern
+
+    struct HLToLLVarsPass : HLToLLVarsBase< HLToLLVarsPass >
+    {
+        void runOnOperation() override
+        {
+            auto op = this->getOperation();
+            auto &mctx = this->getContext();
+
+            mlir::ConversionTarget trg(mctx);
+            trg.markUnknownOpDynamicallyLegal( [](auto) { return true; } );
+            trg.addIllegalOp< hl::VarDeclOp >();
+
+            const auto &dl_analysis = this->getAnalysis< mlir::DataLayoutAnalysis >();
+
+            mlir::LowerToLLVMOptions llvm_options(&mctx);
+            llvm_options.useBarePtrCallConv = true;
+            pattern::TypeConverter type_converter(&mctx, llvm_options, &dl_analysis);
+
+            mlir::RewritePatternSet patterns(&mctx);
+
+            patterns.add< pattern::vardecl_op >(type_converter);
+
+            if (mlir::failed(mlir::applyPartialConversion(op, trg, std::move(patterns))))
+                return signalPassFailure();
+        }
+    };
+}
+
+
+std::unique_ptr< mlir::Pass > vast::hl::createHLToLLVarsPass()
+{
+    return std::make_unique< vast::hl::HLToLLVarsPass >();
+}
