c7b7b3a73908748ecf9b4576fe0aecf6fce51b8c
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-10-26 15:00:27 +0200

    test:tollvm: Disable until pass that lowers hl.func is working.

--- test/vast/Transform/FromHL/ToLLVM/fn-a.c
+++ test/vast/Transform/FromHL/ToLLVM/fn-a.c
@@ -1,5 +1,7 @@
 // RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-lower-types --vast-hl-to-ll-vars --vast-to-llvm | FileCheck %s
 
+// REQUIRES: funcop-lowering
+
 // CHECK: llvm.func @fn() {
 void fn()
 {
