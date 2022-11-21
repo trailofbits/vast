8c7bf0df8866cd12d81da6fe5cd5d7041b1afcde
Robert Konicar <robert.konicar@trailofbits.com>
2022-11-21 17:39:38 +0100

    test: Add tests for lowering of (u,s,f)div and (u,s,f)rem to ll.

---
+++ test/vast/Conversion/Common/CoreToLLVM/divsigned-a.c
@@ -0,0 +1,10 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-lower-types --vast-hl-to-func --vast-hl-to-ll-vars --vast-core-to-llvm | FileCheck %s
+
+// CHECK: llvm.func @fn(%arg0: i32, %arg1: i32) -> i32 {
+int fn(int arg0, int arg1)
+{
+    // CHECK: [[R:%[0-9]+]] = llvm.sdiv [[V1:%[0-9]+]], [[V2:%[0-9]+]] : i32
+    // CHECK: llvm.return [[R]] : i32
+    return arg0 / arg1;
+}
+// CHECK : }
---
+++ test/vast/Conversion/Common/CoreToLLVM/remunsigned-a.c
@@ -0,0 +1,10 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-lower-types --vast-hl-to-func --vast-hl-to-ll-vars --vast-core-to-llvm | FileCheck %s
+
+// CHECK: llvm.func @fn(%arg0: i32, %arg1: i32) -> i32 {
+unsigned int fn(unsigned int arg0, unsigned int arg1)
+{
+    // CHECK: [[R:%[0-9]+]] = llvm.urem [[V1:%[0-9]+]], [[V2:%[0-9]+]] : i32
+    // CHECK: llvm.return [[R]] : i32
+    return arg0 % arg1;
+}
+// CHECK : }
