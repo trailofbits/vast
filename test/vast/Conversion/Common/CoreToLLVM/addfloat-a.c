721de458958638ee6a6a61d14e82325ded597008
Robert Konicar <robert.konicar@trailofbits.com>
2022-11-21 14:27:06 +0100

    test: Add tests for lowering add/sub/mul to ll.

---
+++ test/vast/Conversion/Common/CoreToLLVM/addfloat-a.c
@@ -0,0 +1,10 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-lower-types --vast-hl-to-func --vast-hl-to-ll-vars --vast-core-to-llvm | FileCheck %s
+
+// CHECK: llvm.func @fn(%arg0: f32, %arg1: f32) -> f32 {
+float fn(float arg0, float arg1)
+{
+    // CHECK: [[R:%[0-9]+]] = llvm.fadd [[V1:%[0-9]+]], [[V2:%[0-9]+]] : f32
+    // CHECK: llvm.return [[R]] : f32
+    return arg0 + arg1;
+}
+// CHECK : }
