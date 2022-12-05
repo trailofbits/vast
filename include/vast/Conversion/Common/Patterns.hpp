21ca18488ec47310995c155ce3f5a970c8a50a78
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-12-05 14:19:00 +0100

    conv: Add pattern mixin.

--- include/vast/Conversion/Common/Patterns.hpp
+++ include/vast/Conversion/Common/Patterns.hpp
@@ -12,10 +12,52 @@ namespace vast {
 
     using pattern_rewriter = mlir::PatternRewriter;
 
-    template< typename op_t >
-    using operation_rewrite_pattern = mlir::OpRewritePattern< op_t >;
+    using conversion_rewriter = mlir::ConversionPatternRewriter;
+
+    template< typename derived_pattern >
+    struct mlir_pattern_mixin {
+
+        using integer_attr_t = mlir::IntegerAttr;
+
+        derived_pattern& self() { return static_cast< derived_pattern& >(this); }
+        const derived_pattern& self() const { return static_cast< const derived_pattern& >(this); }
+
+        template< typename T >
+        mlir_type bitwidth_type() const {
+            auto ctx = self().getContext();
+            return mlir::IntegerType::get(ctx, bits< T >());
+        }
+
+        template< typename T >
+        integer_attr_t interger_attr(T v) const { return integer_attr_t::get(bitwidth_type< T >(), v); }
+
+        integer_attr_t  u8(uint8_t  v) const { return interger_attr(v); }
+        integer_attr_t u16(uint16_t v) const { return interger_attr(v); }
+        integer_attr_t u32(uint32_t v) const { return interger_attr(v); }
+        integer_attr_t u64(uint64_t v) const { return interger_attr(v); }
+
+        integer_attr_t  i8(int8_t  v) const { return interger_attr(v); }
+        integer_attr_t i16(int16_t v) const { return interger_attr(v); }
+        integer_attr_t i32(int32_t v) const { return interger_attr(v); }
+        integer_attr_t i64(int64_t v) const { return interger_attr(v); }
+    };
 
     template< typename op_t >
-    using operation_conversion_pattern = mlir::OpConversionPattern< op_t >;
+    struct operation_rewrite_pattern
+        : mlir_pattern_mixin< operation_rewrite_pattern< op_t > >
+        , mlir::OpRewritePattern< op_t >
+    {
+        using base = mlir::OpRewritePattern< op_t >;
+        using base::base;
+    };
+
+    template< typename op_t >
+    struct operation_conversion_pattern
+        : mlir_pattern_mixin< operation_conversion_pattern< op_t > >
+        , mlir::OpConversionPattern< op_t >
+    {
+        using base = mlir::OpConversionPattern< op_t >;
+        using base::base;
+    };
 
 } // namespace vast
