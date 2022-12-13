eb2f7ca83de859aeb6d6ea24e262fd3f5c5932f0
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-12-13 15:27:37 +0100

    abi: Add function operations.

--- include/vast/Dialect/ABI/ABIOps.hpp
+++ include/vast/Dialect/ABI/ABIOps.hpp
@@ -8,11 +8,15 @@ VAST_RELAX_WARNINGS
 #include <mlir/IR/BuiltinOps.h>
 #include <mlir/IR/BuiltinTypes.h>
 #include <mlir/IR/Dialect.h>
+#include <mlir/IR/FunctionInterfaces.h>
+#include <mlir/IR/FunctionImplementation.h>
 #include <mlir/IR/OperationSupport.h>
 #include <mlir/Interfaces/SideEffectInterfaces.h>
+#include <mlir/Interfaces/CallInterfaces.h>
 VAST_RELAX_WARNINGS
 
 #include "vast/Interfaces/SymbolInterface.hpp"
+#include "vast/Dialect/HighLevel/HighLevelLinkage.hpp"
 
 #define GET_OP_CLASSES
 #include "vast/Dialect/ABI/ABI.h.inc"
