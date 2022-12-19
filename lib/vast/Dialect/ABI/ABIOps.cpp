677f6844f85e3f55f3c7d44ad75643c21b836cc7
Lukas Korencik <lukas.korencik@trailofbits.com>
2022-12-19 18:22:45 +0100

    abi: Extend dialect operations to include prologues and epilogues.

--- lib/vast/Dialect/ABI/ABIOps.cpp
+++ lib/vast/Dialect/ABI/ABIOps.cpp
@@ -23,4 +23,64 @@ namespace vast::abi
     {
         return this->getOperands();
     }
+
+    void build_op_with_region(Builder &bld, State &st, BuilderCallback body_cb)
+    {
+        Builder::InsertionGuard guard(bld);
+        auto body = st.addRegion();
+        if (body_cb.has_value())
+        {
+            bld.createBlock(body);
+            body_cb.value()(bld, st.location);
+        }
+    }
+
+    void PrologueOp::build(Builder &bld, State &st,
+                           mlir::TypeRange types, BuilderCallback body_cb)
+    {
+        st.addTypes(types);
+        return build_op_with_region(bld, st, body_cb);
+    }
+
+    void EpilogueOp::build(Builder &bld, State &st,
+                           mlir::TypeRange types, BuilderCallback body_cb)
+    {
+        st.addTypes(types);
+        return build_op_with_region(bld, st, body_cb);
+    }
+
+    void CallArgsOp::build(Builder &bld, State &st,
+                           mlir::TypeRange types, BuilderCallback body_cb)
+    {
+        st.addTypes(types);
+        return build_op_with_region(bld, st, body_cb);
+    }
+
+    void CallRetsOp::build(Builder &bld, State &st,
+                           mlir::TypeRange types, BuilderCallback body_cb)
+    {
+        st.addTypes(types);
+        return build_op_with_region(bld, st, body_cb);
+    }
+
+    void CallExecutionOp::build(Builder &bld, State &st, llvm::StringRef callee,
+                                mlir::TypeRange types, mlir::ValueRange operands,
+                                BuilderCallback body_cb)
+    {
+        st.addTypes(types);
+        build_op_with_region(bld, st, body_cb);
+        st.addOperands(operands);
+        st.addAttribute("callee", mlir::SymbolRefAttr::get(bld.getContext(), callee));
+    }
+
+    mlir::CallInterfaceCallable CallExecutionOp::getCallableForCallee()
+    {
+        return (*this)->getAttrOfType< mlir::SymbolRefAttr >("callee");
+    }
+
+    mlir::Operation::operand_range CallExecutionOp::getArgOperands()
+    {
+        return this->getOperands();
+    }
+
 } // namespace vast::abi
