c3e3912828c390a3a48909a4bd3d635f4ab7b189
Robert Konicar <robert.konicar@trailofbits.com>
2023-01-05 16:14:52 +0100

    hl: Fix pass for conversion of binary logical operations to core lazy operations.

--- lib/vast/Dialect/HighLevel/Transforms/HLEmitLazyRegions.cpp
+++ lib/vast/Dialect/HighLevel/Transforms/HLEmitLazyRegions.cpp
@@ -27,36 +27,41 @@ namespace vast::hl
 {
     using conversion_rewriter = mlir::ConversionPatternRewriter;
 
+    template< typename source, typename LOp >
+    struct bin_lop_pattern : operation_conversion_pattern< source > {
+        using base = operation_conversion_pattern< source >;
+        using base::base;
+        using adaptor_t = typename source::Adaptor;
+
+        logical_result matchAndRewrite(source op, adaptor_t adaptor, conversion_rewriter &rewriter) const override {
+
+            mlir::BlockAndValueMapping mapper;
+
+            auto lhs_lazy = rewriter.create< core::LazyOp >(op.getLoc(), op.getType());
+            op.getLhs().cloneInto(&lhs_lazy.getLazy(), mapper);
+
+            auto rhs_lazy = rewriter.create< core::LazyOp >(op.getLoc(), op.getType());
+            op.getRhs().cloneInto(&rhs_lazy.getLazy(), mapper);
+
+            auto logical = rewriter.create< LOp >(op.getLoc(), op.getType(), lhs_lazy, rhs_lazy);
+            rewriter.replaceOp(op, { logical });
+            return logical_result::success();
+        }
+
+        static void legalize(conversion_target &target) {
+            target.addLegalOp< core::BinLAndOp, core::BinLOrOp >();
+            target.addIllegalOp< hl::BinLAndOp, hl::BinLOrOp >();
+        }
+    };
+
+    using bin_lop_conversions = util::type_list<
+        bin_lop_pattern< hl::BinLAndOp, core::BinLAndOp >,
+        bin_lop_pattern< hl::BinLOrOp,  core::BinLOrOp >
+    >;
+
     struct HLEmitLazyRegionsPass : ModuleConversionPassMixin< HLEmitLazyRegionsPass, HLEmitLazyRegionsBase > {
+
         using base = ModuleConversionPassMixin< HLEmitLazyRegionsPass, HLEmitLazyRegionsBase>;
-
-        template< typename source, typename LOp >
-        struct bin_lop_pattern : operation_conversion_pattern< source > {
-            using base = operation_conversion_pattern< source >;
-            using base::base;
-            using adaptor_t = typename source::Adaptor;
-
-            logical_result matchAndRewrite(source op, adaptor_t adaptor, conversion_rewriter &rewriter) const override {
-                auto lhs_lazy = rewriter.create< core::LazyOp >(op.getLoc(), op.getType());
-                mlir::BlockAndValueMapping mapper;
-                op.getLhs().cloneInto(&lhs_lazy.getLazy(), mapper);
-                auto rhs_lazy = rewriter.create< core::LazyOp >(op.getLoc(), op.getType());
-                op.getRhs().cloneInto(&rhs_lazy.getLazy(), mapper);
-                auto logical = rewriter.create< LOp >(op.getLoc(), op.getType(), lhs_lazy, rhs_lazy);
-                rewriter.replaceOp(op, { logical });
-                return mlir::success();
-            }
-
-            static void legalize(conversion_target &target) {
-                target.addLegalOp< core::BinLAndOp, core::BinLOrOp >();
-                target.addIllegalOp< hl::BinLAndOp, hl::BinLOrOp >();
-            }
-        };
-
-        using bin_lop_conversions = util::type_list<
-            bin_lop_pattern< hl::BinLAndOp, core::BinLAndOp >,
-            bin_lop_pattern< hl::BinLOrOp, core::BinLOrOp >
-        >;
 
         static conversion_target create_conversion_target(MContext &context) {
             conversion_target target(context);
@@ -64,16 +69,14 @@ namespace vast::hl
             return target;
         }
 
-        static void populate_conversion(rewrite_pattern_set &patterns) {
-            populate_conversions<
-            bin_lop_conversions
+        static void populate_conversions(rewrite_pattern_set &patterns) {
+            populate_conversions_base<
+                bin_lop_conversions
             >(patterns);
         }
-
     };
 
-    std::unique_ptr< mlir::Pass > createHLEmitLazyRegionsPass()
-    {
+    std::unique_ptr< mlir::Pass > createHLEmitLazyRegionsPass() {
         return std::make_unique< HLEmitLazyRegionsPass >();
     }
 
