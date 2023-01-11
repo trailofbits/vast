0445b37f14ff6b000aa905c57b7ebfd2f6426934
Henrich Lauko <henrich.lauko@trailofbits.com>
2023-01-11 13:23:01 +0100

    treewide: Setup project versioning.

---
+++ include/vast/Config/Config.h.cmake
@@ -0,0 +1,21 @@
+#
+# Copyright (c) 2021-present, Trail of Bits, Inc.
+# All rights reserved.
+#
+# This source code is licensed in accordance with the terms specified in
+# the LICENSE file found in the root directory of this source tree.
+#
+
+/* This generated file is for internal use. Do not include it from headers. */
+
+#ifdef VAST_CONFIG_H
+#error config.h can only be included once
+#else
+#define VAST_CONFIG_H
+
+
+#define VAST_VERSION            "${VAST_VERSION}"
+
+#endif
+
+
