681580627ae708a89bdc8a1cc719905c23319d6d
Henrich Lauko <henrich.lauko@trailofbits.com>
2023-01-18 10:52:13 +0100

    cg: Setup hl emit pass.

--- lib/vast/CodeGen/Passes.cpp
+++ lib/vast/CodeGen/Passes.cpp
@@ -2,15 +2,25 @@
 
 #include "vast/Util/Warnings.hpp"
 
+VAST_RELAX_WARNINGS
+#include <mlir/Pass/Pass.h>
+#include <mlir/Pass/PassManager.h>
+VAST_UNRELAX_WARNINGS
+
 #include "vast/CodeGen/Passes.hpp"
 #include "vast/Frontend/Common.hpp"
 
 namespace vast::cg {
 
     logical_result emit_high_level_pass(
-        vast_module /* mod */, mcontext_t */* mctx */, acontext_t */* actx */, bool /* enable_verifier */
+        vast_module mod, mcontext_t *mctx, acontext_t */* actx */, bool enable_verifier
     ) {
-        throw cc::compiler_error("emit_high_level_pass not implemented");
+        mlir::PassManager mgr(mctx);
+
+        // TODO: setup vast intermediate codegen passes
+
+        mgr.enableVerifier(enable_verifier);
+        return mgr.run(mod);
     }
 
 } // namespace vast::cg
