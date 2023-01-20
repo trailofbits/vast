06023b9715a10e15e2d8ac44c10ef0bec17ab5a8
Robert Konicar <robert.konicar@trailofbits.com>
2023-01-20 12:53:50 +0100

    test: Add tests for conversion of core dialect to llvm.

---
+++ test/vast/Conversion/Common/CoreToLLVM/binland-a.c
@@ -0,0 +1,15 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-lower-types --vast-hl-to-func --vast-hl-to-ll-vars --vast-hl-to-lazy-regions --vast-irs-to-llvm --vast-core-to-llvm | FileCheck %s
+
+int fun(int arg1, int arg2) {
+    int res = arg1 && arg2;
+    // CHECK: [[LHS:%[0-9]+]] = llvm.load [[V1:%[0-9]+]]
+    // CHECK: [[Z:%[0-9]+]] = llvm.mlir.constant(0 : i32) : i32
+    // CHECK: [[LR:%[0-9]+]] = llvm.icmp "ne" [[LHS]], [[Z]] : i32
+    // CHECK: llvm.cond_br [[LR]], ^[[TBLOCK:bb[0-9]+]], ^[[RBLOCK:bb[0-9]+]]([[LR]] : i1)
+    // CHECK: ^[[TBLOCK]]: // pred: ^[[PRED:bb[0-9]+]]
+    // CHECK: [[RHS:%[0-9]+]] = llvm.load [[V2:%[0-9]+]]
+    // CHECK: [[RR:%[0-9]+]] = llvm.icmp "ne" [[RHS]], [[Z]]
+    // CHECK: llvm.br ^[[RBLOCK]]([[RR]] : i1)
+    // CHECK: ^[[RBLOCK]]([[V3:%[0-9]+]]: i1): // 2 preds: ^[[PRED]], ^[[TBLOCK]]
+    return res;
+}
