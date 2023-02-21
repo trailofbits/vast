3bb76d8870484e0ad288e8f5fe9ee42b3b0c1378
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-02-21 17:59:36 +0100

    conv: Register conversion from hl control flow to low level.

--- include/vast/Conversion/Passes.hpp
+++ include/vast/Conversion/Passes.hpp
@@ -35,6 +35,8 @@ namespace vast
 
     std::unique_ptr< mlir::Pass > createHLFuncToFuncPass();
 
+    std::unique_ptr< mlir::Pass > createHLToLLCFPass();
+
     // Generate the code for registering passes.
     #define GEN_PASS_REGISTRATION
     #include "vast/Conversion/Passes.h.inc"
