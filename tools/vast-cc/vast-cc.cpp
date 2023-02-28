1ca4b854611c6671b68d666078cf6aac36a985f3
Henrich Lauko <henrich.lauko@trailofbits.com>
2023-02-28 15:27:37 +0100

    cc: Resurrect vast-cc tool.

--- tools/vast-cc/vast-cc.cpp
+++ tools/vast-cc/vast-cc.cpp
@@ -9,10 +9,17 @@ VAST_RELAX_WARNINGS
 #include <clang/Frontend/FrontendAction.h>
 #include <clang/Tooling/Tooling.h>
 #include <clang/Tooling/CommonOptionsParser.h>
+#include <mlir/Tools/mlir-translate/MlirTranslateMain.h>
 VAST_UNRELAX_WARNINGS
 
+#include <vast/Translation/Register.hpp>
+
 int main(int argc, char **argv) try {
-    // vast::registerAllTranslations();
+    vast::registerAllTranslations();
+
+    return failed(
+        mlir::mlirTranslateMain(argc, argv, "VAST Translation Testing Tool")
+    );
 } catch (std::exception &e) {
     llvm::errs() << "error: " << e.what() << '\n';
     std::exit(1);
