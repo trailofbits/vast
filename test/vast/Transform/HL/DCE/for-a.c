a27f2de2dc986d1a1402128d79a6e2835014ee93
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-02-28 17:33:37 +0100

    test:hl:dce: Add tests.

---
+++ test/vast/Transform/HL/DCE/for-a.c
@@ -0,0 +1,17 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt ---vast-hl-dce | FileCheck %s
+
+void fn()
+{
+    for (int i = 0; i < 15; ++i)
+    {
+        // CHECK: } do {
+        // CHECK-NEXT:   [[V1:%[0-9]+]] = hl.ref [[V0:%[0-9]+]] : !hl.lvalue<hl.int>
+        // CHECK-NEXT:   [[V2:%[0-9]+]] = hl.pre.inc [[V1]] : !hl.lvalue<hl.int>
+        // CHECK-NEXT:   hl.break
+        // CHECK-NEXT: }
+        ++i;
+        break;
+        --i;
+        continue;
+    }
+}
---
+++ test/vast/Transform/HL/DCE/if-a.c
@@ -0,0 +1,22 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt ---vast-hl-dce | FileCheck %s
+
+void fn()
+{
+    int x = 12;
+    // CHECK:     hl.cond.yield [[V2:%[0-9]+]] : !hl.int
+    // CHECK-NEXT:   } then {
+    // CHECK-NEXT:     hl.return
+    // CHECK-NEXT:   } else {
+    // CHECK-NEXT:     hl.return
+    // CHECK-NEXT:   }
+    // CHECK-NEXT:   hl.return
+    // CHECK-NEXT: }
+    if (x)
+    {
+        return;
+        ++x;
+    } else {
+        return;
+        int z = 5;
+    }
+}
