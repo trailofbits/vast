2a74309fb0ffd0649bba0cf32a66b449b51ace69
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-02-28 17:33:37 +0100

    test:hl:dce: Add tests.

---
+++ test/vast/Transform/HL/DCE/for-b.c
@@ -0,0 +1,22 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt ---vast-hl-dce | FileCheck %s
+
+void fn()
+{
+    for (int i = 0; i < 15; ++i)
+    {
+        // CHECK:   } then {
+        // CHECK-NEXT:     hl.break
+        // CHECK-NEXT:   }
+        // CHECK-NEXT:   hl.continue
+        // CHECK-NEXT: }
+        // CHECK-NEXT: hl.return
+        if (i == 5)
+        {
+            break;
+            return;
+        }
+
+        continue;
+        return;
+    }
+}
