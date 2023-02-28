ba3bbd71f418c314fc7b6b14c0a1cb940ff26c55
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-02-28 17:31:10 +0100

    test: Remove hl-to-func tests.

--- test/vast/Conversion/FromHL/ToFunc/fn-def-a.c
+++
@@ -1,7 +0,0 @@
-// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-to-func | FileCheck %s
-
-// CHECK: func.func @boo(%arg0: !hl.lvalue<!hl.int>) -> !hl.int {
-int boo( int x )
-{
-    return x;
-}
