4d72aae39af3a845cd434ea5d883476936ca139f
Henrich Lauko <henrich.lauko@trailofbits.com>
2023-03-08 10:25:48 +0100

    cg: Indtroduce scope context wrappers.

---
+++ include/vast/Translation/FunctionScope.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2023, Trail of Bits, Inc.
+
+#pragma once
+
+#include "vast/Translation/ScopeContext.hpp"
+
+namespace vast::cg
+{
+    struct function_scope : scope_context {
+
+    };
+
+} // namespace vast::cg
