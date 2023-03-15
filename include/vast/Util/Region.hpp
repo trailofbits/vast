bc8f704d57a62fa5f886131d88aa6c7ba4adb1af
Henrich Lauko <henrich.lauko@trailofbits.com>
2023-03-15 10:12:01 +0100

    util: Add mlir region utilities.

---
+++ include/vast/Util/Region.hpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2023, Trail of Bits, Inc.
+
+#pragma once
+
+#include "vast/Util/Warnings.hpp"
+
+VAST_RELAX_WARNINGS
+#include <mlir/Support/LogicalResult.h>
+VAST_UNRELAX_WARNINGS
+
+#include "vast/Dialect/HighLevel/HighLevelOps.hpp"
+#include "vast/Util/Common.hpp"
+
+namespace vast {
+
+    Region* build_region(Builder &bld, State &st, BuilderCallback callback);
+
+    hl::ValueYieldOp get_maybe_yielded_value(Region &reg);
+
+    hl::ValueYieldOp get_yielded_value(Region &reg);
+
+    mlir_type get_yielded_type(Region &reg);
+
+    mlir::RegionSuccessor trivial_region_succ(Region *reg);
+
+} // namespace vast
