c1b4c9a52a781f41351d97bcee6a04185711a13e
Henrich Lauko <henrich.lauko@trailofbits.com>
2023-03-17 15:41:56 +0100

    cfg: Fix vast config file.

--- include/vast/Config/config.h.cmake
+++ include/vast/Config/config.h.cmake
@@ -1,20 +1,26 @@
-#
-# Copyright (c) 2021-present, Trail of Bits, Inc.
-# All rights reserved.
-#
-# This source code is licensed in accordance with the terms specified in
-# the LICENSE file found in the root directory of this source tree.
-#
+//
+// Copyright (c) 2021-present, Trail of Bits, Inc.
+// All rights reserved.
+//
+// This source code is licensed in accordance with the terms specified in
+// the LICENSE file found in the root directory of this source tree.
+//
 
 /* This generated file is for internal use. Do not include it from headers. */
+
+#include <string_view>
 
 #ifdef VAST_CONFIG_H
 #error config.h can only be included once
 #else
 #define VAST_CONFIG_H
 
+namespace vast {
 
-#define VAST_VERSION            "${VAST_VERSION}"
+    constexpr std::string_view version = "${VAST_VERSION}";
+
+    constexpr std::string_view bug_report_url = "${BUG_REPORT_URL}";
+} // namespace vast
 
 #endif
 
