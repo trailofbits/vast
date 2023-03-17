479b920257d04325bd7af6c89df972a171474ded
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-03-17 16:06:47 +0100

    test: Update some tests.

--- test/vast/Conversion/FromHL/ToLLCF/for-a.c
+++ test/vast/Conversion/FromHL/ToLLCF/for-a.c
@@ -2,20 +2,14 @@
 
 void fn()
 {
-    // CHECK: "ll.scope" ({
+    // CHECK: "ll.scope"() ({
     // CHECK-NEXT: ll.br ^bb1
 
     // CHECK: ^bb1:  // pred: ^bb0
     // CHECK: "ll.cond_scope_ret"(%5)[^bb2] : (i1) -> ()
 
-    // CHECK: ^bb2:  // pred: bb1
-    // CHECK: "ll.cond_br"(%10)[^bb3, ^bb4] {operand_segment_sizes = dense<[1, 0, 0]> : vector<3xi32>} : (i1) -> ()
-
-    // CHECK: ^bb3:  // pred ^b2
+    // CHECK: ^bb2:  // pred: ^bb1
     // CHECK: "ll.scope_ret"() : () -> ()
-
-    // CHECK: ^bb4:  // pred ^bb2
-    // CHECK: "ll.scope_recurse"() : () -> ()
 
     // CHECK: }) : () -> ()
     for (int i = 0; i < 15; ++i)
