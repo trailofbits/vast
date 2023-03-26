c8a2a01e3e475b20b5df54872d829af4099a3744
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-03-26 15:08:20 +0200

    core: Add soft_terminator trait.

---
+++ lib/vast/Dialect/Core/CoreTraits.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2023-present, Trail of Bits, Inc.
+
+#include "vast/Dialect/Core/CoreTraits.hpp"
+
+namespace vast::core {
+
+} // namespace vast::core
+
+//#define  GET_TYPEDEF_CLASSES
+//#include "vast/Dialect/Core/CoreTraits.cpp.inc"
