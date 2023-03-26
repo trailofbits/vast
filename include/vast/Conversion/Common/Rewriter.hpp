807e6661f248812d6663cf3cc9e2ee9d79da9693
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-03-26 15:01:58 +0200

    conv:common: Introduce wrapper class around op rewriter.

--- include/vast/Conversion/Common/Rewriter.hpp
+++ include/vast/Conversion/Common/Rewriter.hpp
@@ -8,8 +8,59 @@ VAST_RELAX_WARNINGS
 #include "mlir/Transforms/DialectConversion.h"
 VAST_UNRELAX_WARNINGS
 
+#include "vast/Util/Common.hpp"
+
 namespace vast::conv
 {
+    template< typename impl_t >
+    struct rewriter_wrapper_t
+    {
+        using self_t = rewriter_wrapper_t< impl_t >;
+        using underlying_t = impl_t;
+
+        // Sadly, it looks like mlir does not expose a generic way to retrieve guard per
+        // builder type.
+        using guard_t = InsertionGuard;
+
+        underlying_t &bld;
+
+        rewriter_wrapper_t( underlying_t &bld ) : bld( bld ) {}
+
+        auto &operator*() { return bld; }
+        const auto &operator*() const { return bld; }
+
+        auto guarded( auto &&fn )
+        {
+            auto g = guard();
+            return fn();
+        }
+
+        auto guard() { return guard_t( bld ); }
+
+        auto guarded_at_end( mlir::Block *block, auto &&fn )
+        {
+            auto g = guard();
+            bld.setInsertionPointToEnd( block );
+            return fn();
+        }
+
+        template< typename Trg, typename Bld, typename ... Args >
+        auto make_after_op( Operation *op, Args && ... args )
+        {
+            auto g = guard();
+            bld.setInsertionPointAfter( op );
+            return bld.template create< Trg >( std::forward< Args >( args ) ... );
+        }
+
+        template< typename Trg, typename ... Args >
+        auto make_at_end( mlir::Block *block, Args && ... args )
+        {
+            auto g = guard();
+            bld.setInsertionPointToEnd( block );
+            return bld.template create< Trg >( std::forward< Args >( args ) ... );
+        }
+    };
+
     auto guarded( auto &bld, auto &&fn )
     {
         auto g = mlir::OpBuilder::InsertionGuard( bld );
