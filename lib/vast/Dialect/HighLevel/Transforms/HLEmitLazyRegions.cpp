39698694416068fe29ec2272f6e15248979ce2cf
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-04-05 15:32:43 +0200

    conv:hlemitlazy: Support hl::CondOp.

--- lib/vast/Dialect/HighLevel/Transforms/HLEmitLazyRegions.cpp
+++ lib/vast/Dialect/HighLevel/Transforms/HLEmitLazyRegions.cpp
@@ -11,6 +11,7 @@ VAST_RELAX_WARNINGS
 VAST_UNRELAX_WARNINGS
 
 #include "PassesDetails.hpp"
+#include "vast/Conversion/Common/Block.hpp"
 #include "vast/Conversion/Common/Passes.hpp"
 #include "vast/Conversion/Common/Patterns.hpp"
 
@@ -22,50 +23,112 @@ VAST_UNRELAX_WARNINGS
 
 #include "vast/Util/TypeList.hpp"
 #include "vast/Util/DialectConversion.hpp"
+#include "vast/Util/Terminator.hpp"
 
 namespace vast::hl
 {
     using conversion_rewriter = mlir::ConversionPatternRewriter;
 
+    struct lazy_utils
+    {
+        static auto region_value_yield_type(Region &region)
+        {
+            return region.back().back().getOperand(0).getType();
+        }
+
+        static auto lazy_side(auto &&rewriter, mlir::Location loc, mlir::Region &side)
+        {
+            // Passed by val, we are expecting only mlir lightweight objects.
+            auto mk_lazy_op = [&](auto ... args)
+            {
+                return rewriter.template create< core::LazyOp >( loc, args ... );
+            };
+
+            auto lazy_op = [&]
+            {
+                if (!terminator_t< hl::ValueYieldOp >::get(side.front()))
+                {
+                    return mk_lazy_op(mlir::NoneType::get(rewriter.getContext()));
+                }
+                return mk_lazy_op(region_value_yield_type(side));
+            }();
+
+            auto &lazy_region = lazy_op.getLazy();
+            rewriter.inlineRegionBefore(side, lazy_region, lazy_region.end());
+            return lazy_op;
+        }
+    };
+
     template< typename source, typename LOp >
-    struct bin_lop_pattern : operation_conversion_pattern< source > {
+    struct bin_lop_pattern : operation_conversion_pattern< source >,
+                             lazy_utils
+    {
         using base = operation_conversion_pattern< source >;
         using base::base;
         using adaptor_t = typename source::Adaptor;
 
-        auto region_value_yield_type(Region &region) const {
-            return region.back().back().getOperand(0).getType();
-        }
-
         logical_result matchAndRewrite(
             source op, adaptor_t adaptor, conversion_rewriter &rewriter) const override
         {
-            auto lazy_side = [&] (Region &side) {
-                auto lazy_op = rewriter.create< core::LazyOp >(
-                    op.getLoc(), region_value_yield_type(side)
-                );
-                auto &lazy_region = lazy_op.getLazy();
-                rewriter.inlineRegionBefore(side, lazy_region, lazy_region.end());
-                return lazy_op;
-            };
+            auto lhs_lazy = lazy_side(rewriter, op.getLoc(), op.getLhs());
+            auto rhs_lazy = lazy_side(rewriter, op.getLoc(), op.getRhs());
 
-            auto lhs_lazy = lazy_side(op.getLhs());
-            auto rhs_lazy = lazy_side(op.getRhs());
-
-            auto logical = rewriter.create< LOp >(op.getLoc(), op.getType(), lhs_lazy, rhs_lazy);
+            auto logical = rewriter.create< LOp >(op.getLoc(), op.getType(),
+                                                  lhs_lazy, rhs_lazy);
             rewriter.replaceOp(op, { logical });
             return logical_result::success();
         }
 
         static void legalize(conversion_target &target) {
-            target.addLegalOp< core::BinLAndOp, core::BinLOrOp >();
-            target.addIllegalOp< hl::BinLAndOp, hl::BinLOrOp >();
+            target.addLegalOp< LOp >();
+            target.addIllegalOp< source >();
+        }
+    };
+
+    struct cond_op : operation_conversion_pattern< hl::CondOp >,
+                     lazy_utils
+    {
+        using source = hl::CondOp;
+        using base = operation_conversion_pattern< source >;
+        using base::base;
+        using adaptor_t = typename source::Adaptor;
+
+        logical_result matchAndRewrite(source op, adaptor_t adaptor,
+                                       conversion_rewriter &rewriter) const override
+        {
+            auto &cond_block = op.getCondRegion().front();
+            VAST_PATTERN_CHECK(conv::size(op.getCondRegion()) == 1,
+                               "Unsupported shape of cond region of hl::CondOp");
+
+            auto yield = terminator_t< hl::CondYieldOp >::get(cond_block);
+            VAST_PATTERN_CHECK(yield, "Was not able to retrieve cond yield, {0}.", op);
+
+            rewriter.mergeBlockBefore(&cond_block, op, llvm::None);
+
+            auto then_region = lazy_side(rewriter, op.getLoc(), op.getThenRegion());
+            auto else_region = lazy_side(rewriter, op.getLoc(), op.getElseRegion());
+
+            auto yielded_val = yield.op().getResult();
+            auto select = rewriter.create< core::SelectOp >(
+                    op.getLoc(),
+                    yielded_val.getType(), yielded_val,
+                    then_region, else_region);
+
+            rewriter.eraseOp(yield.op());
+            rewriter.replaceOp(op, select.getResults());
+            return mlir::success();
+        }
+
+        static void legalize(conversion_target &target) {
+            target.addLegalOp< core::SelectOp >();
+            target.addIllegalOp< hl::CondOp >();
         }
     };
 
     using bin_lop_conversions = util::type_list<
         bin_lop_pattern< hl::BinLAndOp, core::BinLAndOp >,
-        bin_lop_pattern< hl::BinLOrOp,  core::BinLOrOp >
+        bin_lop_pattern< hl::BinLOrOp,  core::BinLOrOp >,
+        cond_op
     >;
 
     struct HLEmitLazyRegionsPass
