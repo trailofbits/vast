e5120942d92a3f7b9ade09332a9c3da411c79850
Robert Konicar <robert.konicar@trailofbits.com>
2023-04-13 11:53:47 +0200

    test: Add tests for generation of global variables in frontend.

---
+++ test/vast/Dialect/HighLevel/glob-front-a.c
@@ -0,0 +1,8 @@
+// RUN: vast-front %s -vast-emit-high-level -o - | FileCheck %s
+
+// CHECK: hl.var "NUM"
+// CHECK: hl.value.yield
+short NUM = 10;
+//CHECK: hl.globref "NUM"
+//CHECK: hl.assign
+int main() {NUM = 11;}
