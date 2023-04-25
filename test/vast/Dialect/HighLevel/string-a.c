f76917d87e7ef62c1976847f92549fb323e7e737
Robert Konicar <robert.konicar@trailofbits.com>
2023-04-25 15:31:05 +0200

    test: Fix types in tests containing string literals.

--- test/vast/Dialect/HighLevel/string-a.c
+++ test/vast/Dialect/HighLevel/string-a.c
@@ -2,6 +2,6 @@
 // RUN: vast-cc --ccopts -xc --from-source %s > %t && vast-opt %t | diff -B %t -
 
 // CHECK: hl.var "gstr" : !hl.lvalue<!hl.ptr<!hl.char< const >>>
-// CHECK:   hl.const #hl.str<"global"> : !hl.array<7, !hl.char>
-// CHECK:   ArrayToPointerDecay : !hl.array<7, !hl.char> -> !hl.ptr<!hl.char>
+// CHECK:   hl.const #hl.str<"global"> : !hl.lvalue<!hl.array<7, !hl.char>>
+// CHECK:   ArrayToPointerDecay : !hl.lvalue<!hl.array<7, !hl.char>> -> !hl.lvalue<!hl.ptr<!hl.char>>
 const char *gstr = "global";
