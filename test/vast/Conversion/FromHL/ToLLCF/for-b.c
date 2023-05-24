01932e9752c295bd45e9bc2ec8bb7d7cda75bedc
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-05-24 16:41:23 +0200

    test:conv: Add some new tests of lowering to various ll forms.

---
+++ test/vast/Conversion/FromHL/ToLLCF/for-b.c
@@ -0,0 +1,26 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-dce --vast-hl-lower-types --vast-hl-to-ll-cf | FileCheck %s
+
+int fn()
+{
+    // CHECK: hl.scope {
+    // CHECK:   ll.scope {
+    // CHECK:     ll.br ^bb2
+    // CHECK:   ^bb1:  // pred: ^bb4
+    // CHECK:     ll.br ^bb2
+    // CHECK:   ^bb2:  // 2 preds: ^bb0, ^bb1
+    // CHECK:     ll.cond_scope_ret [[V8:%[0-9]+]] : i1, ^bb3
+    // CHECK:   ^bb3:  // pred: ^bb2
+    // CHECK:     ll.cond_br [[V13:%[0-9]+]] : i1, ^bb5, ^bb4
+    // CHECK:   ^bb4:  // pred: ^bb3
+    // CHECK:     ll.br ^bb1
+    // CHECK:   ^bb5:  // pred: ^bb3
+    // CHECK:     ll.scope_ret
+    for ( int i = 0; i < 5; ++i )
+        if ( i == 5 )
+            break;
+        else
+            continue;
+    // CHECK:   }
+    // CHECK: }
+    return 43;
+}
---
+++ test/vast/Conversion/FromHL/ToLLVars/vars-a.c
@@ -0,0 +1,12 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-dce --vast-hl-lower-types --vast-hl-to-ll-vars | FileCheck %s
+
+int main()
+{
+    // CHECK: [[V0:%[0-9]+]] = ll.uninitialized_var : !hl.lvalue<si32>
+    int a;
+
+    // CHECK: [[V1:%[0-9]+]] = ll.uninitialized_var : !hl.lvalue<si32>
+    // CHECK: [[V2:%[0-9]+]] = hl.const #hl.integer<1> : si32
+    // CHECK: [[V3:%[0-9]+]] = ll.initialize [[V1]], [[V2]] : (!hl.lvalue<si32>, si32) -> !hl.lvalue<si32>
+    int b = 1;
+}
