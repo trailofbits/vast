9c18278ea142bfe19428709fdf1661081c5d2b84
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-06-06 17:56:25 +0200

    conv:typedefs: Implement the typedef resolution pass.

--- lib/vast/Conversion/FromHL/ResolveTypeDefs.cpp
+++ lib/vast/Conversion/FromHL/ResolveTypeDefs.cpp
@@ -25,22 +25,98 @@ VAST_UNRELAX_WARNINGS
 
 namespace vast::conv
 {
-    struct ResolveTypeDefs : ModuleConversionPassMixin< ResolveTypeDefs, ResolveTypeDefsBase >
+    namespace
     {
-        using base = ModuleConversionPassMixin< ResolveTypeDefs, ResolveTypeDefsBase >;
-        using config_t = typename base::config_t;
+        using type_map = std::map< mlir_type, mlir_type >;
 
-        static auto create_conversion_target( mcontext_t &mctx )
+        struct typedef_resolver
         {
-            mlir::ConversionTarget trg(mctx);
-            trg.markUnknownOpDynamicallyLegal([](auto){ return true; });
-            return trg;
+            vast_module mod;
+
+            mlir_type nested_type(mlir_type type)
+            {
+                return hl::getBottomTypedefType(type, mod);
+            }
+
+            auto get_mapper()
+            {
+                return [=](hl::ElaboratedType type) -> std::optional< mlir_type >
+                {
+                    return { nested_type(type) };
+                };
+            }
+
+            auto do_conversion(mlir_type type)
+            {
+                if (auto subelements = mlir::dyn_cast< mlir::SubElementTypeInterface >(type))
+                {
+                    return subelements.replaceSubElements(get_mapper());
+                }
+                return nested_type(type);
+            }
+
+            void fixup_entry_block(mlir::Block *block)
+            {
+                for (std::size_t i = 0; i < block->getNumArguments(); ++i)
+                {
+                    auto arg = block->getArgument(i);
+                    arg.setType(do_conversion(arg.getType()));
+                }
+            }
+
+            auto get_process()
+            {
+                return [=](mlir::Operation *nested)
+                {
+                    // Special case for functions, it may be that we can unify it with
+                    // the generic one.
+                    if (auto fn = mlir::dyn_cast< hl::FuncOp >(nested))
+                    {
+                        auto type = fn.getFunctionType();
+                        fn.setType(do_conversion(type));
+                        if (nested->getNumRegions() != 0)
+                            fixup_entry_block(&*nested->getRegions().begin()->begin());
+
+                        return;
+                    }
+
+                    // Generic conversion of only result types.
+                    std::vector< mlir_type > new_types;
+                    for (auto res : nested->getResultTypes())
+                    {
+                        auto trg_type = do_conversion(res);
+                        new_types.push_back(trg_type);
+                    }
+
+
+
+                    if (nested->getNumRegions() != 0)
+                        fixup_entry_block(&*nested->getRegions().begin()->begin());
+
+                    for (std::size_t i = 0; i < new_types.size(); ++i)
+                        nested->getResult(i).setType(new_types[i]);
+                };
+            }
+
+            void run(mlir::Operation *op)
+            {
+                op->walk(get_process());
+            }
+        };
+
+    } // namespace
+
+    struct ResolveTypeDefs : ResolveTypeDefsBase< ResolveTypeDefs >
+    {
+        using base = ResolveTypeDefsBase< ResolveTypeDefsBase >;
+
+        void runOnOperation() override
+        {
+            auto op = this->getOperation();
+            typedef_resolver{ op }.run(op.getOperation());
+
         }
 
-        static void populate_conversions(config_t &config)
-        {
-            // TODO: fill;
-        }
     };
 
 } // namespace vast::conv
