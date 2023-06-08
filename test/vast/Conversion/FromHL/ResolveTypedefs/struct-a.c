0edbbc36b8d431c291e83d8ce0d414a6411a9a3d
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-06-08 02:09:03 +0200

    test:conv: Add tests for typedef resolution pass.

---
+++ test/vast/Conversion/FromHL/ResolveTypedefs/struct-a.c
@@ -0,0 +1,13 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-dce --vast-hl-lower-types --vast-hl-resolve-typedefs | FileCheck %s
+
+typedef int INT;
+
+struct X
+{
+    // CHECK: hl.field "a" : si32
+    INT a;
+};
+
+// CHECK: [[V1:%[0-9]+]] = hl.const #hl.integer<0> : si32
+// CHECK: [[V2:%[0-9]+]] = hl.initlist [[V1]] : (si32) -> !hl.elaborated<!hl.record<"X">>
+struct X x = { 0 };
