4351c90016bac44df7833f6fda7c0f57284fd95e
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-06-08 02:09:03 +0200

    test:conv: Add tests for typedef resolution pass.

---
+++ test/vast/Conversion/FromHL/ResolveTypedefs/func-a.c
@@ -0,0 +1,6 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-dce --vast-hl-lower-types --vast-hl-resolve-typedefs | FileCheck %s
+
+typedef int INT;
+
+// CHECK: hl.func external @foo (!hl.lvalue<si32>) -> si32 attributes {sym_visibility = "private"}
+INT foo(INT);
---
+++ test/vast/Conversion/FromHL/ResolveTypedefs/var-a.c
@@ -0,0 +1,16 @@
+// RUN: vast-cc --ccopts -xc --from-source %s | vast-opt --vast-hl-dce --vast-hl-lower-types --vast-hl-resolve-typedefs | FileCheck %s
+
+typedef int INT;
+
+// CHECK: {{.* = hl.var "a".*!hl.lvalue<si32>.*}}
+INT a = 0;
+
+typedef INT IINT;
+
+// CHECK: {{.* = hl.var "b".*!hl.lvalue<si32>.*}}
+IINT b = 0;
+
+typedef IINT IIINT;
+
+// CHECK: {{.* = hl.var "c".*!hl.lvalue<si32>.*}}
+IIINT c = 0;
