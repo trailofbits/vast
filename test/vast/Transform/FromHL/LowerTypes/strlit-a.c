2ec2affa73f1f66fec0ac367c7893568bbded4ef
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-06-08 02:44:11 +0200

    test:hl: Test type lowering in hl.strlit.

---
+++ test/vast/Transform/FromHL/LowerTypes/strlit-a.c
@@ -0,0 +1,8 @@
+// RUN: vast-cc --from-source %s | vast-opt --vast-hl-lower-types | FileCheck %s
+
+// CHECK-LABEL: hl.func external @main () -> si32
+int main()
+{
+    // CHECK: [[V2:%[0-9]+]] = hl.const #hl.strlit<"hello"> : !hl.lvalue<memref<6xsi8>>
+    const char *hello = "hello";
+}
