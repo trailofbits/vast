0710a92656493e71a0317ab8a4720831f2944e65
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2023-06-22 14:47:58 +0200

    test:hl: Add some tests for C++.

---
+++ test/vast/Dialect/HighLevel/class-a.cpp
@@ -0,0 +1,19 @@
+// RUN: vast-cc --from-source %s | FileCheck %s
+// RUN: vast-cc --from-source %s > %t && vast-opt %t | diff -B %t -
+
+// CHECK: hl.class "A" :
+class A {};
+
+// CHECK: hl.class "B" :
+class B {};
+
+// CHECK: hl.class "C" :
+class C : public A, protected virtual B {
+    // CHECK: hl.base !hl.elaborated<!hl.record<"A">> public
+    // CHECK: hl.base !hl.elaborated<!hl.record<"B">> protected virtual
+
+// CHECK: hl.access public
+public:
+    // CHECK:  hl.field "x" : !hl.int
+    int x;
+};
