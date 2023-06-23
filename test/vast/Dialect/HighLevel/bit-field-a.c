e0b585529c8e425a25de52ef90ddb71f633f7a59
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2023-06-23 08:52:33 +0200

    test:hl: Make anonymous names in tests not hardcoded.

--- test/vast/Dialect/HighLevel/bit-field-a.c
+++ test/vast/Dialect/HighLevel/bit-field-a.c
@@ -9,7 +9,7 @@ struct S1 {
 
 struct S2 {
     // CHECK: hl.field "b1" bw 5 : !hl.int< unsigned >
-    // CHECK: hl.field "anonymous[618]" bw 11 : !hl.int< unsigned >
+    // CHECK: hl.field "anonymous[{{[0-9]+}}]" bw 11 : !hl.int< unsigned >
     // CHECK: hl.field "b2" bw 6 : !hl.int< unsigned >
     // CHECK: hl.field "b3" bw 2 : !hl.int< unsigned >
     unsigned b1 : 5, : 11, b2 : 6, b3 : 2;
@@ -17,7 +17,7 @@ struct S2 {
 
 struct S3 {
     // CHECK: hl.field "b1" bw 5 : !hl.int< unsigned >
-    // CHECK: hl.field "anonymous[722]" bw 0 : !hl.int< unsigned >
+    // CHECK: hl.field "anonymous[{{[0-9]+}}]" bw 0 : !hl.int< unsigned >
     // CHECK: hl.field "b2" bw 6 : !hl.int< unsigned >
     // CHECK: hl.field "b3" bw 15 : !hl.int< unsigned >
     unsigned b1 : 5;
--- test/vast/Dialect/HighLevel/struct-c.c
+++ test/vast/Dialect/HighLevel/struct-c.c
@@ -1,10 +1,10 @@
 // RUN: vast-cc --ccopts -xc --from-source %s | FileCheck %s
 // RUN: vast-cc --ccopts -xc --from-source %s > %t && vast-opt %t | diff -B %t -
 
-// CHECK: hl.struct "anonymous[512]" : {
+// CHECK: hl.struct "[[N:anonymous\[[0-9]+\]]]" : {
 // CHECK:  hl.field "data" : !hl.int
 // CHECK: }
-// CHECK: hl.var "named" : !hl.lvalue<!hl.elaborated<!hl.record<"anonymous[512]">>>
+// CHECK: hl.var "named" : !hl.lvalue<!hl.elaborated<!hl.record<"[[N]]">>>
 struct {
   int data;
 } named;
