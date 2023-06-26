5a85f392524e38642fc1de13270f701a0f32ae21
Robert Konicar <robert.konicar@trailofbits.com>
2023-06-26 15:57:00 +0200

    cg: Remove no longer required splicing utils.

--- include/vast/Translation/Util.hpp
+++ include/vast/Translation/Util.hpp
@@ -22,47 +22,4 @@ namespace vast::cg
             }
         }
     }
-
-    inline void splice_trailing_scopes(mlir::Region::BlockListType &blocks) {
-        auto has_trailing_scope = [&] {
-            if (blocks.empty())
-                return false;
-            auto &last_block = blocks.back();
-            if (last_block.empty())
-                return false;
-            return mlir::isa< hl::ScopeOp >(last_block.back());
-        };
-
-        while (has_trailing_scope()) {
-            auto &last_block = blocks.back();
-
-            auto scope  = mlir::cast< hl::ScopeOp >(last_block.back());
-            auto parent = scope.getBody().getParentRegion();
-            scope->remove();
-
-            auto &prev = parent->getBlocks().back();
-
-            mlir::IRMapping mapping;
-            scope.getBody().cloneInto(parent, mapping);
-
-            auto next = prev.getNextNode();
-
-            auto &ops = last_block.getOperations();
-            ops.splice(ops.end(), next->getOperations());
-
-            next->erase();
-            scope.erase();
-        }
-    }
-
-    inline void splice_trailing_scopes(hl::FuncOp &fn) {
-        if (fn.empty())
-            return;
-        splice_trailing_scopes(fn.getBlocks());
-    }
-
-    inline void splice_trailing_scopes(mlir::Region &reg) {
-        splice_trailing_scopes(reg.getBlocks());
-    }
-
 } // namespace vast::cg
