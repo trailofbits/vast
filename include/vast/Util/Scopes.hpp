b9d1f2ad333d4afdd4473796cbe766721fa1d21b
Robert Konicar <robert.konicar@trailofbits.com>
2023-06-27 19:24:20 +0200

    util: Remove terminator check from trailing scope detection.

--- include/vast/Util/Scopes.hpp
+++ include/vast/Util/Scopes.hpp
@@ -6,19 +6,13 @@
 
 namespace vast
 {
-    bool is_trailing_scope(operation op) {
+    inline bool is_trailing_scope(operation op) {
         if (!mlir::isa< hl::ScopeOp >(op))
             return false;
         if (auto parent = op->getParentRegion()) {
             if(parent->hasOneBlock()) {
                 auto &block = parent->back();
                 auto last = --block.end();
-
-                // vast-cc adds UnreachableOp if it doesn't see a proper terminator
-                // But the real terminator might be enclosed in the scope
-                if (mlir::isa< hl::UnreachableOp >(block.back()))
-                        --last;
-                // check if we have only the scope operation in the block
                 return block.begin() == last;
             }
         }
