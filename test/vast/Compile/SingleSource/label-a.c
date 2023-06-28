f2b9133e9ffcaeb2d0b36fad219af59a783ab15c
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-06-29 01:46:01 +0200

    test:compile: Add more basic single source tests.

---
+++ test/vast/Compile/SingleSource/label-a.c
@@ -0,0 +1,11 @@
+// RUN: vast-front -o %t %s && (%t; test $? -eq 2)
+// RUN: vast-front -o %t %s && (%t 1 2 3; test $? -eq 4)
+
+int main(int argc, char **argv)
+{
+    int x = 1;
+begin:
+    x += argc;
+end:
+    return x;
+}
