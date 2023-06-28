8bf5498ee0b95ec361f3ddd3d76839e3c5070404
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2023-06-28 12:27:50 +0200

    test:hl: Purge untested stuff.

--- test/vast/Dialect/HighLevel/class-a.cpp
+++ test/vast/Dialect/HighLevel/class-a.cpp
@@ -2,11 +2,7 @@
 // RUN: vast-cc --from-source %s > %t && vast-opt %t | diff -B %t -
 
 // CHECK: hl.class "A" :
-class A {
-public:
-    virtual int get_x() = 0;
-    virtual ~A() = default;
-};
+class A {};
 
 // CHECK: hl.class "B" :
 class B {};
@@ -16,10 +12,6 @@ class C : public A, protected virtual B {
     // CHECK: hl.base !hl.elaborated<!hl.record<"A">> public
     // CHECK: hl.base !hl.elaborated<!hl.record<"B">> protected virtual
 
-    friend class D;
-    friend int foobar();
-    friend int A::get_x();
-
 // CHECK: hl.access public
 public:
     // CHECK: hl.field "x" : !hl.int
@@ -27,30 +19,4 @@ public:
 
     // CHECK: %0 = hl.var "C::y" sc_static : !hl.lvalue<!hl.int>
     static int y;
-
-    int get_x() override;
-
-    static int get_y() {
-        return y;
-    }
-
-    int operator++(void) {
-        return x;
-    }
-
-    int operator++(int) {
-        return x;
-    }
-
-    C(int x);
-
-    ~C();
 };
-
-C::C(int x) : x(x) {}
-
-C::~C() {}
-
-int C::get_x() {
-    return x;
-}
