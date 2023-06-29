2739225d814e2fcea37c52a039cab160c6134857
Robert Konicar <robert.konicar@trailofbits.com>
2023-06-29 12:40:01 +0200

    test: Fix tests expecting lvalue after array to ptr decay.

--- test/vast/Dialect/HighLevel/array-d.c
+++ test/vast/Dialect/HighLevel/array-d.c
@@ -8,9 +8,9 @@ int main() {
     int arr[3];
     // CHECK: hl.var "v0" : !hl.lvalue<!hl.int> = {
     // CHECK:   [[V1:%[0-9]+]] = hl.ref [[ARR]] : !hl.lvalue<!hl.array<3, !hl.int>>
-    // CHECK:   [[V2:%[0-9]+]] = hl.implicit_cast [[V1]] ArrayToPointerDecay : !hl.lvalue<!hl.array<3, !hl.int>> -> !hl.lvalue<!hl.ptr<!hl.int>>
+    // CHECK:   [[V2:%[0-9]+]] = hl.implicit_cast [[V1]] ArrayToPointerDecay : !hl.lvalue<!hl.array<3, !hl.int>> -> !hl.ptr<!hl.int>
     // CHECK:   [[V3:%[0-9]+]] = hl.const #hl.integer<0> : !hl.int
-    // CHECK:   hl.subscript [[V2]] at {{.*}}[[V3]] : !hl.int] : !hl.lvalue<!hl.ptr<!hl.int>> -> !hl.lvalue<!hl.int>
+    // CHECK:   hl.subscript [[V2]] at {{.*}}[[V3]] : !hl.int] : !hl.ptr<!hl.int> -> !hl.lvalue<!hl.int>
     // CHECK: }
     int v0 = arr[0];
 
@@ -18,10 +18,10 @@ int main() {
     int idx = 2;
     // CHECK: hl.var "v2" : !hl.lvalue<!hl.int> = {
     // CHECK:   [[V1:%[0-9]+]] = hl.ref [[ARR]] : !hl.lvalue<!hl.array<3, !hl.int>>
-    // CHECK:   [[V2:%[0-9]+]] = hl.implicit_cast [[V1]] ArrayToPointerDecay : !hl.lvalue<!hl.array<3, !hl.int>> -> !hl.lvalue<!hl.ptr<!hl.int>>
+    // CHECK:   [[V2:%[0-9]+]] = hl.implicit_cast [[V1]] ArrayToPointerDecay : !hl.lvalue<!hl.array<3, !hl.int>> -> !hl.ptr<!hl.int>
     // CHECK:   [[V3:%[0-9]+]] = hl.ref [[IDX]] : !hl.lvalue<!hl.int>
     // CHECK:   [[V4:%[0-9]+]] = hl.implicit_cast [[V3]] LValueToRValue : !hl.lvalue<!hl.int> -> !hl.int
-    // CHECK:   hl.subscript [[V2]] at {{.*}}[[V4]] : !hl.int] : !hl.lvalue<!hl.ptr<!hl.int>> -> !hl.lvalue<!hl.int>
+    // CHECK:   hl.subscript [[V2]] at {{.*}}[[V4]] : !hl.int] : !hl.ptr<!hl.int> -> !hl.lvalue<!hl.int>
     // CHECK: }
     int v2 = arr[idx];
 }
