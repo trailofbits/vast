1176a72f532a069777c911e005fb85f33c2587fb
Robert Konicar <robert.konicar@trailofbits.com>
2023-07-12 12:04:29 +0200

    test:conv: Add test for plus/minus op llvm conversion.

---
+++ test/vast/Conversion/Common/IRsToLLVM/signs.c
@@ -0,0 +1,14 @@
+// RUN: vast-front -vast-emit-mlir=llvm -o - %s | FileCheck %s
+
+int main() {
+    int a = 5;
+    // CHECK: {{.* = llvm.load.*}}
+    // CHECK-NEXT: {{.* = llvm.mlir.constant.*}}
+    // CHECK-NEXT: {{.* = llvm.sub.*}}
+    // CHECK-NEXT: {{.*llvm.store.*}}
+    // CHECK: {{.* = llvm.load.*}}
+    // CHECK-NEXT: {{.*llvm.store.*}}
+    int b = -a;
+    int c = +a;
+    return 0;
+}
