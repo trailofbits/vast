508e35b29a5e1051a82c06ab418492977a39ac41
Marek Surovič <marek.surovic@trailofbits.com>
2023-07-31 19:22:54 +0200

    tower: Create old-to-new op mapping in `apply`.

--- lib/vast/Tower/Tower.cpp
+++ lib/vast/Tower/Tower.cpp
@@ -2,12 +2,31 @@
 
 #include "vast/Tower/Tower.hpp"
 
-#include "mlir/Pass/Pass.h"
 #include "mlir/Pass/PassManager.h"
+#include "vast/Util/Common.hpp"
 
 namespace vast::tower
 {
+    static auto insert_op_ptr_loc(mlir::Operation *op) -> void {
+        auto ctx = op->getContext();
+        auto ol  = mlir::OpaqueLoc::get< mlir::Operation  *>(op, ctx);
+        op->setLoc(mlir::FusedLoc::get({ op->getLoc() }, ol, ctx));
+    }
+
+    static auto get_op_ptr_loc(mlir::Operation *op) -> mlir::Operation * {
+        auto fl = mlir::cast< mlir::FusedLoc >(op->getLoc());
+        auto ol = mlir::cast< mlir::OpaqueLoc >(fl.getMetadata());
+        return mlir::OpaqueLoc::getUnderlyingLocation< mlir::Operation * >(ol);
+    }
+
+    static auto remove_op_ptr_loc(mlir::Operation *op) -> void {
+        auto fl = mlir::cast< mlir::FusedLoc >(op->getLoc());
+        op->setLoc(fl.getLocations().front());
+    }
+
     auto manager_t::apply(handle_t handle, pass_ptr_t pass) -> handle_t {
+        handle.mod.walk(insert_op_ptr_loc);
+
         _modules.emplace_back(mlir::cast< vast_module >(handle.mod->clone()));
 
         auto id  = _modules.size() - 1;
@@ -16,9 +35,15 @@ namespace vast::tower
         mlir::PassManager pm(&_ctx);
         pm.addPass(std::move(pass));
 
-        auto run_result = pm.run(_modules.back().get());
-        VAST_CHECK(mlir::succeeded(run_result), "Some pass in apply() failed");
+        VAST_CHECK(mlir::succeeded(pm.run(mod)), "Some pass in apply() failed");
 
-        return { id, mod };
+        mlir::IRMapping prev;
+
+        mod.walk([&prev](mlir::Operation *op) {
+            prev.map(op, get_op_ptr_loc(op));
+            remove_op_ptr_loc(op);
+        });
+
+        return { id, mod, prev };
     }
 } // namespace vast::tower
