78749684cb9b9215f64a3058d9ef0fa91dcd0766
Marek Surovič <marek.surovic@trailofbits.com>
2023-08-02 16:36:44 +0200

    tower: Provide query API instead of `IRMapping`.

--- lib/vast/Tower/Tower.cpp
+++ lib/vast/Tower/Tower.cpp
@@ -2,48 +2,22 @@
 
 #include "vast/Tower/Tower.hpp"
 
-#include "mlir/Pass/PassManager.h"
-#include "vast/Util/Common.hpp"
-
 namespace vast::tower
 {
-    static auto insert_op_ptr_loc(mlir::Operation *op) -> void {
+    auto default_loc_rewriter_t::insert(mlir::Operation *op) -> void {
         auto ctx = op->getContext();
         auto ol  = mlir::OpaqueLoc::get< mlir::Operation  *>(op, ctx);
         op->setLoc(mlir::FusedLoc::get({ op->getLoc() }, ol, ctx));
     }
 
-    static auto get_op_ptr_loc(mlir::Operation *op) -> mlir::Operation * {
-        auto fl = mlir::cast< mlir::FusedLoc >(op->getLoc());
-        auto ol = mlir::cast< mlir::OpaqueLoc >(fl.getMetadata());
-        return mlir::OpaqueLoc::getUnderlyingLocation< mlir::Operation * >(ol);
-    }
-
-    static auto remove_op_ptr_loc(mlir::Operation *op) -> void {
+    auto default_loc_rewriter_t::remove(mlir::Operation *op) -> void {
         auto fl = mlir::cast< mlir::FusedLoc >(op->getLoc());
         op->setLoc(fl.getLocations().front());
     }
 
-    auto manager_t::apply(handle_t handle, pass_ptr_t pass) -> handle_t {
-        handle.mod.walk(insert_op_ptr_loc);
-
-        _modules.emplace_back(mlir::cast< vast_module >(handle.mod->clone()));
-
-        auto id  = _modules.size() - 1;
-        auto mod = _modules.back().get();
-
-        mlir::PassManager pm(&_ctx);
-        pm.addPass(std::move(pass));
-
-        VAST_CHECK(mlir::succeeded(pm.run(mod)), "Some pass in apply() failed");
-
-        mlir::IRMapping prev;
-
-        mod.walk([&prev](mlir::Operation *op) {
-            prev.map(op, get_op_ptr_loc(op));
-            remove_op_ptr_loc(op);
-        });
-
-        return { id, mod, prev };
+    auto default_loc_rewriter_t::prev(mlir::Operation *op) -> mlir::Operation * {
+        auto fl = mlir::cast< mlir::FusedLoc >(op->getLoc());
+        auto ol = mlir::cast< mlir::OpaqueLoc >(fl.getMetadata());
+        return mlir::OpaqueLoc::getUnderlyingLocation< mlir::Operation * >(ol);
     }
 } // namespace vast::tower
