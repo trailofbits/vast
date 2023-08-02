5819f5015a879e5a844127c46994cfb5094b8357
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-08-02 17:25:52 +0200

    hl: Add `traverse_record` utility function.
    Emits `hl.member` for each field.

--- include/vast/Dialect/HighLevel/HighLevelUtils.hpp
+++ include/vast/Dialect/HighLevel/HighLevelUtils.hpp
@@ -8,6 +8,8 @@
 #include "vast/Dialect/HighLevel/HighLevelOps.hpp"
 #include "vast/Interfaces/SymbolInterface.hpp"
 
+#include "vast/Util/Common.hpp"
+
 #include "gap/core/generator.hpp"
 
 /* Contains common utilities often needed to work with hl dialect. */
@@ -27,12 +29,10 @@ namespace vast::hl
     }
 
     using type_generator = gap::generator< mlir::Type >;
+    using value_generator = gap::generator< mlir::Value >;
 
-    static inline type_generator field_types(hl::StructDeclOp op)
+    static inline gap::generator< hl::FieldDeclOp > field_defs(hl::StructDeclOp op)
     {
-        if (op.getFields().empty())
-            co_return;
-
         for (auto &maybe_field : op.getOps())
         {
             // TODO(hl): So normally only `hl.field` should be present here,
@@ -43,17 +43,52 @@ namespace vast::hl
 
             auto field_decl = mlir::dyn_cast< hl::FieldDeclOp >(maybe_field);
             VAST_ASSERT(field_decl);
-            co_yield field_decl.getType();
+            co_yield field_decl;
         }
     }
 
-    static inline type_generator field_types(mlir::Type t, vast_module mod)
+    static inline type_generator field_types(hl::StructDeclOp op)
+    {
+        for (auto def : field_defs(op))
+            co_yield def.getType();
+    }
+
+    static inline auto definiton_of(mlir::Type t, vast_module mod)
+        -> std::optional< hl::StructDeclOp >
     {
         auto type_name = hl::name_of_record(t);
         VAST_ASSERT(type_name);
         for (auto op : get_nested< hl::StructDeclOp >(mod))
             if (op.getName() == *type_name)
-                return field_types(op);
-        VAST_UNREACHABLE("Was not able to fetch definition of type: {0}", t);
+                return { op };
+        return {};
+    }
+
+    static inline type_generator field_types(mlir::Type t, vast_module mod)
+    {
+        auto def = definiton_of(t, mod);
+        VAST_CHECK(def, "Was not able to fetch definition of type: {0}", t);
+        return field_types(*def);
+    }
+
+    // TODO(hl): Custom hook to provide a location?
+    auto traverse_record(operation root, auto &bld)
+        -> gap::generator< hl::RecordMemberOp >
+    {
+        auto mod = root->getParentOfType< vast_module >();
+        VAST_ASSERT(mod);
+        auto def = definiton_of(root->getResultTypes()[0], mod);
+        VAST_CHECK(def, "Was not able to fetch definition of type from: {0}", *root);
+
+        for (auto field_def : field_defs(*def))
+        {
+            auto as_val = root->getResult(0);
+            // `hl.member` requires type to be an lvalue.
+            auto wrap_type = hl::LValueType::get(mod.getContext(), field_def.getType());
+            co_yield bld.template create< hl::RecordMemberOp >(root->getLoc(),
+                                                               wrap_type,
+                                                               as_val,
+                                                               field_def.getName());
+        }
     }
 }
