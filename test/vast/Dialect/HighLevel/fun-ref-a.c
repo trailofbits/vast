42d946c71a65fd7f14a2cc466ea58a9f6cdd2bcb
Robert Konicar <robert.konicar@trailofbits.com>
2023-08-03 17:15:51 +0200

    test:hl: Add test for lazy emitting via function ref.

---
+++ test/vast/Dialect/HighLevel/fun-ref-a.c
@@ -0,0 +1,15 @@
+// RUN: vast-front -vast-emit-mlir=hl -o - %s | FileCheck %s
+
+void fun(void);
+
+void fun2(void (*f)(void)){ f(); }
+
+// CHECK: {{.*hl.func .* @fun.*}}
+// CHECK: {{.*hl.func .* @main.*}}
+int main(void) {
+    //CHECK: {{.*hl.funcref @fun.*}}
+    fun2(&fun);
+    return 0;
+}
+
+void fun(void) {}
