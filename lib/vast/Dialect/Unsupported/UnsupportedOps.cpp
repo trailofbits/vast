529e70450a5e97e17aa71df22bc2a5045838e92a
Henrich Lauko <henrich.lauko@trailofbits.com>
2023-08-11 16:28:53 +0200

    us: Refactor unsupported operations.

--- lib/vast/Dialect/Unsupported/UnsupportedOps.cpp
+++ lib/vast/Dialect/Unsupported/UnsupportedOps.cpp
@@ -14,24 +14,28 @@ VAST_UNRELAX_WARNINGS
 #define GET_OP_CLASSES
 #include "vast/Dialect/Unsupported/Unsupported.cpp.inc"
 
+#include <span>
+
 namespace vast::us {
 
     void UnsupportedDecl::build(
-        Builder &bld, State &st, mlir::StringRef name, BuilderCallback fields
+        Builder &bld, State &st, llvm::StringRef name, BuilderCallback body
     ) {
-        st.addAttribute("name", bld.getStringAttr(name));
+        st.addAttribute(getNameAttrName(st.name), bld.getStringAttr(name));
         InsertionGuard guard(bld);
-        build_region(bld, st, fields);
+        build_region(bld, st, body);
     }
 
-    void UnsupportedExpr::build(
-        Builder &bld, State &st, mlir::StringRef name, Type rty,
-        std::unique_ptr< Region > &&region
+    void UnsupportedStmt::build(
+        Builder &bld, State &st, llvm::StringRef name, Type rty, const std::vector< BuilderCallBackFn > &builders
     ) {
         InsertionGuard guard(bld);
-        st.addRegion(std::move(region));
         st.addTypes(rty);
         st.addAttribute(getNameAttrName(st.name), bld.getStringAttr(name));
+        for (auto child : builders) {
+            build_region(bld, st, child);
+        }
     }
+
 
 } // namespace vast::us
