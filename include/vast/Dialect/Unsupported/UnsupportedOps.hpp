dd58c7b9e1a6151623b370680b1df85ac26746f2
Henrich Lauko <henrich.lauko@trailofbits.com>
2023-08-11 09:37:13 +0200

    us: Clean up unsupported type definition.

--- include/vast/Dialect/Unsupported/UnsupportedOps.hpp
+++ include/vast/Dialect/Unsupported/UnsupportedOps.hpp
@@ -2,14 +2,7 @@
 
 #pragma once
 
-#include "vast/Util/Warnings.hpp"
-
-VAST_RELAX_WARNINGS
-#include <mlir/IR/BuiltinOps.h>
-#include <mlir/IR/BuiltinTypes.h>
-#include <mlir/IR/Dialect.h>
-#include <mlir/IR/OperationSupport.h>
-VAST_RELAX_WARNINGS
+#include "vast/Util/Common.hpp"
 
 #define GET_OP_CLASSES
 #include "vast/Dialect/Unsupported/Unsupported.h.inc"
