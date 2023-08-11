dfd053251ace7c1b8b83ffe18b8c80abd64389a1
Marek Surovič <marek.surovic@trailofbits.com>
2023-08-11 19:07:18 +0200

    repl: Make `raise` take csv list of passes to run.

--- test/repl/raise.c
+++ test/repl/raise.c
@@ -1,3 +1,3 @@
-// RUN: printf "load %s\n raise\n exit" | vast-repl | FileCheck %s
+// RUN: printf "load %s\n raise vast-hl-to-ll-cf\n exit" | vast-repl | FileCheck %s
 // CHECK: ll.return %0 : !hl.int
 int main(void) { return 0; }
