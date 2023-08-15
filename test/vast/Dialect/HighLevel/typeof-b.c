4fb056b461334696ea6d12692c2ccd82acae0566
Robert Konicar <robert.konicar@trailofbits.com>
2023-08-15 14:29:20 +0200

    test:hl: Add tests for qualifiers with typeof expressions.

---
+++ test/vast/Dialect/HighLevel/typeof-b.c
@@ -0,0 +1,20 @@
+// RUN: vast-front -vast-emit-mlir=hl -o - %s | FileCheck %s
+// RUN: vast-front -vast-emit-mlir=hl -o - %s > %t && vast-opt %t | diff -B %t -
+
+int main() {
+// CHECK: hl.var "i" : !hl.lvalue<!hl.int< const >>
+const int i = 0;
+
+// CHECK: hl.typeof.expr "(i)" {
+// CHECK: hl.expr : !hl.lvalue<!hl.int< const >> {
+// CHECK: }
+// CHECK: {{hl.type.yield .* : !hl.lvalue<!hl.int< const >>}}
+// CHECK: } : !hl.int< const >
+// CHECK: hl.var "j" : !hl.lvalue<!hl.typeof.expr<"(i)">>
+// CHECK: hl.const #hl.integer<0> : !hl.int
+typeof(i) j = 0;
+// CHECK hl.typeof.type : !hl.int< const >
+// CHECK: {{.* hl.var "k" : !hl.lvalue<!hl.typeof.type<!hl.int< const >>>.*}}
+typeof(const int) k = 0;
+return 0;
+}
