0e3f55fceda8560846b346bb36b6fe1bc4bcad18
Robert Konicar <robert.konicar@trailofbits.com>
2023-08-15 14:43:19 +0200

    test:hl: Remove propagated qualifiers from types.

--- test/vast/Dialect/HighLevel/array-a.c
+++ test/vast/Dialect/HighLevel/array-a.c
@@ -4,16 +4,16 @@
 // CHECK: hl.var "ai" : !hl.lvalue<!hl.array<10, !hl.int>>
 int ai[10];
 
-// CHECK: hl.var "aci" : !hl.lvalue<!hl.array<5, !hl.int< const >,  const >>
+// CHECK: hl.var "aci" : !hl.lvalue<!hl.array<5, !hl.int< const >>>
 const int aci[5];
 
-// CHECK: hl.var "avi" : !hl.lvalue<!hl.array<5, !hl.int< volatile >,  volatile >>
+// CHECK: hl.var "avi" : !hl.lvalue<!hl.array<5, !hl.int< volatile >>>
 volatile int avi[5];
 
-// CHECK: hl.var "acvi" : !hl.lvalue<!hl.array<5, !hl.int< const, volatile >,  const, volatile >>
+// CHECK: hl.var "acvi" : !hl.lvalue<!hl.array<5, !hl.int< const, volatile >>>
 const volatile int acvi[5];
 
-// CHECK: hl.var "acvui" : !hl.lvalue<!hl.array<5, !hl.int< unsigned, const, volatile >,  const, volatile >>
+// CHECK: hl.var "acvui" : !hl.lvalue<!hl.array<5, !hl.int< unsigned, const, volatile >>>
 const volatile unsigned int acvui[5];
 
 // CHECK: hl.var "af" : !hl.lvalue<!hl.array<10, !hl.float>>
