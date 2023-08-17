8efda4950d20bc69b47a88f7a224d9f6ceeaf9f7
Robert Konicar <robert.konicar@trailofbits.com>
2023-08-17 15:12:52 +0200

    hl: Make TypeOfExprOp builder use builder callback for expr region.

--- lib/vast/Dialect/HighLevel/HighLevelOps.cpp
+++ lib/vast/Dialect/HighLevel/HighLevelOps.cpp
@@ -472,11 +472,11 @@ namespace vast::hl
         st.addTypes(rty);
     }
 
-    void TypeOfExprOp::build(Builder &bld, State &st, llvm::StringRef name, Type type, std::unique_ptr< Region > &&region) {
+    void TypeOfExprOp::build(Builder &bld, State &st, llvm::StringRef name, Type type, BuilderCallback expr) {
         InsertionGuard guard(bld);
         st.addAttribute("name", bld.getStringAttr(name));
         st.addAttribute("type", mlir::TypeAttr::get(type));
-        st.addRegion(std::move(region));
+        build_region(bld, st, expr);
     }
 
     FuncOp getCallee(CallOp call)
