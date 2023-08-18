fc7570628331d306d97f79a0bd8ea1e2ef8d0759
Henrich Lauko <xlauko@mail.muni.cz>
2023-08-18 16:10:31 +0200

    cmake: Refactor definitions of vast-related tools.

--- tools/vast-opt/vast-opt.cpp
+++ tools/vast-opt/vast-opt.cpp
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
 {
     mlir::registerAllPasses();
     // Register VAST passes here
-    vast::hl::registerPasses();
+    vast::hl::registerHighLevelPasses();
     vast::registerConversionPasses();
 
     mlir::DialectRegistry registry;
