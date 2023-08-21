359b7a803d880603865e7b4a3a17d1e6f81d4eb3
Henrich Lauko <henrich.lauko@trailofbits.com>
2023-08-21 12:51:39 +0200

    test: Update unsupported cxx struct type test.

--- test/vast/Dialect/Unsupported/struct-type.cpp
+++ test/vast/Dialect/Unsupported/struct-type.cpp
@@ -1,16 +1,15 @@
 // RUN: vast-front %s -vast-emit-mlir=hl -o - | FileCheck %s
 // RUN: vast-front %s -vast-emit-mlir=hl -o - > %t && vast-opt %t | diff -B %t -
 
-// CHECK: hl.struct
+// CHECK: hl.cxxstruct
 struct  Student {
+  // CHECK: unsup.stmt "CXXConstructExpr"
   unsigned int id;
   char name[40];
 };
 
 Student aux;
 
-// !hl.lvalue<!hl.lvalue<!hl.reference<!hl.elaborated<!hl.record<"Student">,  const >>>>
-//            '!hl.lvalue<!hl.elaborated<!hl.record<"Student">,  const >>
 void StudentRegistry(struct Student *students, int index)
 {
    aux = students[index];
