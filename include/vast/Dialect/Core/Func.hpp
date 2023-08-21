e7f096a93e39d68021f60910ce2198da4306708e
Robert Konicar <robert.konicar@trailofbits.com>
2023-08-21 14:34:48 +0200

    core: Add FuncOp template for use by other dialects.

---
+++ include/vast/Dialect/Core/Func.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2023-present, Trail of Bits, Inc.
+
+#pragma once
+
+#include "vast/Util/Warnings.hpp"
+
+VAST_RELAX_WARNINGS
+#include <mlir/Interfaces/CallInterfaces.h>
+#include <mlir/IR/FunctionInterfaces.h>
+VAST_RELAX_WARNINGS
+
+#include "vast/Dialect/Core/Linkage.hpp"
