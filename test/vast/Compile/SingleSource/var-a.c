546ef669ba5d2cb3f6f077c14bc69f384359bb00
Robert Konicar <robert.konicar@trailofbits.com>
2023-08-23 12:20:34 +0200

    test:compile: Add var+sizeof compile test.

---
+++ test/vast/Compile/SingleSource/var-a.c
@@ -0,0 +1,18 @@
+// RUN: vast-front -o %t %s && (%t 1; test $? -eq 2)
+// REQUIRES: union_lowering
+
+#include <stdlib.h>
+
+int putchar(int);
+int main(int argc, char **argv) {
+    int x = 0;
+    if (argc > 1) {
+        int *y = malloc(sizeof(*y));
+    }
+    for (int i = 0; i < argc; i++){
+        int *p = malloc(sizeof(*p));
+        putchar('a');
+        putchar('\n');
+    }
+    return argc;
+}
