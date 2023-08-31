1fa446a62c22e432d011c857e15330ff361c9b05
Robert Konicar <robert.konicar@trailofbits.com>
2023-08-31 15:27:51 +0200

    test:hl: Fix checks with void function return types.

--- test/vast/Dialect/HighLevel/ops-b.c
+++ test/vast/Dialect/HighLevel/ops-b.c
@@ -87,7 +87,7 @@ void assign_signed(int a, int b)
     a %= b;
 }
 
-// CHECK: hl.func external @assign_unsigned ([[A1:%arg[0-9]+]]: !hl.lvalue<!hl.int< unsigned >>, [[A2:%arg[0-9]+]]: !hl.lvalue<!hl.int< unsigned >>) {
+// CHECK: hl.func external @assign_unsigned ([[A1:%arg[0-9]+]]: !hl.lvalue<!hl.int< unsigned >>, [[A2:%arg[0-9]+]]: !hl.lvalue<!hl.int< unsigned >>)
 void assign_unsigned(unsigned a, unsigned b)
 {
     // CHECK: [[V1:%[0-9]+]] = hl.ref [[A1]]
