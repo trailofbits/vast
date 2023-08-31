c151c823706e9b3cf988986478b15130db7e4660
Robert Konicar <robert.konicar@trailofbits.com>
2023-08-31 15:48:33 +0200

    test:hl: Add test for casting function call to void.

---
+++ test/vast/Dialect/HighLevel/function-void-b.c
@@ -0,0 +1,20 @@
+// RUN: vast-front -vast-emit-mlir=hl -x c -o - %s | FileCheck %s
+// RUN: vast-front -vast-emit-mlir=hl -x c++ -o - %s | FileCheck %s
+
+// CHECK: hl.func external {{.*}} () -> !hl.void
+void f1() {}
+// CHECK: hl.func external {{.*}} () -> !hl.void
+void f2(void) {}
+// CHECK: hl.func external {{.*}} () -> !hl.int
+int  f3(void) {return 1;}
+
+int main() {
+// CHECK: hl.cstyle_cast {{.*}} ToVoid : {{.*}} -> !hl.void
+    (void) f1();
+// CHECK: hl.cstyle_cast {{.*}} ToVoid : {{.*}} -> !hl.void
+    (void) f2();
+// CHECK: hl.cstyle_cast {{.*}} ToVoid : {{.*}} -> !hl.void
+    (void) f3();
+
+}
+
