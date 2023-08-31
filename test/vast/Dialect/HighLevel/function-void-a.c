191fd3ebde8501c23d9351c963e78f73d81b7478
Robert Konicar <robert.konicar@trailofbits.com>
2023-08-31 15:33:19 +0200

    hl: Add test with for functions with void types.

---
+++ test/vast/Dialect/HighLevel/function-void-a.c
@@ -0,0 +1,10 @@
+// RUN: vast-front -vast-emit-mlir=hl -x c -o - %s | FileCheck %s
+// RUN: vast-front -vast-emit-mlir=hl -x c++ -o - %s | FileCheck %s
+
+// CHECK: hl.func external {{.*}} () -> !hl.void
+void f1() {}
+// CHECK: hl.func external {{.*}} () -> !hl.void
+void f2(void) {}
+// CHECK: hl.func external {{.*}} () -> !hl.int
+int  f3(void) {return 1;}
+
