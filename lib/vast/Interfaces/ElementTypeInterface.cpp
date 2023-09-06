54a413f201efd841ee142ac173df3c12199729e2
Robert Konicar <robert.konicar@trailofbits.com>
2023-09-06 14:59:42 +0200

    interface: Add ElementTypeInterface.

---
+++ lib/vast/Interfaces/ElementTypeInterface.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2023-present, Trail of Bits, Inc.
+
+#include "vast/Interfaces/ElementTypeInterface.hpp"
+
+//===----------------------------------------------------------------------===//
+// Element Type Interface
+//===----------------------------------------------------------------------===//
+
+/// Include the generated interface.
+#include "vast/Interfaces/ElementTypeInterface.cpp.inc"
