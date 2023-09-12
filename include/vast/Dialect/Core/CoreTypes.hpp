4fcfafef6cb5346e61d77d5aa172782d87c7a75c
Henrich Lauko <xlauko@mail.muni.cz>
2023-09-12 14:34:10 +0200

    core: Remove obsolete function type lowering.

--- include/vast/Dialect/Core/CoreTypes.hpp
+++ include/vast/Dialect/Core/CoreTypes.hpp
@@ -6,9 +6,3 @@
 
 #define GET_TYPEDEF_CLASSES
 #include "vast/Dialect/Core/CoreTypes.h.inc"
-
-namespace vast::core {
-
-    mlir::FunctionType lower(core::FunctionType fty);
-
-} // namespace vast::core
