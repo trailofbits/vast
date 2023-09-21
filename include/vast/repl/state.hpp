fb93c98d70e75550399e10dd5dc62e483673d372
Henrich Lauko <xlauko@mail.muni.cz>
2023-09-21 22:47:10 +0200

    repl: Load config of sticky commands and pipelines.

--- include/vast/repl/state.hpp
+++ include/vast/repl/state.hpp
@@ -5,6 +5,7 @@
 #include "vast/Tower/Tower.hpp"
 #include "vast/repl/common.hpp"
 #include "vast/repl/command_base.hpp"
+#include "vast/repl/pipeline.hpp"
 
 #include <filesystem>
 
@@ -13,14 +14,36 @@ namespace vast::repl {
     struct state_t {
         explicit state_t(mcontext_t &ctx) : ctx(ctx) {}
 
+        //
+        // perform exit in next step
+        //
         bool exit = false;
 
+        //
+        // c/c++ source file to compile
+        //
         std::optional< std::filesystem::path > source;
 
+        //
+        // mlir module and context
+        //
         mcontext_t &ctx;
         std::optional< tw::default_tower > tower;
 
+        //
+        // sticked commands performed after each step
+        //
         std::vector< command_ptr > sticked;
+
+        //
+        // named pipelines
+        //
+        llvm::StringMap< pipeline > pipelines;
+
+        //
+        // verbosity flags
+        //
+        bool verbose_pipeline = true;
     };
 
 } // namespace vast::repl
