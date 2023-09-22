3c40e94ab81b16c18b64c7cd025e8aea8c87739e
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-09-22 18:01:50 +0200

    XXX: Dummy mapping of mlir to llvm.

--- lib/vast/Dialect/HighLevel/Transforms/LLVMDump.cpp
+++ lib/vast/Dialect/HighLevel/Transforms/LLVMDump.cpp
@@ -25,8 +25,12 @@ VAST_UNRELAX_WARNINGS
 
 #include <vast/Dialect/HighLevel/HighLevelDialect.hpp>
 #include <vast/Dialect/HighLevel/HighLevelOps.hpp>
+#include <vast/Dialect/HighLevel/HighLevelUtils.hpp>
 
 #include "PassesDetails.hpp"
+
+#include <unordered_map>
+#include <unordered_set>
 
 namespace vast::hl
 {
@@ -46,6 +50,153 @@ namespace vast::hl
                 .Default([&](mlir::Operation *) {
                     return mlir::failure();
                 });
+        }
+    };
+
+    using mlir_to_llvm = std::unordered_map< mlir::Operation *, llvm::Instruction * >;
+        // op->getStringRef.
+
+    struct get_mapping
+    {
+        // func to mapping of its things
+        std::map< std::string, mlir_to_llvm > function_mapping;
+        std::map< operation, llvm::GlobalVariable * > global_vars;
+        std::map< operation, llvm::Function * > global_functions;
+
+        inline static const std::unordered_set< std::string > m_skip =
+        {
+            "llvm.mlir.constant"
+        };
+
+        inline static const std::unordered_set< std::string > allowed_miss =
+        {
+            "llvm.bitcast",
+            "llvm.mlir.addressof"
+        };
+
+        inline static const std::unordered_set< std::string > l_skip =
+        {
+        };
+
+        inline static const std::unordered_map< std::string, std::string > translations =
+        {
+            { "llvm.alloca", "alloca" },
+            { "llvm.bitcast","bitcast" },
+            { "llvm.store", "store" },
+            { "llvm.load", "load" },
+            { "llvm.return", "ret" },
+            { "llvm.call", "call" },
+            { "llvm.getelementptr", "getelementptr" },
+
+            { "llvm.mlir.addressof", "" }
+        };
+
+        std::tuple< mlir::LLVM::LLVMFuncOp, llvm::Function * >
+        get_fn( auto name, auto m_mod, auto l_mod )
+        {
+            auto l_fn = l_mod->getFunction(name);
+            for (auto m_fn : top_level_ops< mlir::LLVM::LLVMFuncOp >(m_mod))
+            {
+                if (m_fn.getName() == name)
+                    return { m_fn, l_fn };
+            }
+
+            return {};
+        }
+
+        auto key(mlir::Block::iterator it) -> std::string
+        {
+            return it->getName().getStringRef().str();
+        }
+
+        auto key(llvm::BasicBlock::iterator it) -> std::string
+        {
+            return it->getOpcodeName();
+        }
+
+
+        bool skip(mlir::Block::iterator it) { return m_skip.count( key( it ) ); }
+        bool skip(llvm::BasicBlock::iterator it) { return l_skip.count( key( it ) ); }
+
+        bool match( auto m_it, auto l_it )
+        {
+            auto m = translations.find( key( m_it ) );
+            VAST_CHECK( m != translations.end(),
+                        "Missing translation {0}: {1}", key( m_it ),  *m_it );
+            return m->second == key(l_it);
+        }
+
+        auto annotate_functions(mlir::ModuleOp op, llvm::Module *l_mod)
+        {
+            auto [ m_func, l_func ] = get_fn("main", op, l_mod);
+            VAST_ASSERT( m_func && l_func );
+            global_functions.emplace( m_func, l_func );
+            llvm::errs() << "Matched functions!\n";
+
+
+            auto &current = function_mapping[ "main" ];
+
+            auto m_it = m_func.getRegion().begin()->begin();
+            auto m_end = m_func.getRegion().begin()->end();
+            llvm::BasicBlock::iterator l_it = l_func->begin()->begin();
+            llvm::BasicBlock::iterator l_end = l_func->begin()->end();
+
+            while (m_it != m_end)
+            {
+                if (skip(m_it))
+                {
+                    llvm::errs() << "m_skip rule: " << key(m_it) << "\n";
+                    ++m_it;
+                    continue;
+                }
+                VAST_ASSERT(l_it != l_end);
+
+                llvm::errs() << "Matching: " << key(m_it) << " to " << key(l_it) << "\n";
+
+                if (!match(m_it, l_it))
+                {
+                    if (skip(l_it))
+                    {
+                        llvm::errs() << " \tl_skip rule: " << key(l_it) << "\n";
+                        ++l_it;
+                        continue;
+                    }
+                    if ( allowed_miss.count( key( m_it ) ) )
+                    {
+                        llvm::errs() << " \tallowed_miss rule: " << key(m_it) << "\n";
+                        ++m_it;
+                        continue;
+                    }
+                    VAST_CHECK(false, "Cannot progress on {0}!", key(m_it));
+                }
+                llvm::errs() << ".... Matched!\n";
+
+                current.emplace( &*m_it, &*l_it );
+                ++l_it;
+                ++m_it;
+            }
+        }
+
+        auto annotate_gvs(mlir::ModuleOp m_mod, llvm::Module *l_mod)
+        {
+            for (auto m_var : top_level_ops< mlir::LLVM::GlobalOp >(m_mod))
+            {
+                auto l_var = l_mod->getGlobalVariable(m_var.getName());
+                global_vars.emplace( m_var, l_var );
+                llvm::errs() << "Matched globals!\n";
+            }
+        }
+
+        void get(mlir::ModuleOp m_mod, llvm::Module *l_mod)
+        {
+            // Not sure what to do with global and functions yet.
+            annotate_functions(m_mod, l_mod);
+            annotate_gvs(m_mod, l_mod);
+            //auto print = [&]( operation thing )
+            //{
+            //    llvm::errs() << thing->getName().getStringRef() << "\n";
+            //};
+            //m_mod->walk< mlir::WalkOrder::PostOrder >(print);
         }
     };
 
@@ -84,6 +235,8 @@ namespace vast::hl
             stream << *lmodule;
             stream.flush();
         };
+
+        (void)get_mapping{}.get(op, lmodule.get());
 
         auto outname = this->bitcode_file.getValue();
         if (outname.empty())
