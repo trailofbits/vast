b6ce0a84e7ed514ecea0eec353577eedc6bf2e84
Henrich Lauko <xlauko@mail.muni.cz>
2023-10-02 12:05:41 +0200

    hl: Extract default data layout impl to an interface.

---
+++ lib/vast/Interfaces/DefaultDataLayoutTypeInterface.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2023-present, Trail of Bits, Inc.
+
+#include "vast/Interfaces/DefaultDataLayoutTypeInterface.hpp"
+
+//===----------------------------------------------------------------------===//
+// Default Data Layout Type Interface
+//===----------------------------------------------------------------------===//
+
+/// Include the generated interface.
+#include "vast/Interfaces/DefaultDataLayoutTypeInterface.cpp.inc"
