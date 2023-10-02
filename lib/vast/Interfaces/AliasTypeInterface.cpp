c0c6284ba176e11b842cc29e561274a70a0be773
Henrich Lauko <xlauko@mail.muni.cz>
2023-10-02 14:48:59 +0200

    interface: Introduce AliasTypeInterface.

---
+++ lib/vast/Interfaces/AliasTypeInterface.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2023-present, Trail of Bits, Inc.
+
+#include "vast/Interfaces/AliasTypeInterface.hpp"
+
+//===----------------------------------------------------------------------===//
+// Alias Type Interface
+//===----------------------------------------------------------------------===//
+
+/// Include the generated interface.
+#include "vast/Interfaces/AliasTypeInterface.cpp.inc"
