ff05f0f800adc24be807ccf32c0ded10012affc9
Henrich Lauko <xlauko@mail.muni.cz>
2023-10-03 14:35:30 +0200

    test: Add float to bool conversion tests.

---
+++ test/vast/Conversion/Common/IRsToLLVM/cast-e.cpp
@@ -0,0 +1,24 @@
+// RUN: %vast-cc1 -triple x86_64-unknown-linux-gnu -vast-emit-mlir=hl %s -o %t.mlir
+// RUN: %file-check --input-file=%t.mlir %s -check-prefix=HL
+// RUN: %vast-cc1 -triple x86_64-unknown-linux-gnu -vast-emit-mlir=llvm %s -o %t.mlir
+// RUN: %file-check --input-file=%t.mlir %s -check-prefix=MLIR
+// RUN: %vast-cc1 -triple x86_64-unknown-linux-gnu -vast-emit-llvm %s -o %t.ll
+// RUN: %file-check --input-file=%t.ll %s -check-prefix=LLVM
+
+void float_to_bool() {
+    float f;
+    bool b = f;
+}
+
+// HL: hl.func @_Z13float_to_boolv ()
+// HL:    hl.implicit_cast {{.*}} FloatingToBoolean
+// HL: }
+
+// MLIR: llvm.func @_Z13float_to_boolv()
+// MLIR:    llvm.mlir.constant(0.000000e+00 : f32)
+// MLIR:    llvm.fcmp "une"
+// MLIR: }
+
+// LLVM: define void @_Z13float_to_boolv()
+// LLVM:    fcmp une {{.*}} 0.000000e+00
+// LLVM: }
---
+++ test/vast/Conversion/Common/IRsToLLVM/cast-f.c
@@ -0,0 +1,25 @@
+// RUN: %vast-cc1 -triple x86_64-unknown-linux-gnu -vast-emit-mlir=hl %s -o %t.mlir
+// RUN: %file-check --input-file=%t.mlir %s -check-prefix=HL
+// RUN: %vast-cc1 -triple x86_64-unknown-linux-gnu -vast-emit-mlir=llvm %s -o %t.mlir
+// RUN: %file-check --input-file=%t.mlir %s -check-prefix=MLIR
+// RUN: %vast-cc1 -triple x86_64-unknown-linux-gnu -vast-emit-llvm %s -o %t.ll
+// RUN: %file-check --input-file=%t.ll %s -check-prefix=LLVM
+
+void float_to_bool() {
+    float f;
+    _Bool b = f;
+}
+
+// HL: hl.func @float_to_bool ()
+// HL:    hl.implicit_cast {{.*}} FloatingToBoolean
+// HL: }
+
+// MLIR: llvm.func @float_to_bool()
+// MLIR:    llvm.mlir.constant(0.000000e+00 : f32)
+// MLIR:    llvm.fcmp "une"
+// MLIR:    llvm.zext {{.*}} : i1 to i8
+// MLIR: }
+
+// LLVM: define void @float_to_bool()
+// LLVM:    fcmp une {{.*}} 0.000000e+00
+// LLVM: }
