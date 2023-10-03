16b3c160055d9a5f5c556c54251191616d0060a9
Henrich Lauko <xlauko@mail.muni.cz>
2023-10-03 18:16:05 +0200

    test: Add pointer cast conversion tests.

---
+++ test/vast/Conversion/Common/IRsToLLVM/cast-j.c
@@ -0,0 +1,22 @@
+// RUN: %vast-cc1 -triple x86_64-unknown-linux-gnu -vast-emit-mlir=hl %s -o %t.mlir
+// RUN: %file-check --input-file=%t.mlir %s -check-prefix=HL
+// RUN: %vast-cc1 -triple x86_64-unknown-linux-gnu -vast-emit-mlir=llvm %s -o %t.mlir
+// RUN: %file-check --input-file=%t.mlir %s -check-prefix=MLIR
+// RUN: %vast-cc1 -triple x86_64-unknown-linux-gnu -vast-emit-llvm %s -o %t.ll
+// RUN: %file-check --input-file=%t.ll %s -check-prefix=LLVM
+
+void null_to_ptr() {
+    void *p = 0;
+}
+
+// HL: hl.func @null_to_ptr ()
+// HL:    hl.implicit_cast {{.*}} NullToPointer : !hl.int -> !hl.ptr<!hl.void>
+// HL: }
+
+// MLIR: llvm.func @null_to_ptr()
+// MLIR:    llvm.mlir.null
+// MLIR: }
+
+// LLVM: define void @null_to_ptr()
+// LLVM:    store ptr null, ptr
+// LLVM: }
