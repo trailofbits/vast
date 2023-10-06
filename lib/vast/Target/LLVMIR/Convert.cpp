fb8925a56538c5f012815cffade7a31dce3203b5
Henrich Lauko <xlauko@mail.muni.cz>
2023-10-06 14:33:12 +0200

    target:llvmir: Filter obsolete data lyaout.

--- lib/vast/Target/LLVMIR/Convert.cpp
+++ lib/vast/Target/LLVMIR/Convert.cpp
@@ -6,6 +6,7 @@ VAST_RELAX_WARNINGS
 #include <mlir/IR/BuiltinOps.h>
 
 #include <mlir/Dialect/LLVMIR/LLVMDialect.h>
+#include <mlir/Dialect/LLVMIR/Transforms/Passes.h>
 
 #include <mlir/ExecutionEngine/ExecutionEngine.h>
 #include <mlir/Target/LLVMIR/Export.h>
@@ -48,12 +49,34 @@ namespace vast::target::llvmir
         }
     };
 
-    std::unique_ptr< llvm::Module > translate(mlir::Operation *op,
-                                              llvm::LLVMContext &llvm_ctx,
-                                              const std::string &module_name)
-    {
-        auto mlir_module = mlir::dyn_cast< mlir::ModuleOp >(op);
-        VAST_CHECK(mlir_module, "Cannot translate operation that is not an mlir::ModuleOp!");
+    // TODO: move to translation passes that erase specific types from module
+    void clean_up_data_layout(vast_module mlir_module) {
+        // If the old data layout with high level types is left in the module,
+        // some parsing functionality inside the `mlir::translateModuleToLLVMIR`
+        // will fail and no conversion translation happens, even in case these
+        // entries are not used at all.
+        auto dl = mlir_module.getDataLayoutSpec();
+
+        auto is_llvm_compatible_entry = [] (auto entry) {
+            return mlir::LLVM::isCompatibleType(entry.getKey().template get< mlir_type >());
+        };
+
+        auto filtered_entries = [&] {
+            return llvm::to_vector(
+                llvm::make_filter_range(dl.getEntries(),is_llvm_compatible_entry)
+            );
+        } ();
+
+        mlir_module->setAttr(
+            mlir::DLTIDialect::kDataLayoutAttrName,
+            mlir::DataLayoutSpecAttr::get(mlir_module.getContext(), filtered_entries)
+        );
+    }
+
+    std::unique_ptr< llvm::Module > translate(
+        vast_module mlir_module, llvm::LLVMContext &llvm_ctx,const std::string &module_name
+    ) {
+        clean_up_data_layout(mlir_module);
         return mlir::translateModuleToLLVMIR(mlir_module, llvm_ctx);
     }
 
@@ -77,12 +100,20 @@ namespace vast::target::llvmir
         pm.addPass(createIRsToLLVMPass());
         pm.addPass(createCoreToLLVMPass());
 
+        // This is necessary to have line tables emitted and basic
+        // debugger working. In the future we will add proper debug information
+        // emission directly from our frontend.
+        pm.addNestedPass<mlir::LLVM::LLVMFuncOp>(
+            mlir::LLVM::createDIScopeForLLVMFuncOpPass()
+        );
+
         pm.enableIRPrinting([](auto *, auto *) { return false; }, // before
                             [](auto *, auto *) { return true; }, //after
                             false, // module scope
                             false, // after change
                             true, // after failure
                             llvm::errs());
+
 
         auto run_result = pm.run(op);
 
