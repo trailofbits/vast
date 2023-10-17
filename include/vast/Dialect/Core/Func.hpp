9374f04b879ec920c43a1c7bea4e64b34ac253e9
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-10-17 14:46:10 +0200

    core: Expose common utilities used by function ops.

---
+++ include/vast/Dialect/Core/Func.hpp
@@ -0,0 +1,83 @@
+// Copyright (c) 2021-present, Trail of Bits, Inc.
+
+#pragma once
+
+#include "vast/Util/Warnings.hpp"
+#include "vast/Util/Common.hpp"
+
+VAST_RELAX_WARNINGS
+#include <mlir/IR/FunctionImplementation.h>
+VAST_UNRELAX_WARNINGS
+
+#include "vast/Dialect/Core/CoreDialect.hpp"
+
+namespace vast::core {
+
+    llvm::StringRef getLinkageAttrNameString();
+
+    // This function is adapted from CIR:
+    //
+    // Verifies linkage types, similar to LLVM:
+    // - functions don't have 'common' linkage
+    // - external functions have 'external' or 'extern_weak' linkage
+    logical_result verifyFuncOp(auto op) {
+        using core::GlobalLinkageKind;
+
+        auto linkage = op.getLinkage();
+        constexpr auto common = GlobalLinkageKind::CommonLinkage;
+        if (linkage == common) {
+            return op.emitOpError() << "functions cannot have '"
+                << stringifyGlobalLinkageKind(common)
+                << "' linkage";
+        }
+
+        // isExternal(FunctionOpInterface) only checks for empty
+        // bodyonly checks for empty body...
+        // We need to be able to handle functions with internal linkage without body.
+        if (linkage != GlobalLinkageKind::InternalLinkage && op.isExternal()) {
+            constexpr auto external = GlobalLinkageKind::ExternalLinkage;
+            constexpr auto weak_external = GlobalLinkageKind::ExternalWeakLinkage;
+            if (linkage != external && linkage != weak_external) {
+                return op.emitOpError() << "external functions must have '"
+                    << stringifyGlobalLinkageKind(external)
+                    << "' or '"
+                    << stringifyGlobalLinkageKind(weak_external)
+                    << "' linkage";
+            }
+            return mlir::success();
+        }
+        return mlir::success();
+    }
+
+    ParseResult parseFunctionSignatureAndBody(
+        Parser &parser, Attribute &funcion_type,
+        mlir::NamedAttrList &attr_dict, Region &body
+    );
+
+
+    void printFunctionSignatureAndBody(
+        Printer &printer, auto op,
+        Attribute /* funcion_type */, mlir::DictionaryAttr, Region &body
+    ) {
+        if (op.getLinkage() != core::GlobalLinkageKind::ExternalLinkage) {
+            printer << stringifyGlobalLinkageKind(op.getLinkage()) << ' ';
+        }
+
+        auto fty = op.getFunctionType();
+        mlir::function_interface_impl::printFunctionSignature(
+            printer, op, fty.getInputs(), fty.isVarArg(), fty.getResults()
+        );
+
+        mlir::function_interface_impl::printFunctionAttributes(
+            printer, op, { getLinkageAttrNameString(), op.getFunctionTypeAttrName() }
+        );
+
+        if (!body.empty()) {
+            printer.getStream() << " ";
+            printer.printRegion( body,
+                /* printEntryBlockArgs */false,
+                /* printBlockTerminators */true
+            );
+        }
+    }
+} // namespace vast::core
---
+++ lib/vast/Dialect/Core/Func.cpp
@@ -0,0 +1,100 @@
+// Copyright (c) 2021-present, Trail of Bits, Inc.
+
+#include "vast/Util/Warnings.hpp"
+
+VAST_RELAX_WARNINGS
+#include <mlir/Support/LogicalResult.h>
+#include <mlir/IR/OperationSupport.h>
+#include <mlir/IR/FunctionImplementation.h>
+
+#include <llvm/Support/ErrorHandling.h>
+VAST_UNRELAX_WARNINGS
+
+#include "vast/Dialect/Core/Func.hpp"
+
+#include "vast/Dialect/Core/CoreAttributes.hpp"
+#include "vast/Dialect/Core/CoreDialect.hpp"
+#include "vast/Dialect/Core/Linkage.hpp"
+
+#include "vast/Util/Common.hpp"
+#include "vast/Util/Dialect.hpp"
+#include "vast/Util/Region.hpp"
+
+namespace vast::core
+{
+    //===----------------------------------------------------------------------===//
+    // FuncOp
+    //===----------------------------------------------------------------------===//
+
+    llvm::StringRef getLinkageAttrNameString() { return "linkage"; }
+
+    ParseResult parseFunctionSignatureAndBody(
+        Parser &parser, Attribute &funcion_type, mlir::NamedAttrList &attr_dict, Region &body
+    ) {
+        using core::GlobalLinkageKind;
+
+        // Default to external linkage if no keyword is provided.
+        attr_dict.append(
+            getLinkageAttrNameString(),
+            core::GlobalLinkageKindAttr::get(
+                parser.getContext(),
+                parse_optional_vast_keyword< GlobalLinkageKind >(
+                    parser, GlobalLinkageKind::ExternalLinkage
+                )
+            )
+        );
+
+        llvm::SmallVector< Parser::Argument, 8 > arguments;
+        llvm::SmallVector< mlir::DictionaryAttr, 1 > result_attrs;
+        llvm::SmallVector< Type, 8 > arg_types;
+        llvm::SmallVector< Type, 4 > result_types;
+
+        bool is_variadic = false;
+        if (mlir::failed(mlir::function_interface_impl::parseFunctionSignature(
+            parser, /*allowVariadic=*/true, arguments, is_variadic, result_types, result_attrs
+        ))) {
+            return mlir::failure();
+        }
+
+        for (auto &arg : arguments) {
+            arg_types.push_back(arg.type);
+        }
+
+        // TODO: no idea why CoreDialect is not loaded before
+        parser.getContext()->loadDialect< core::CoreDialect >();
+
+        // create parsed function type
+        funcion_type = mlir::TypeAttr::get(
+            core::FunctionType::get(
+                arg_types, result_types, is_variadic
+            )
+        );
+
+        // If additional attributes are present, parse them.
+        if (parser.parseOptionalAttrDictWithKeyword(attr_dict)) {
+            return mlir::failure();
+        }
+
+        // TODO: Add the attributes to the function arguments.
+        // VAST_ASSERT(result_attrs.size() == result_types.size());
+        // return mlir::function_interface_impl::addArgAndResultAttrs(
+        //     builder, state, arguments, result_attrs
+        // );
+
+        auto loc = parser.getCurrentLocation();
+        auto parse_result = parser.parseOptionalRegion(
+            body, arguments, /* enableNameShadowing */false
+        );
+
+        if (parse_result.has_value()) {
+            if (failed(*parse_result))
+                return mlir::failure();
+            // Function body was parsed, make sure its not empty.
+            if (body.empty())
+                return parser.emitError(loc, "expected non-empty function body");
+        }
+
+        return mlir::success();
+    }
+
+} // namespace vast::core
