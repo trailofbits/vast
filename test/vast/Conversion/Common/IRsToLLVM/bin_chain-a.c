4ef7d6af9abc31fc2a4b3e3f578d015be062da3f
Robert Konicar <robert.konicar@trailofbits.com>
2023-10-19 17:39:25 +0200

    test:conv: Add test for chained binary logical operators.

---
+++ test/vast/Conversion/Common/IRsToLLVM/bin_chain-a.c
@@ -0,0 +1,12 @@
+// RUN: %vast-front -vast-emit-mlir=llvm -o - %s | %file-check %s
+
+int main() {
+    int a = 5;
+    int b = 5;
+    int c = 5;
+    // CHECK: ^bb1{{.*}}// pred: ^bb0
+    // CHECK: ^bb2{{.*}}// pred: ^bb1
+    // CHECK: ^bb3{{.*}}// 2 preds: ^bb1, ^bb2
+    // CHECK: ^bb4{{.*}}// 2 preds: ^bb0, ^bb3
+    return a && (!b && !c);
+}
