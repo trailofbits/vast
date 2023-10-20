daebc0adbc3a2e4a1ee79799482dcc8a08790b90
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-10-20 18:59:49 +0200

    test: Update `irs-to-llvm` tests to reflect switch in type converter.

--- test/vast/Conversion/Common/IRsToLLVM/array-a.c
+++ test/vast/Conversion/Common/IRsToLLVM/array-a.c
@@ -2,6 +2,6 @@
 
 void count()
 {
-    // CHECK: llvm.store [[V4:%[0-9]+]], [[V10:%[0-9]+]] : !llvm.ptr<i32>
+    // CHECK: llvm.store [[V4:%[0-9]+]], [[V10:%[0-9]+]] : !llvm.ptr<array<3 x i32>>
     int x[3] = { 112, 212, 4121 };
 }
--- test/vast/Conversion/Common/IRsToLLVM/array-b.c
+++ test/vast/Conversion/Common/IRsToLLVM/array-b.c
@@ -2,6 +2,6 @@
 
 void count()
 {
-    // CHECK: llvm.store [[V4:%[0-9]+]], [[V10:%[0-9]+]] : !llvm.ptr<f32>
+    // CHECK: llvm.store [[V4:%[0-9]+]], [[V10:%[0-9]+]] : !llvm.ptr<array<3 x f32>>
     float x[3] = { 112.0f, 212.0f, 4121.0f };
 }
