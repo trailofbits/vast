e3b6ebd0a68dc279da95159e705fcb75967bc692
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-10-23 21:39:28 +0200

    conv:abi: Improve codestyle and add more comments.

--- include/vast/Conversion/ABI/AggregateTypes.hpp
+++ include/vast/Conversion/ABI/AggregateTypes.hpp
@@ -11,23 +11,21 @@ namespace vast::conv::abi
 {
     /* Handles aggregate type reconstruction. */
 
-    // TODO(conv:abi): Parametrize by materializer, which will resolve what dialect
-    //                is the target
-    //                 * hl
-    //                 * ll-vars
-    //                 * llvm
-    template< typename pattern, typename op_t >
-    struct aggregate_reconstructor
+    struct aggregate_construction_base
     {
       protected:
-        using self_t = aggregate_reconstructor< pattern, op_t >;
-
+        template< typename pattern, typename op_t >
         struct state_t
         {
+            // Current argument or result we work with.
             std::size_t arg_idx = 0;
+            // Current offset into the type - how many bits are already used.
             std::size_t offset = 0;
 
+            // Pattern that constructed this object - in case some
+            // data from broader context is needed.
             const pattern &parent;
+            // Current operation conversion is rewriting.
             op_t abi_op;
 
             state_t(const pattern &parent, op_t abi_op)
@@ -39,32 +37,67 @@ namespace vast::conv::abi
 
             state_t &operator=(state_t) = delete;
 
-            auto arg() { return abi_op.getOperands()[arg_idx]; };
             auto bw(auto w) { return parent.bw(w); };
-
-            bool fits(mlir_type type)
-            {
-                return bw(arg()) >= offset + bw(type);
-            };
 
             void advance()
             {
                 ++arg_idx;
                 offset = 0;
             }
+        };
+
+        // TODO(conv:abi): Issue #423 - figure out how to make this not adhoc.
+        //                 `SubElement` interface won't work, for pointers or arrays
+        //                 we do not care.
+        bool needs_nesting(mlir_type type) const
+        {
+            return contains_subtype< hl::RecordType >(type);
+        }
+    };
+
+    // TODO(conv:abi): Parametrize by materializer, which will resolve what dialect
+    //                is the target
+    //                 * hl
+    //                 * ll-vars
+    //                 * llvm
+    template< typename pattern, typename op_t >
+    struct aggregate_reconstructor : aggregate_construction_base
+    {
+      protected:
+        using self_t = aggregate_reconstructor< pattern, op_t >;
+
+        struct state_t : aggregate_construction_base::state_t< pattern, op_t >
+        {
+            using base = aggregate_construction_base::state_t< pattern, op_t >;
+            using base::base;
+
+            using base::bw;
+            using base::advance;
+
+            auto arg()
+            {
+                return this->abi_op.getOperands()[this->arg_idx];
+            };
+
+
+            bool fits(mlir_type type)
+            {
+                return this->bw(arg()) >= this->offset + bw(type);
+            };
+
 
             // TODO(conv:abi): Possibly add some debug prints to help us
             //                 debug some weird corner cases in the future?
             mlir::Value allocate(mlir_type type, auto &rewriter)
             {
-                auto start = offset;
-                offset += bw(type);
+                auto start = this->offset;
+                this->offset += bw(type);
                 if (bw(type) == bw(arg()))
                     return arg();
 
                 return rewriter.template create< ll::Extract >(
-                        abi_op.getLoc(), type, arg(),
-                        start, offset);
+                        this->abi_op.getLoc(), type, arg(),
+                        start, this->offset);
             };
         };
 
@@ -72,10 +105,6 @@ namespace vast::conv::abi
         vast_module mod;
         std::vector< mlir::Value > partials;
 
-        bool needs_nesting(mlir_type type) const
-        {
-            return contains_subtype< hl::RecordType >(type);
-        }
 
         mlir::Value run_on(mlir_type root_type, auto &rewriter)
         {
@@ -101,7 +130,7 @@ namespace vast::conv::abi
                                    auto &rewriter)
         {
             return rewriter.template create< hl::InitListExpr >(
-                    state.abi_op.getLoc(), type, partials).getResult(0);
+                state.abi_op.getLoc(), type, partials).getResult(0);
         }
 
       public:
@@ -125,51 +154,38 @@ namespace vast::conv::abi
     /* Aggregate type deconstruction. */
 
     template< typename pattern, typename op_t >
-    struct aggregate_deconstructor
+    struct aggregate_deconstructor : aggregate_construction_base
     {
       protected:
         using self_t = aggregate_deconstructor< pattern, op_t >;
 
-        struct state_t
+        struct state_t : aggregate_construction_base::state_t< pattern, op_t >
         {
-            std::size_t arg_idx = 0;
-            std::size_t offset = 0;
+            using base = aggregate_construction_base::state_t< pattern, op_t >;
+            using base::base;
 
-            const pattern &parent;
-            op_t abi_op;
+            using base::bw;
+            using base::advance;
 
             std::vector< mlir::Value > current;
 
-            state_t(const pattern &parent, op_t abi_op)
-                : parent(parent), abi_op(abi_op)
-            {}
-
-            state_t( const state_t &) = delete;
-            state_t( state_t &&) = delete;
-
-            state_t &operator=(state_t) = delete;
-
-            auto dst() { return abi_op.getResults().getTypes()[arg_idx]; };
-
-            auto bw(auto w) { return parent.bw(w); };
+            auto dst()
+            {
+                return this->abi_op.getResults().getTypes()[this->arg_idx];
+            }
 
             bool fits(mlir_type type)
             {
-                return bw(dst()) >= offset + bw(type);
+                return bw(dst()) >= this->offset + bw(type);
             };
-
-            void advance()
-            {
-                ++arg_idx;
-                offset = 0;
-            }
 
             mlir::Value construct(auto &rewriter)
             {
                 // `std::vector` is guaranteed to be empty after move.
                 auto out = rewriter.template create< ll::Concat >(
-                        abi_op.getLoc(),
-                        dst(), std::move(current));
+                    this->abi_op.getLoc(),
+                    dst(), std::move(current));
+
                 advance();
                 current.clear();
                 return out;
@@ -179,11 +195,11 @@ namespace vast::conv::abi
             auto allocate(mlir_type type, auto &rewriter, auto val)
                 -> std::optional< mlir::Value >
             {
-                auto start = offset;
+                auto start = this->offset;
 
                 if (fits(type))
                 {
-                    offset += bw(type);
+                    this->offset += bw(type);
                     current.push_back(val);
                     if (start + bw(type) == bw(dst()))
                         return { construct(rewriter) };
@@ -196,24 +212,24 @@ namespace vast::conv::abi
                 auto mk_int_type = [&](auto size)
                 {
                     return mlir::IntegerType::get(
-                           abi_op.getContext(),
+                           this->abi_op.getContext(),
                            size,
                            mlir::IntegerType::Signless);
                 };
 
                 auto prefix = rewriter.template create< ll::Extract >(
-                        abi_op.getLoc(), mk_int_type(breakpoint), val,
-                        0, breakpoint);
+                    this->abi_op.getLoc(), mk_int_type(breakpoint), val,
+                    0, breakpoint);
 
                 current.push_back(prefix);
                 auto to_yield = construct(rewriter);
 
                 auto suffix_size = (start + bw(type)) - bw(dst());
                 auto suffix = rewriter.template create< ll::Extract >(
-                        abi_op.getLoc(), mk_int_type(suffix_size), val,
-                        breakpoint, bw(type));
+                    this->abi_op.getLoc(), mk_int_type(suffix_size), val,
+                    breakpoint, bw(type));
 
-                offset = suffix_size;
+                this->offset = suffix_size;
                 current.push_back(suffix);
 
                 return { to_yield };
@@ -267,6 +283,11 @@ namespace vast::conv::abi
         }
     };
 
+    // Top-level hooks to perform conversion from `abi.` to executable dialect.
+    // Structures are reconstructed/deconstructed in such way that data flow
+    // can be followed without memory operations.
+
+    // Nested attributes are flattened together into one value.
     template< typename pattern_t, typename abi_op_t, typename rewriter_t >
     auto deconstruct_aggregate(const pattern_t &pattern, abi_op_t op,
                                mlir::Operation *value, rewriter_t &rewriter)
@@ -282,6 +303,8 @@ namespace vast::conv::abi
     // TODO(conv:abi): This is currently probably too restrained - figure out
     //                 if we need to constraint type, and whether it actually
     //                 needs to an argument (or we can just extract it from `op`).
+    // From one value, attributes are extracted and structures are reconstructed
+    // (including nested structures).
     template< typename pattern_t, typename abi_op_t, typename rewriter_t >
     auto reconstruct_aggregate(const pattern_t &pattern, abi_op_t op,
                                hl::RecordType record_type, rewriter_t &rewriter)
