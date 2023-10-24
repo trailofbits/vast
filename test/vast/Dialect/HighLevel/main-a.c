f947f6611dc2a991b973ab90795b3d10061aebad
Henrich Lauko <xlauko@mail.muni.cz>
2023-10-24 16:57:41 +0200

    test: Check module name.

--- test/vast/Dialect/HighLevel/main-a.c
+++ test/vast/Dialect/HighLevel/main-a.c
@@ -1,5 +1,6 @@
 // RUN: %vast-cc1 -vast-emit-mlir=hl %s -o - | %file-check %s
 // RUN: %vast-cc1 -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
 
-// CHECK-LABEL: hl.func @main () -> !hl.int
+// CHECK: module @"{{.*}}vast/Dialect/HighLevel/main-a.c"
+// CHECK: hl.func @main () -> !hl.int
 int main() {}
