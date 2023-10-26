9839adf9cc1af5da28028c787808ec4992c8581b
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-10-27 00:02:55 +0200

    test: Enable re-compilation tests with abi in pipeline.

--- test/vast/Compile/ObjectFiles/identity-a.c
+++ test/vast/Compile/ObjectFiles/identity-a.c
@@ -1,4 +1,3 @@
 // RUN: %vast-front -c -vast-pipeline=with-abi -o %t.vast.o %s && clang -c -xc %s.driver -o %t.clang.o  && clang %t.vast.o %t.clang.o -o %t && (%t; test $? -eq 0)
-// REQUIRES: fix-dl
 
 int identity(int a) { return a; }
