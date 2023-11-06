f2b90dbda110cab20a636b9f72104b38dfdff548
Henrich Lauko <xlauko@mail.muni.cz>
2023-11-06 15:19:43 +0100

    test: Add metadata checks.

--- test/vast/Dialect/HighLevel/main-a.c
+++ test/vast/Dialect/HighLevel/main-a.c
@@ -1,6 +1,16 @@
 // RUN: %vast-cc1 -vast-emit-mlir=hl %s -o - | %file-check %s
+// RUN: %vast-cc1 -vast-emit-mlir=hl -vast-show-locs %s -o - | %file-check %s -check-prefix=LOCS
+// RUN: %vast-cc1 -vast-emit-mlir=hl -vast-show-locs -vast-locs-as-meta-ids %s -o - | %file-check %s -check-prefix=META
 // RUN: %vast-cc1 -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
+
+int main() {}
 
 // CHECK: module @"{{.*}}vast/Dialect/HighLevel/main-a.c"
 // CHECK: hl.func @main () -> !hl.int
-int main() {}
+
+// LOCS: hl.func @main () -> !hl.int {
+// LOCS: } {{.*}}/main-a.c:6:5
+// LOCS: } {{.*}}/main-a.c:0:0
+
+// META: hl.func @main () -> !hl.int {
+// META: } <#meta.id<0>>
