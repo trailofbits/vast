751fb46d6f35aaf7091720ef89fda3ba0af9854c
Robert Konicar <robert.konicar@trailofbits.com>
2023-11-09 13:25:13 +0100

    test: Add test for lowering of using a typedefed function argument.

---
+++ test/vast/Transform/HL/ResolveTypedefs/func-b.c
@@ -0,0 +1,8 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %vast-opt --vast-hl-lower-typedefs | %file-check %s
+typedef int INT;
+
+// CHECK: hl.func @fn ([[A1:%arg[0-9]+]]: !hl.lvalue<!hl.int>) -> !hl.int
+int fn(INT x) {
+    // CHECK: hl.ref [[A1]] : (!hl.lvalue<!hl.int>) -> !hl.lvalue<!hl.int>
+    return x;
+}
