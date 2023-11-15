24e6579fe5bcf23cd82a6b58a715db9aa5b69aea
Lukas Korencik <lukas.korencik@trailofbits.com>
2023-11-15 15:33:48 +0100

    test:hl: Add additional type conversion tests.

---
+++ test/vast/Transform/HL/LowerTypes/fn-arg-a.c
@@ -0,0 +1,20 @@
+// RUN: %vast-cc1 -vast-emit-mlir=hl %s -o - | %vast-opt --vast-hl-lower-types | %file-check %s
+typedef enum { CURLE_OK = 0 } CURLcode ;
+
+static CURLcode base64_encode(const char * table64, const char * inputbuff,
+                              unsigned long insize, char * * outptr, unsigned long * outlen);
+
+static const char base64encdec [] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+// CHECK: hl.func @Curl_base64_encode (%arg0: !hl.lvalue<!hl.ptr<si8>>, %arg1: !hl.lvalue<ui64>, %arg2: !hl.lvalue<!hl.ptr<!hl.ptr<si8>>>, %arg3: !hl.lvalue<!hl.ptr<ui64>>) -> !hl.elaborated<!hl.typedef<"CURLcode">> {
+CURLcode Curl_base64_encode (const char *inputbuff, unsigned long insize,
+                             char **outptr, unsigned long *outlen)
+{
+    // CHECK: {{.*}} = hl.ref %arg0 : (!hl.lvalue<!hl.ptr<si8>>) -> !hl.lvalue<!hl.ptr<si8>>
+    // CHECK: {{.*}} = hl.ref %arg1 : (!hl.lvalue<ui64>) -> !hl.lvalue<ui64>
+    // CHECK: {{.*}} = hl.ref %arg2 : (!hl.lvalue<!hl.ptr<!hl.ptr<si8>>>) -> !hl.lvalue<!hl.ptr<!hl.ptr<si8>>>
+    // CHECK: {{.*}} = hl.ref %arg3 : (!hl.lvalue<!hl.ptr<ui64>>) -> !hl.lvalue<!hl.ptr<ui64>>
+
+
+    return base64_encode(base64encdec, inputbuff, insize, outptr, outlen) ;
+}
---
+++ test/vast/Transform/HL/LowerTypes/fn-ptr-a.c
@@ -0,0 +1,16 @@
+// RUN: %vast-cc1 -vast-emit-mlir=hl %s -o - | %vast-opt --vast-hl-lower-types | %file-check %s -check-prefix=LTYPES
+// RUN: %vast-cc1 -vast-emit-mlir=hl %s -o - | %vast-opt --vast-hl-lower-types --vast-hl-lower-typedefs | %file-check %s -check-prefix=LTYPEDEFS
+
+typedef void * ( * curl_malloc_callback ) ( unsigned long size ) ;
+
+extern curl_malloc_callback Curl_cmalloc ;
+
+void * malloc ( unsigned long __size );
+
+curl_malloc_callback Curl_cmalloc = ( curl_malloc_callback ) malloc ;
+
+// LTYPES: hl.typedef "curl_malloc_callback" : !hl.ptr<!core.fn<(!hl.lvalue<ui64>) -> (!hl.ptr<ui8>)>>
+// LTYPES: {{.*}} = hl.var "Curl_cmalloc" : !hl.lvalue<!hl.elaborated<!hl.typedef<"curl_malloc_callback">>> = {
+
+// LTYPEDEFS: {{.*}} = hl.var "Curl_cmalloc" sc_extern : !hl.lvalue<!hl.ptr<!core.fn<(!hl.lvalue<ui64>) -> (!hl.ptr<ui8>)>>>
+// LTYPEDEFS: {{.*}} = hl.var "Curl_cmalloc" : !hl.lvalue<!hl.ptr<!core.fn<(!hl.lvalue<ui64>) -> (!hl.ptr<ui8>)>>> = {
