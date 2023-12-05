a7d987d86e9ce7b6fcad490b134f2f758e0dc921
Robert Konicar <robert.konicar@trailofbits.com>
2023-12-05 14:31:18 +0100

    test:hl: Add tests for more complicated asm statements.

---
+++ test/vast/Dialect/HighLevel/asm-c.c
@@ -0,0 +1,17 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %file-check %s
+// RUN: %vast-front -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
+
+int frob(int x)
+{
+  int y;
+
+  // CHECK: hl.inline_asm {has_goto} #core.strlit<"frob %%r5, %1; jc %l[error]; mov (%2), %%r5">() (ins : [#core.strlit<"fst">, 1] %3, %5 : [#core.strlit<"r">, #core.strlit<"r">]) ([#core.strlit<"rax">, #core.strlit<"memory">]) (%6) : (!hl.int, !hl.ptr<!hl.int>, !hl.lvalue<!hl.ptr<!hl.void>>) -> ()
+  asm goto ("frob %%r5, %1; jc %l[error]; mov (%2), %%r5"
+            : /* No outputs. */
+            : [fst] "r"(x), "r"(&y)
+            : "rax", "memory"
+            : error);
+  return y;
+error:
+  return -1;
+}
