9b07eca0668acdd670195ba8923ce59d7684842f
Robert Konicar <robert.konicar@trailofbits.com>
2023-12-05 14:18:46 +0100

    test:hl: Add test for simple asm.

---
+++ test/vast/Dialect/HighLevel/asm-a.c
@@ -0,0 +1,5 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %file-check %s
+// RUN: %vast-front -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
+
+// CHECK: hl.inline_asm "int $3"
+void f(){asm("int $3");}
