bfd2b9c644d5d9ac697f7be577c63a5c849555bf
Henrich Lauko <xlauko@mail.muni.cz>
2023-12-05 17:55:52 +0100

    cc: Implement pipeline builder.

--- include/vast/Frontend/Pipelines.hpp
+++ include/vast/Frontend/Pipelines.hpp
@@ -9,6 +9,8 @@ VAST_RELAX_WARNINGS
 VAST_UNRELAX_WARNINGS
 
 #include "vast/Util/Pipeline.hpp"
+#include "vast/Frontend/Options.hpp"
+#include "vast/Frontend/Targets.hpp"
 
 namespace vast::cc {
 
@@ -18,4 +20,27 @@ namespace vast::cc {
 
     pipelines_config default_pipelines_config();
 
+    enum class pipeline_source { ast };
+
+    //
+    // Create pipeline schedule from source `src` to target `trg`
+    //
+    // Source can be either AST or MLIR dialect
+    //
+    // Target can be either MLIR dialect, LLVM IR or other downstream target
+    // (object file, assembly, etc.)
+    //
+    // If the target is LLVM IR or other downstream target, the pipeline will
+    // proceed into LLVM dialect.
+    //
+    // Pipeline configuration describe named pipelines that are used in the
+    // scheduled pipeline.
+    //
+    pipeline_t setup_pipeline(
+        pipeline_source src, output_type trg,
+        const vast_args &vargs,
+        const pipelines_config &config
+    );
+
 } // namespace vast::cc
+
--- lib/vast/Frontend/Pipelines.cpp
+++ lib/vast/Frontend/Pipelines.cpp
@@ -15,4 +15,113 @@ namespace vast::cc {
         }};
     }
 
+    namespace pipeline {
+
+        pipeline_step_ptr empty() {
+            return pipeline_step_init< pipeline_step >();
+        }
+
+        // Generates almost AST like MLIR, without any conversions applied
+        pipeline_step_ptr high_level() {
+            return compose(
+                hl::pipeline::canonicalize
+            );
+        }
+
+        // Simplifies high level MLIR
+        pipeline_step_ptr reduce_high_level() {
+            return compose(
+                hl::pipeline::desugar,
+                hl::pipeline::simplify
+            );
+        }
+
+        // Generates MLIR with standard types
+        pipeline_step_ptr standard_types() {
+            return compose(
+                hl::pipeline::stdtypes
+            );
+        }
+
+        // Conversion to LLVM dialects
+        pipeline_step_ptr llvm() {
+            // TODO: implement
+            return empty();
+        }
+
+        pipeline_step_ptr codegen() {
+            // TODO: pass further options to augment high level MLIR
+            return high_level();
+        }
+
+        // Defines a sequence of dialects and for each conversion dialect a
+        // passes to be run. This allows to stop conversion at any point (i.e.,
+        // specify some intermediate taarget dialect) but produce all dependend
+        // steps before.
+        using conversion_path = std::vector<
+            std::pair< target_dialect, std::vector< llvm::function_ref< pipeline_step_ptr() > > >
+        >;
+
+        conversion_path default_conversion_path = {
+            { target_dialect::high_level, { reduce_high_level } },
+            { target_dialect::std, { standard_types } },
+            { target_dialect::llvm, { llvm } }
+        };
+
+        gap::generator< pipeline_step_ptr > conversion(
+            pipeline_source src,
+            output_type trg,
+            const vast_args &vargs,
+            const pipelines_config &config
+        ) {
+            // TODO: add support for custom conversion paths
+            // deduced from source, target and vargs
+            auto path = default_conversion_path;
+
+            auto target_dialect =  [&] () -> enum target_dialect {
+                if (trg == output_type::emit_mlir) {
+                    return parse_target_dialect(vargs.get_option(opt::emit_mlir).value());
+                }
+                // if we user do not specify target dialect, we convert all the way to llvm
+                return target_dialect::llvm;
+            } ();
+
+            for (const auto &[dialect, step_passes] : path) {
+                for (auto &step : step_passes) {
+                    co_yield step();
+                }
+
+                if (target_dialect == dialect) {
+                    break;
+                }
+            }
+        }
+
+    } // namespace pipeline
+
+    std::unique_ptr< pipeline_t > setup_pipeline(
+        pipeline_source src,
+        output_type trg,
+        mcontext_t &mctx,
+        const vast_args &vargs,
+        const pipelines_config &config
+    ) {
+        auto passes = std::make_unique< pipeline_t >(&mctx);
+
+        // generate high level MLIR in case of AST input
+        if (pipeline_source::ast == src) {
+            *passes << pipeline::codegen();
+        }
+
+        // apply desired conversion to target dialect, if target is llvm or
+        // binary/assembly, we perform entire conversion to llvm dialect vargs
+        // can specify how we want to convert to llvm dialect and allows to turn
+        // off optional pipelines
+        for (auto &&step : pipeline::conversion(src, trg, vargs, config)) {
+            *passes << std::move(step);
+        }
+
+        return passes;
+    }
+
 } // namespace vast::cc
