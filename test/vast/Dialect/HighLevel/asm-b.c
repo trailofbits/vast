a9216eb4a4c1a004df8aebe70c7b249d61a17936
Robert Konicar <robert.konicar@trailofbits.com>
2023-12-05 14:31:18 +0100

    test:hl: Add tests for more complicated asm statements.

---
+++ test/vast/Dialect/HighLevel/asm-b.c
@@ -0,0 +1,12 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %file-check %s
+// RUN: %vast-front -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
+
+void f() {
+int src = 1;
+int dst;
+// CHECK: hl.inline_asm {is_volatile} #core.strlit<"mov %1, %0\n\tadd $1, %0">([0] %2 : [#core.strlit<"=r">]) (ins : [1] %4 : [#core.strlit<"r">]) () () : (!hl.lvalue<!hl.int>, !hl.int) -> ()
+asm inline volatile ("mov %1, %0\n\t"
+    "add $1, %0"
+    : "=r" (dst)
+    : "r" (src));
+}
