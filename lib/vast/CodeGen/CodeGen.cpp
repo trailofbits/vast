363b8969372e66974f38e22735f28dff09550e3a
xlauko <xlauko@mail.muni.cz>
2024-02-08 13:28:59 +0100

    cg: Remove obsolete codegen.

--- lib/vast/CodeGen/CodeGen.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2022-present, Trail of Bits, Inc.
-
-#include "vast/CodeGen/CodeGen.hpp"
-
-namespace vast::cg
-{
-} // namespace vast::cg
--- lib/vast/CodeGen/CodeGenScope.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2023-present, Trail of Bits, Inc.
-
-#include "vast/CodeGen/CodeGenScope.hpp"
-#include "vast/CodeGen/CodeGenContext.hpp"
-
-namespace vast::cg
-{
-
-} // namespace vast::cg
