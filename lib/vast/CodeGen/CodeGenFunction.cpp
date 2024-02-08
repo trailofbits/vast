18bba0a2854a9e6fd69140e49cd3b7e99e450d4b
xlauko <xlauko@mail.muni.cz>
2024-02-08 16:20:07 +0100

    cg: Clean up function decl naming.

--- lib/vast/CodeGen/CodeGenFunction.cpp
+++ lib/vast/CodeGen/CodeGenFunction.cpp
@@ -28,7 +28,7 @@ namespace vast::cg
         }
 
         template< typename generator_t >
-        auto generate(emition_kind emition, clang::FunctionDecl *decl, mangler_t &mangler)
+        auto generate(emition_kind emition, clang_function *decl, mangler_t &mangler)
             -> std::unique_ptr< generator_t >
         {
             return generate< generator_t >(emition, [decl, &mangler] (auto *gen) {
@@ -37,7 +37,7 @@ namespace vast::cg
         }
 
         template< typename generator_t >
-        auto generate(emition_kind emition, clang::FunctionDecl *decl)
+        auto generate(emition_kind emition, clang_function *decl)
             -> std::unique_ptr< generator_t >
         {
             return generate< generator_t >(emition, [decl] (auto *gen) {
@@ -46,37 +46,37 @@ namespace vast::cg
         }
     } // namespace detail
 
-    void function_generator::emit(clang::FunctionDecl *decl, mangler_t &mangler) {
+    void function_generator::emit(clang_function *decl, mangler_t &mangler) {
         hook(generate_prototype(decl, mangler));
         hook(generate_body(decl, emition_kind::deferred));
     }
 
-    void prototype_generator::emit(clang::FunctionDecl *decl, mangler_t &mangler) {
+    void prototype_generator::emit(clang_function *decl, mangler_t &mangler) {
         VAST_UNIMPLEMENTED;
     }
 
-    void body_generator::emit(clang::FunctionDecl *decl) {
+    void body_generator::emit(clang_function *decl) {
         VAST_UNIMPLEMENTED;
         emit_epilogue(decl);
     }
 
-    void body_generator::emit_epilogue(clang::FunctionDecl *decl) {
+    void body_generator::emit_epilogue(clang_function *decl) {
         VAST_UNIMPLEMENTED;
     }
 
-    auto generate_function(clang::FunctionDecl *decl, mangler_t &mangler)
+    auto generate_function(clang_function *decl, mangler_t &mangler)
         -> std::unique_ptr< function_generator >
     {
         return detail::generate< function_generator >(emition_kind::immediate, decl, mangler);
     }
 
-    auto generate_prototype(clang::FunctionDecl *decl, mangler_t &mangler)
+    auto generate_prototype(clang_function *decl, mangler_t &mangler)
         -> std::unique_ptr< prototype_generator >
     {
         return detail::generate< prototype_generator >(emition_kind::immediate, decl, mangler);
     }
 
-    auto generate_body(clang::FunctionDecl *decl, emition_kind emition)
+    auto generate_body(clang_function *decl, emition_kind emition)
         -> std::unique_ptr< body_generator >
     {
         return detail::generate< body_generator >(emition, decl);
