4d0dab0838d7c4cdfb00d9395490a860a75871f9
xlauko <xlauko@mail.muni.cz>
2024-03-05 11:44:53 +0100

    test: Simplify conversion invocations.

--- test/vast/Conversion/Common/IRsToLLVM/struct-a.c
+++ test/vast/Conversion/Common/IRsToLLVM/struct-a.c
@@ -1,4 +1,4 @@
-// RUN: %vast-cc1 -vast-emit-mlir=hl %s -o - | %vast-opt --vast-hl-lower-types --vast-hl-to-ll-cf --vast-hl-to-ll-vars --vast-irs-to-llvm | %file-check %s
+// RUN: %vast-cc1 -vast-emit-mlir=hl %s -o - | %vast-opt-irs-to-llvm | %file-check %s
 
 struct X { int x; };
 
