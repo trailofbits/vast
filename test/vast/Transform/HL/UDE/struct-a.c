da786d0729090f56a648f7ceed4e6bdf5f585b98
xlauko <xlauko@mail.muni.cz>
2024-03-06 17:06:03 +0100

    test: Add UDE struct/union passes.

---
+++ test/vast/Transform/HL/UDE/struct-a.c
@@ -0,0 +1,11 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %vast-opt --vast-hl-ude | %file-check %s
+
+// CHECK: hl.struct "used"
+struct used {};
+
+// CHECK-NOT: hl.struct "unused"
+struct unused {};
+
+int main() {
+    struct used u;
+}
---
+++ test/vast/Transform/HL/UDE/union-b.c
@@ -0,0 +1,10 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %vast-opt --vast-hl-ude | %file-check %s
+
+// CHECK-NOT: hl.struct "used"
+struct used {};
+
+// CHECK-NOT: hl.union "unused_union"
+union unused_union
+{
+    struct used s;
+};
