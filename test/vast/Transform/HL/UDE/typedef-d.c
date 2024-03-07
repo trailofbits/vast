c772e825ee11c73e2bf4384aa34c4bbd13bbb3f1
xlauko <xlauko@mail.muni.cz>
2024-03-07 12:48:19 +0100

    test: Add typedef UDE tests.

---
+++ test/vast/Transform/HL/UDE/typedef-d.c
@@ -0,0 +1,10 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %vast-opt --vast-hl-ude | %file-check %s
+
+// CHECK-NOT: hl.struct "used"
+struct unused_struct {};
+
+// CHECK-NOT: hl.typedef "unused_typedef"
+typedef struct unused_struct unused_typedef;
+
+// CHECK-NOT: hl.typedef "used_typedef"
+typedef unused_typedef unused_transitive_typedef;
