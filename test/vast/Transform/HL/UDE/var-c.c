44ef29e32466282bd2ee1f513bec72b796e877c8
xlauko <xlauko@mail.muni.cz>
2024-03-07 22:10:59 +0100

    test: Add tests on unused global variables elimination.

---
+++ test/vast/Transform/HL/UDE/var-c.c
@@ -0,0 +1,10 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %vast-opt --vast-hl-ude | %file-check %s
+
+// CHECK-NOT: hl.typedecl "unused_struct"
+struct unused_struct;
+
+// CHECK-NOT: hl.typedef "unused_t"
+typedef struct unused_struct unused_t;
+
+// CHECK-NOT: hl.var "unused"
+extern unused_t unused;
