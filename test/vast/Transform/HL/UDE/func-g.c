31a6222a50eababb4742bbda63dad7c29093fa8d
xlauko <xlauko@mail.muni.cz>
2024-03-07 21:20:58 +0100

    test: Add always inline function UDE test.

---
+++ test/vast/Transform/HL/UDE/func-g.c
@@ -0,0 +1,18 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %vast-opt --vast-hl-ude | %file-check %s
+
+// CHECK-NOT: hl.struct "unused"
+struct unused {};
+
+// CHECK: hl.struct "used"
+struct used {};
+
+// CHECK-NOT: hl.func @inlined
+__attribute__((always_inline)) void inlined() {
+    struct unused un;
+    struct used u;
+}
+
+// CHECK: hl.func @not_inlined
+void not_inlined() {
+    struct used u;
+}
