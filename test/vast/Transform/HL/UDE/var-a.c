87cf709f7201648550c168ea0a2d84c23440501b
xlauko <xlauko@mail.muni.cz>
2024-03-07 22:10:59 +0100

    test: Add tests on unused global variables elimination.

---
+++ test/vast/Transform/HL/UDE/var-a.c
@@ -0,0 +1,15 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %vast-opt --vast-hl-ude | %file-check %s
+
+// CHECK: hl.var "a"
+int a;
+
+// CHECK-NOT: hl.var "e"
+extern int e;
+
+// CHECK: hl.var "s"
+static int s;
+
+void local(void) {
+    // CHECK: hl.var "l"
+    int l;
+}
---
+++ test/vast/Transform/HL/UDE/var-b.c
@@ -0,0 +1,8 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %vast-opt --vast-hl-ude | %file-check %s
+
+// CHECK: hl.var "used"
+extern int used;
+
+void use() {
+    used = 0;
+}
