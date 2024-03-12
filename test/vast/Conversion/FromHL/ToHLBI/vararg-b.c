195655b7efbc322236918e9b2c314f89d50a6d9d
Robert Konicar <robert.konicar@trailofbits.com>
2024-03-12 10:35:06 +0100

    test: Expand tests for va_list.

--- test/vast/Conversion/FromHL/ToHLBI/vararg-b.c
+++ test/vast/Conversion/FromHL/ToHLBI/vararg-b.c
@@ -1,5 +1,4 @@
-// RUN: %vast-front -vast-emit-mlir=hl -o - %s | %file-check %s
-// RUN: %vast-front -vast-emit-mlir=hl -o - %s > %t && %vast-opt %t | diff -B %t -
+// RUN: %vast-front -vast-emit-mlir=hl -o - %s | %vast-opt --vast-hl-to-hl-builtin | %file-check %s
 
 typedef __builtin_va_list va_list;
 
@@ -16,10 +15,10 @@ int average(int count, ...) {
 // CHECK: hl.func @{{.*}}average{{.*}}(%arg0: !hl.lvalue<!hl.int>, ...) -> !hl.int
     va_list args, args_copy;
     va_start(args, count);
-    // CHECK: hl.call @__builtin_va_start
+    // CHECK: hlbi.va_start
 
     va_copy(args_copy, args);
-    // CHECK: hl.call @__builtin_va_copy
+    // CHECK: hlbi.va_copy
 
     int sum = 0;
     for(int i = 0; i < count; i++) {
@@ -28,7 +27,7 @@ int average(int count, ...) {
     }
 
     va_end(args);
-    // CHECK: hl.call @__builtin_va_end
+    // CHECK: hlbi.va_end
 
     return count > 0 ? sum / count : 0;
 }
