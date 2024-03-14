969de9c807767158f7b3e624e0b09a386c1f7e2d
xlauko <xlauko@mail.muni.cz>
2024-03-14 22:15:44 +0100

    cg: Add return types to visitation methods.

--- include/vast/CodeGen/CodeGenVisitor.hpp
+++ include/vast/CodeGen/CodeGenVisitor.hpp
@@ -16,24 +16,46 @@ namespace vast::cg
 {
     struct default_visitor : visitor_base
     {
-        void Visit(clang::Decl *decl) override
+        operation visit(clang_decl *decl) override
         {
             VAST_UNIMPLEMENTED;
         }
 
-        void Visit(clang::Stmt *stmt) override
+        operation visit(clang_stmt *stmt) override
         {
             VAST_UNIMPLEMENTED;
         }
 
-        void Visit(clang::Type *type) override
+        mlir_type visit(clang_type *type) override
         {
             VAST_UNIMPLEMENTED;
         }
 
-        void Visit(clang::Attr *attr) override
+        mlir_type visit(clang_qual_type ty) override
         {
             VAST_UNIMPLEMENTED;
+        }
+
+        mlir_attr visit(clang_attr *attr) override
+        {
+            VAST_UNIMPLEMENTED;
+        }
+
+        mlir_type visit_as_lvalue_type(clang_qual_type ty) override
+        {
+            VAST_UNIMPLEMENTED;
+        }
+
+        mlir_type visit_function_type(const clang_function_type *fty, bool is_variadic) override
+        {
+            llvm::SmallVector< mlir_type > args;
+            if (auto proto = clang::dyn_cast< clang_function_proto_type >(fty)) {
+                for (auto param : proto->getParamTypes()) {
+                    args.push_back(visit_as_lvalue_type(param));
+                }
+            }
+
+            return core::FunctionType::get(args, {visit(fty->getReturnType())}, is_variadic);
         }
     };
 
