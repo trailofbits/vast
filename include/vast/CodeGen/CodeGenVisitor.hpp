467ea25356e31b375e32c6e2f0801d676c266d88
xlauko <xlauko@mail.muni.cz>
2024-03-15 09:00:50 +0100

    cg: Create dynamic fallback visitor.

--- include/vast/CodeGen/CodeGenVisitor.hpp
+++ include/vast/CodeGen/CodeGenVisitor.hpp
@@ -5,48 +5,18 @@
 #include "vast/CodeGen/CodeGenScope.hpp"
 #include "vast/Util/Warnings.hpp"
 
-#include "vast/CodeGen/CodeGenDeclVisitor.hpp"
-#include "vast/CodeGen/CodeGenStmtVisitor.hpp"
-#include "vast/CodeGen/CodeGenTypeVisitor.hpp"
-#include "vast/CodeGen/CodeGenAttrVisitor.hpp"
 #include "vast/CodeGen/CodeGenVisitorBase.hpp"
 #include "vast/CodeGen/FallBackVisitor.hpp"
 
 namespace vast::cg
 {
-    struct default_visitor : visitor_base
+    struct default_visitor : fallback_visitor
     {
-        operation visit(clang_decl *decl) override
-        {
-            VAST_UNIMPLEMENTED;
-        }
+        using fallback_visitor::fallback_visitor;
 
-        operation visit(clang_stmt *stmt) override
-        {
-            VAST_UNIMPLEMENTED;
-        }
+        using fallback_visitor::visit;
 
-        mlir_type visit(clang_type *type) override
-        {
-            VAST_UNIMPLEMENTED;
-        }
-
-        mlir_type visit(clang_qual_type ty) override
-        {
-            VAST_UNIMPLEMENTED;
-        }
-
-        mlir_attr visit(clang_attr *attr) override
-        {
-            VAST_UNIMPLEMENTED;
-        }
-
-        mlir_type visit_as_lvalue_type(clang_qual_type ty) override
-        {
-            VAST_UNIMPLEMENTED;
-        }
-
-        mlir_type visit_function_type(const clang_function_type *fty, bool is_variadic) override
+        mlir_type visit(const clang_function_type *fty, bool is_variadic) override
         {
             llvm::SmallVector< mlir_type > args;
             if (auto proto = clang::dyn_cast< clang_function_proto_type >(fty)) {
