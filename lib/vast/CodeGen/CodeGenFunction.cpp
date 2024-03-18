639e4a096d29c39331853c48c876a308d66af6b3
xlauko <xlauko@mail.muni.cz>
2024-03-18 12:53:17 +0100

    cg: Generate function prototypes.

--- lib/vast/CodeGen/CodeGenFunction.cpp
+++ lib/vast/CodeGen/CodeGenFunction.cpp
@@ -8,37 +8,115 @@ VAST_RELAX_WARNINGS
 VAST_UNRELAX_WARNINGS
 
 #include "vast/CodeGen/CodeGenModule.hpp"
+#include "vast/CodeGen/Util.hpp"
+
+#include "vast/Dialect/Core/Linkage.hpp"
 
 namespace vast::cg
 {
-   void function_generator::emit(clang_function *decl) {
+   vast_function function_generator::emit(clang_function *decl) {
         auto ctx = dynamic_cast< module_context* >(parent);
         VAST_CHECK(ctx, "function generator must be a child of a module context");
-        generate_child< prototype_generator >(decl);
+        function = generate_child< prototype_generator >(decl);
         defer([=] { generate_child< body_generator >(decl); });
+        return function;
     }
 
-    void prototype_generator::emit(clang_function *decl) {
+    vast_function prototype_generator::emit(clang_function *decl) {
         auto ctx = dynamic_cast< function_context* >(parent);
         VAST_CHECK(ctx, "prototype generator must be a child of a function context");
 
         auto mod = dynamic_cast< module_context* >(ctx->parent);
         VAST_CHECK(mod, "function context must be a child of a module context");
 
+        auto fty = visitor.visit(decl->getFunctionType(), decl->isVariadic());
         if (auto proto = get_global_value(mod, clang_global(decl))) {
-            return;
+            VAST_ASSERT(mlir::isa< vast_function >(proto));
+            VAST_ASSERT(mlir::cast< vast_function >(proto).getFunctionType() == fty);
+            function = mlir::cast< vast_function >(proto);
+        } else {
+            auto loc = visitor.location(decl);
+            auto mangled_name = get_mangled_name(mod, decl);
+            auto linkage = core::get_function_linkage(decl);
+            auto visibility = get_function_visibility(decl, linkage);
+            auto attrs = get_function_attrs(decl);
+
+            function = declare(
+                loc, mangled_name, fty, linkage, visibility, std::move(attrs)
+            );
         }
 
-        // emit_function_type(decl->getFunctionType(), decl->isVariadic());
-        // get_or_create_vast_function(mangled_name, fty, decl, emit);
-        // auto fn = create_vast_function(meta_location(decl), mangled_name, fty, function_decl);
+        return function;
     }
 
-    // mlir_type prototype_generator::emit_function_type(
-    //     const clang_function_type *fty, bool is_variadic
-    // ) {
-    //     VAST_UNIMPLEMENTED;
-    // }
+    vast_function prototype_generator::declare(
+        loc_t loc,
+        mangled_name_ref mangled_name,
+        mlir_type ty,
+        linkage_kind linkage,
+        mlir_visibility visibility,
+        mlir_attr_list attrs
+    ) {
+        // At the point we need to create the function, the insertion point
+        // could be anywhere (e.g. callsite). Do not rely on whatever it might
+        // be, properly save, find the appropriate place and restore.
+        auto guard = visitor.insertion_guard();
+
+        auto fty = mlir::cast< core::FunctionType >(ty);
+        auto fn = visitor.builder().create< hl::FuncOp >(
+            loc, mangled_name.name, fty, linkage
+        );
+
+        attrs.append(fn->getAttrs());
+        fn->setAttrs(attrs);
+
+        mlir::SymbolTable::setSymbolVisibility(fn, visibility);
+
+        scope_context::declare(fn);
+        return fn;
+    }
+
+    mlir_attr_list prototype_generator::get_function_attrs(clang_function *decl) {
+        if (!decl->hasAttrs())
+            return {};
+
+        // These are already handled by linkage attributes
+        using excluded_attr_list = util::type_list<
+              clang::WeakAttr
+            , clang::SelectAnyAttr
+            , clang::CUDAGlobalAttr
+        >;
+
+        mlir_attr_list attrs;
+        for (auto attr : exclude_attrs< excluded_attr_list >(decl->getAttrs())) {
+            auto visited = visitor.visit(attr);
+
+            auto spelling = attr->getSpelling();
+            // Bultin attr doesn't have spelling because it can not be written in code
+            if (auto builtin = clang::dyn_cast< clang::BuiltinAttr >(attr)) {
+                spelling = "builtin";
+            }
+
+            if (auto prev = attrs.getNamed(spelling)) {
+                VAST_CHECK(visited == prev.value().getValue(), "Conflicting redefinition of attribute {0}", spelling);
+            }
+
+            attrs.set(spelling, visited);
+        }
+
+        return attrs;
+    }
+
+
+    mlir_visibility prototype_generator::get_function_visibility(
+        clang_function *decl, linkage_kind linkage
+    ) {
+        if (decl->isThisDeclarationADefinition())
+            return core::get_visibility_from_linkage(linkage);
+        if (decl->doesDeclarationForceExternallyVisibleDefinition())
+            return mlir_visibility::Public;
+        return mlir_visibility::Private;
+    }
 
     void body_generator::emit(clang_function *decl) {
         emit_epilogue(decl);
