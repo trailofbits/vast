244b873b9a039d210775e8e4939ef98c43b57b29
xlauko <xlauko@mail.muni.cz>
2024-03-21 15:09:05 +0100

    cg: Make symbol generator customizable.

--- include/vast/CodeGen/Mangler.hpp
+++ include/vast/CodeGen/Mangler.hpp
@@ -13,79 +13,33 @@ VAST_RELAX_WARNINGS
 #include <llvm/Support/Allocator.h>
 VAST_UNRELAX_WARNINGS
 
-#include "vast/Util/Common.hpp"
-
-namespace vast::cg
-{
-    struct mangled_name_ref {
-        using value_type = string_ref;
-
-        value_type name;
-
-        friend std::strong_ordering operator<=>(const mangled_name_ref &a, const mangled_name_ref &b) {
-            return a.name == b.name ? std::strong_ordering::equal :
-                   a.name < b.name  ? std::strong_ordering::less
-                                    : std::strong_ordering::greater;
-        }
-    };
-} // namespace vast::cg
-
-namespace llvm {
-    // Compute a hash_code for a mangled_name_ref
-
-    [[nodiscard]] hash_code hash_value(vast::cg::mangled_name_ref mangled);
-
-    // Provide DenseMapInfo for mangled_name_refs
-    template<>
-    struct DenseMapInfo< vast::cg::mangled_name_ref, void > {
-        using info_type  = vast::cg::mangled_name_ref;
-        using value_type = info_type::value_type;
-        using base = DenseMapInfo< value_type, void >;
-
-        static inline info_type getEmptyKey() {
-            return { base::getEmptyKey() };
-        }
-
-        static inline info_type getTombstoneKey() {
-            return { base::getTombstoneKey() };
-        }
-
-        static unsigned getHashValue(info_type val) {
-            return base::getHashValue(val.name);
-        }
-
-        static bool isEqual(info_type lhs, info_type rhs) {
-            return base::isEqual(lhs.name, rhs.name);
-        }
-    };
-} // namespace llvm
+#include "vast/CodeGen/Common.hpp"
+#include "vast/CodeGen/SymbolGenerator.hpp"
 
 namespace vast::cg
 {
     using target_info = clang::TargetInfo;
 
-    struct mangler_t {
+    using mangle_context = clang::MangleContext;
 
-        explicit mangler_t(clang::MangleContext *mangle_context)
-            : mangle_context(mangle_context)
+    struct default_symbol_mangler final : symbol_generator {
+
+        explicit default_symbol_mangler(mangle_context *mangle_context, const std::string &module_name_hash = "")
+            : mangle_context(mangle_context), module_name_hash(module_name_hash)
         {}
 
-        mangled_name_ref get_mangled_name(
-            clang_global decl, const target_info &target_info, const std::string &module_name_hash
-        ) const;
-
-        std::optional< clang_global > lookup_representative_decl(mangled_name_ref name) const;
+        std::optional< symbol_name > symbol(clang_global decl) override;
 
       private:
-        std::string mangle(
-            clang_global decl, const std::string &module_name_hash
-        ) const;
+        std::optional< symbol_name > symbol(const clang_named_decl *decl);
+        std::optional< std::string > mangle(const clang_named_decl *decl);
 
-        std::unique_ptr< clang::MangleContext > mangle_context;
+        std::unique_ptr< mangle_context > mangle_context;
+        const std::string module_name_hash = "";
 
         // An ordered map of canonical GlobalDecls to their mangled names.
-        mutable llvm::MapVector< clang_global, mangled_name_ref > mangled_decl_names;
-        mutable llvm::StringMap< clang_global, llvm::BumpPtrAllocator > manglings;
+        llvm::MapVector< clang_global, symbol_name > mangled_decl_names;
+        llvm::StringMap< clang_global, llvm::BumpPtrAllocator > manglings;
     };
 
 } // namespace vast::cg
---
+++ include/vast/CodeGen/SymbolGenerator.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2024, Trail of Bits, Inc.
+
+#pragma once
+
+#include "vast/CodeGen/Common.hpp"
+
+namespace vast::cg {
+
+    using symbol_name = string_ref;
+
+    struct symbol_generator
+    {
+        virtual ~symbol_generator() = default;
+
+        virtual std::optional< symbol_name > symbol(clang_global decl) = 0;
+    };
+
+} // namespace vast::cg
