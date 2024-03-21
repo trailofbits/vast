f52947aa278349f1e49ec7b4079d2d4eecc7c736
xlauko <xlauko@mail.muni.cz>
2024-03-21 15:09:43 +0100

    cg: Extend monadic builder utilities.

--- include/vast/CodeGen/CodeGenBuilder.hpp
+++ include/vast/CodeGen/CodeGenBuilder.hpp
@@ -5,6 +5,7 @@
 #include "vast/Util/Warnings.hpp"
 
 #include "vast/Util/Common.hpp"
+#include "vast/Util/Maybe.hpp"
 
 #include "vast/Dialect/Core/CoreOps.hpp"
 #include "vast/Dialect/Core/CoreAttributes.hpp"
@@ -58,20 +59,47 @@ namespace vast::cg {
         template< typename ...args_t >
         static constexpr bool valid(const args_t &...args) { return (valid(args) && ...); }
 
+        template< typename arg_t >
+        constexpr inline auto bind(std::optional< arg_t > &&arg) && {
+            auto bound = [arg = std::move(arg), binder = std::move(binder)] (auto &&...rest) {
+                if (!arg.has_value() || !valid(arg.value())) {
+                    return result_type{};
+                }
+                return binder(std::move(arg.value()), std::forward< decltype(rest) >(rest)...);
+            };
+            return compose_state_t< result_type, decltype(bound) >(std::move(bound));
+        }
+
         template< typename ...args_t >
         constexpr inline auto bind(args_t &&...args) && {
-            auto binded = [... args = std::forward< args_t >(args), binder = std::move(binder)] (auto &&...rest) {
+            auto bound = [... args = std::forward< args_t >(args), binder = std::move(binder)] (auto &&...rest) {
                 if (!valid(args...)) {
                     return result_type{};
                 }
                 return binder(args..., std::forward< decltype(rest) >(rest)...);
             };
-            return compose_state_t< result_type, decltype(binded) >(std::move(binded));
+            return compose_state_t< result_type, decltype(bound) >(std::move(bound));
+        }
+
+        template< typename target_t, typename arg_t >
+        constexpr inline auto bind_dyn_cast(arg_t &&arg) && {
+            auto bound = [arg = std::forward< arg_t >(arg), binder = std::move(binder)] (auto &&...rest) {
+                if (!valid(arg)) {
+                    return result_type{};
+                }
+
+                if (auto casted = dyn_cast< target_t >(arg)) {
+                    return binder(casted, std::forward< decltype(rest) >(rest)...);
+                }
+
+                return result_type{};
+            };
+            return compose_state_t< result_type, decltype(bound) >(std::move(bound));
         }
 
         template< typename arg_t >
         constexpr inline auto bind_if(bool cond, arg_t &&arg) && {
-            auto binded = [cond, arg = std::forward< arg_t >(arg), binder = std::move(binder)] (auto &&...args) {
+            auto bound = [cond, arg = std::forward< arg_t >(arg), binder = std::move(binder)] (auto &&...args) {
                 if (cond) {
                     if (!valid(arg)) {
                         return result_type{};
@@ -82,43 +110,64 @@ namespace vast::cg {
 
                 return binder(std::forward< decltype(args) >(args)...);
             };
-            return compose_state_t< result_type, decltype(binded) >(std::move(binded));
+            return compose_state_t< result_type, decltype(bound) >(std::move(bound));
         }
 
         template< typename arg_t >
         constexpr inline auto bind_if_valid(arg_t &&arg) && {
-            auto binded = [arg = std::forward< arg_t >(arg), binder = std::move(binder)] (auto &&...args) {
+            auto bound = [arg = std::forward< arg_t >(arg), binder = std::move(binder)] (auto &&...args) {
                 if (valid(arg)) {
                     return binder(arg, std::forward< decltype(args) >(args)...);
                 }
 
                 return binder(std::forward< decltype(args) >(args)...);
             };
-            return compose_state_t< result_type, decltype(binded) >(std::move(binded));
+            return compose_state_t< result_type, decltype(bound) >(std::move(bound));
         }
 
-        template< typename arg_t >
+        template <typename arg_t>
         constexpr inline auto bind_region_if(bool cond, arg_t &&arg) && {
-            auto binded = [cond, arg = std::forward< arg_t >(arg), binder = std::move(binder)] (auto &&...args) {
+            auto bound = [cond, arg = std::forward<arg_t>(arg), binder = std::move(binder)](auto &&...args) {
                 if (cond) {
                     if (!valid(arg)) {
                         return result_type{};
                     }
-
-                    return binder(arg, std::forward< decltype(args) >(args)...);
+                    return binder(arg, std::forward<decltype(args)>(args)...);
                 }
-                return binder(std::nullopt, std::forward< decltype(args) >(args)...);
+                return binder(std::nullopt, std::forward<decltype(args)>(args)...);
             };
-            return compose_state_t< result_type, decltype(binded) >(std::move(binded));
+            return compose_state_t<result_type, decltype(bound)>(std::move(bound));
         }
 
         auto freeze() { return binder(); }
+
+        auto freeze_as_maybe() {
+            return Maybe< result_type >(binder());
+        }
 
         bind_type binder;
     };
 
     struct codegen_builder : mlir_builder {
         using mlir_builder::mlir_builder;
+
+        insertion_guard insertion_guard() { return { *this }; }
+
+        void set_insertion_point_to_start(region_ptr region) {
+            set_insertion_point_to_start(&region->front());
+        }
+
+        void set_insertion_point_to_end(region_ptr region) {
+            set_insertion_point_to_end(&region->back());
+        }
+
+        void set_insertion_point_to_start(block_ptr block) {
+            setInsertionPointToStart(block);
+        }
+
+        void set_insertion_point_to_end(block_ptr block) {
+            setInsertionPointToEnd(block);
+        }
 
         template< typename result_type, typename builder_type >
         auto compose_start(builder_type &&builder) {
