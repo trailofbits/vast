2259c2f15d3b266b67e345df452264b574d8436e
xlauko <xlauko@mail.muni.cz>
2024-03-27 08:46:36 +0100

    cg: Setup visitors facede.

--- include/vast/CodeGen/DefaultDeclVisitor.hpp
+++ include/vast/CodeGen/DefaultDeclVisitor.hpp
@@ -22,20 +22,26 @@ namespace vast::cg {
 
         using decl_visitor_base< default_decl_visitor >::Visit;
 
-        //
-        // Function Declaration
-        //
-
         operation visit(const clang_decl *decl) { return Visit(decl); }
         operation visit_prototype(const clang_function *decl);
         mlir_attr_list visit_attrs(const clang_function *decl);
 
-        //
-        // Variable Declaration
-        //
-
         operation VisitVarDecl(const clang_var_decl *decl);
-        operation visit_var_init(const clang_var_decl *decl);
+        operation VisitParmVarDecl(const clang::ParmVarDecl *decl);
+        operation VisitImplicitParamDecl(const clang::ImplicitParamDecl *decl);
+        operation VisitLinkageSpecDecl(const clang::LinkageSpecDecl *decl);
+        operation VisitTranslationUnitDecl(const clang::TranslationUnitDecl *decl);
+        operation VisitTypedefNameDecl(const clang::TypedefNameDecl *decl);
+        operation VisitTypedefDecl(const clang::TypedefDecl *decl);
+        operation VisitTypeAliasDecl(const clang::TypeAliasDecl *decl);
+        operation VisitLabelDecl(const clang::LabelDecl *decl);
+        operation VisitEmptyDecl(const clang::EmptyDecl *decl);
+        operation VisitEnumDecl(const clang::EnumDecl *decl);
+        operation VisitEnumConstantDecl(const clang::EnumConstantDecl *decl);
+        operation VisitRecordDecl(const clang::RecordDecl *decl);
+        operation VisitCXXRecordDecl(const clang::CXXRecordDecl *decl);
+        operation VisitAccessSpecDecl(const clang::AccessSpecDecl *decl);
+        operation VisitFieldDecl(const clang::FieldDecl *decl) ;
     };
 
 } // namespace vast::cg
