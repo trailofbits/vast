7d4843ef4b462d9e6cf37b705233a9d22db6bd04
xlauko <xlauko@mail.muni.cz>
2024-04-17 11:13:32 +0200

    scripts: Add llvm dependencies setup script.

---
+++ scripts/setup_llvm_dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set default LLVM version if not specified
+LLVM_VERSION=${LLVM_VERSION:-17}
+
+# Install LLVM tools and libraries
+bash -c "$(curl -s -o - https://apt.llvm.org/llvm.sh)" llvm.sh $LLVM_VERSION
+
+# Install required packages dynamically based on the LLVM version
+apt-get update && apt-get install -y -q \
+ libstdc++-12-dev \
+ llvm-${LLVM_VERSION} \
+ libmlir-${LLVM_VERSION} \
+ libmlir-${LLVM_VERSION}-dev \
+ mlir-${LLVM_VERSION}-tools \
+ libclang-${LLVM_VERSION}-dev
