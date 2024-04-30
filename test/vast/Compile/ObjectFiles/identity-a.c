6668ec5b7ea8f2e054cbf305be23d7f8137c8aba
Robert Konicar <robert.konicar@trailofbits.com>
2024-04-30 15:28:00 +0200

    test: Use cc instead of hard-coded clang-X binary.

--- test/vast/Compile/ObjectFiles/identity-a.c
+++ test/vast/Compile/ObjectFiles/identity-a.c
@@ -1,3 +1,3 @@
-// RUN: %vast-front -c -vast-pipeline=with-abi -o %t.vast.o %s && %clang -c -xc %s.driver -o %t.clang.o  && %clang %t.vast.o %t.clang.o -o %t && (%t; test $? -eq 0)
+// RUN: %vast-front -c -vast-pipeline=with-abi -o %t.vast.o %s && %cc -c -xc %s.driver -o %t.clang.o  && %cc %t.vast.o %t.clang.o -o %t && (%t; test $? -eq 0)
 
 int identity(int a) { return a; }
