e299e6fde002ae1f7a51f55865af9de00796a009
xlauko <xlauko@mail.muni.cz>
2024-05-04 14:14:11 +0200

    cg: Move default visitor implementation to cpp file.

--- include/vast/CodeGen/DefaultVisitor.hpp
+++ include/vast/CodeGen/DefaultVisitor.hpp
@@ -22,47 +22,13 @@ namespace vast::cg
             : visitor_base(mctx, mg, sg, self.options()), bld(bld), self(self)
         {}
 
-        operation visit(const clang_decl *decl, scope_context &scope) override {
-            default_decl_visitor visitor(bld, self, scope);
-            return visitor.visit(decl);
-        }
+        operation visit(const clang_decl *decl, scope_context &scope) override;
+        operation visit(const clang_stmt *stmt, scope_context &scope) override;
+        mlir_type visit(const clang_type *type, scope_context &scope) override;
+        mlir_type visit(clang_qual_type type, scope_context &scope) override;
+        mlir_attr visit(const clang_attr *attr, scope_context &scope) override;
 
-        operation visit(const clang_stmt *stmt, scope_context &scope) override {
-            default_stmt_visitor visitor(bld, self, scope);
-            return visitor.visit(stmt);
-        }
-
-        mlir_type visit(const clang_type *type, scope_context &scope) override {
-            if (auto value = cache.lookup(type)) {
-                return value;
-            }
-
-            default_type_visitor visitor(bld, self, scope);
-            auto result = visitor.visit(type);
-            cache.try_emplace(type, result);
-            return result;
-        }
-
-        mlir_type visit(clang_qual_type type, scope_context &scope) override {
-            if (auto value = qual_cache.lookup(type)) {
-                return value;
-            }
-
-            default_type_visitor visitor(bld, self, scope);
-            auto result = visitor.visit(type);
-            qual_cache.try_emplace(type, result);
-            return result;
-        }
-
-        mlir_attr visit(const clang_attr *attr, scope_context &scope) override {
-            default_attr_visitor visitor(bld, self, scope);
-            return visitor.visit(attr);
-        }
-
-        operation visit_prototype(const clang_function *decl, scope_context &scope) override {
-            default_decl_visitor visitor(bld, self, scope);
-            return visitor.visit_prototype(decl);
-        }
+        operation visit_prototype(const clang_function *decl, scope_context &scope) override;
 
         llvm::DenseMap< const clang_type *, mlir_type > cache;
         llvm::DenseMap< clang_qual_type, mlir_type > qual_cache;
