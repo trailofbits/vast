5c6a220c10d5f7d0c9ae431258aec35431b02450
xlauko <xlauko@mail.muni.cz>
2024-05-04 14:23:38 +0200

    cg: Make default visitor attach attributes to declarations.

--- lib/vast/CodeGen/DefaultVisitor.cpp
+++ lib/vast/CodeGen/DefaultVisitor.cpp
@@ -2,11 +2,51 @@
 
 #include "vast/CodeGen/DefaultVisitor.hpp"
 
+#include "vast/CodeGen/Util.hpp"
+
 namespace vast::cg
 {
-    operation default_visitor::visit(const clang_decl *decl, scope_context &scope) {
+    operation default_visitor::visit_with_attrs(const clang_decl *decl, scope_context &scope) {
         default_decl_visitor visitor(bld, self, scope);
-        return visitor.visit(decl);
+        if (auto op = visitor.visit(decl)) {
+            return visit_decl_attrs(op, decl, scope);
+        }
+
+        return {};
+    }
+
+    using excluded_attr_list = util::type_list<
+          clang::WeakAttr
+        , clang::SelectAnyAttr
+        , clang::CUDAGlobalAttr
+    >;
+
+    operation default_visitor::visit_decl_attrs(operation op, const clang_decl *decl, scope_context &scope) {
+        if (decl->hasAttrs()) {
+            mlir::NamedAttrList attrs = op->getAttrs();
+            for (auto attr : exclude_attrs< excluded_attr_list >(decl->getAttrs())) {
+                auto visited = self.visit(attr, scope);
+
+                auto spelling = attr->getSpelling();
+                // Bultin attr doesn't have spelling because it can not be written in code
+                if (auto builtin = clang::dyn_cast< clang::BuiltinAttr >(attr)) {
+                    spelling = "builtin";
+                }
+
+                if (auto prev = attrs.getNamed(spelling)) {
+                    VAST_CHECK(visited == prev.value().getValue(), "Conflicting redefinition of attribute {0}", spelling);
+                }
+
+                attrs.set(spelling, visited);
+            }
+            op->setAttrs(attrs);
+        }
+
+        return op;
+    }
+
+    operation default_visitor::visit(const clang_decl *decl, scope_context &scope) {
+        return visit_with_attrs(decl, scope);
     }
 
     operation default_visitor::visit(const clang_stmt *stmt, scope_context &scope) {
