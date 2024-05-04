5d0a6cb07dcb66a063c7ffbd9c1140598e398f33
xlauko <xlauko@mail.muni.cz>
2024-05-04 14:51:22 +0200

    test: Fix asm statement codegen tests.

--- test/vast/Dialect/HighLevel/asm-b.c
+++ test/vast/Dialect/HighLevel/asm-b.c
@@ -3,10 +3,15 @@
 
 void f() {
 int src = 1;
+// CHECK: [[SRC:%[0-9]+]] = hl.var "src"
 int dst;
-// CHECK: hl.asm {is_volatile} "mov %1, %0\0A\09add $1, %0"([0] %2 : ["=r"]) (ins : [1] %4 : ["r"]) () () : (!hl.lvalue<!hl.int>, !hl.int) -> ()
-asm inline volatile ("mov %1, %0\n\t"
-    "add $1, %0"
+// CHECK: [[DST:%[0-9]+]] = hl.var "dst"
+// CHECK: [[DST_REF:%[0-9]+]] = hl.ref [[DST]]
+// CHECK: [[SRC_REF:%[0-9]+]] = hl.ref [[SRC]]
+// CHECK: [[CAST_SRC:%[0-9]+]] = hl.implicit_cast [[SRC_REF]]
+// CHECK: hl.asm {is_volatile} "mov %w1, %w0\0A\09add $1, %w0"([0] [[DST_REF]] : ["=r"]) (ins : [1] [[CAST_SRC]] : ["r"]) () () : (!hl.lvalue<!hl.int>, !hl.int) -> ()
+asm inline volatile ("mov %w1, %w0\n\t"
+    "add $1, %w0"
     : "=r" (dst)
     : "r" (src));
 }
