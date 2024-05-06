3cfb5b8070b8b96b6d159ad728332a46fb885381
xlauko <xlauko@mail.muni.cz>
2024-05-06 20:12:05 +0200

    test:hl: Update high-level enum types.

--- test/vast/Dialect/HighLevel/enum-e.c
+++ test/vast/Dialect/HighLevel/enum-e.c
@@ -8,7 +8,7 @@
 // CHECK:   hl.enum.const "LIQUID"
 // CHECK:   hl.enum.const "GAS"
 // CHECK:   hl.enum.const "PLASMA"
-// CHECK:  hl.field "state" : !hl.elaborated<!hl.record<"State">>
+// CHECK:  hl.field "state" : !hl.elaborated<!hl.enum<"State">>
 struct Element {
     int z;
     enum State { SOLID, LIQUID, GAS, PLASMA } state;
@@ -16,12 +16,12 @@ struct Element {
 // CHECK: hl.var "oxygen" : !hl.lvalue<!hl.elaborated<!hl.record<"Element">>>
 // CHECK:  [[V1:%[0-9]+]] = hl.const #core.integer<8> : !hl.int
 // CHECK:  [[V2:%[0-9]+]] = hl.enumref "GAS" : !hl.int
-// CHECK:  [[V3:%[0-9]+]] = hl.implicit_cast [[V2]] IntegralCast : !hl.int -> !hl.elaborated<!hl.record<"State">>
-// CHECK:  hl.initlist [[V1]], [[V3]] : (!hl.int, !hl.elaborated<!hl.record<"State">>) -> !hl.elaborated<!hl.record<"Element">>
+// CHECK:  [[V3:%[0-9]+]] = hl.implicit_cast [[V2]] IntegralCast : !hl.int -> !hl.elaborated<!hl.enum<"State">>
+// CHECK:  hl.initlist [[V1]], [[V3]] : (!hl.int, !hl.elaborated<!hl.enum<"State">>) -> !hl.elaborated<!hl.record<"Element">>
 } oxygen = { 8, GAS };
 
 void foo(void) {
-    // CHECK: hl.var "e" : !hl.lvalue<!hl.elaborated<!hl.record<"State">>>
+    // CHECK: hl.var "e" : !hl.lvalue<!hl.elaborated<!hl.enum<"State">>>
     // CHECK:   hl.enumref "LIQUID" : !hl.int
     enum State e = LIQUID;
 }
