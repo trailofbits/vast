299e94ef14daf49d6550faacb2e9c41f72ee7f23
xlauko <xlauko@mail.muni.cz>
2024-05-06 20:12:05 +0200

    test:hl: Update high-level enum types.

--- test/vast/Dialect/HighLevel/enum-a.c
+++ test/vast/Dialect/HighLevel/enum-a.c
@@ -8,13 +8,13 @@
 // CHECK: }
 enum color { RED, GREEN, BLUE };
 
-// CHECK: hl.var "c" : !hl.lvalue<!hl.elaborated<!hl.record<"color">>>
+// CHECK: hl.var "c" : !hl.lvalue<!hl.elaborated<!hl.enum<"color">>>
 // CHECK: [[V1:%[0-9]+]] = hl.enumref "GREEN" : !hl.int
-// CHECK: [[V2:%[0-9]+]] = hl.implicit_cast [[V1]] IntegralCast : !hl.int -> !hl.elaborated<!hl.record<"color">>
-// CHECK: hl.value.yield [[V2]] : !hl.elaborated<!hl.record<"color">>
+// CHECK: [[V2:%[0-9]+]] = hl.implicit_cast [[V1]] IntegralCast : !hl.int -> !hl.elaborated<!hl.enum<"color">>
+// CHECK: hl.value.yield [[V2]] : !hl.elaborated<!hl.enum<"color">>
 enum color c = GREEN;
 
-// CHECK: hl.typedef "color" : !hl.elaborated<!hl.record<"color">>
+// CHECK: hl.typedef "color" : !hl.elaborated<!hl.enum<"color">>
 typedef enum color color;
 
 // CHECK: hl.var "tc" : !hl.lvalue<!hl.elaborated<!hl.typedef<"color">>>
