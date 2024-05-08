572e3387de5076bbf091e73fef81480aabedf33a
xlauko <xlauko@mail.muni.cz>
2024-05-08 11:52:27 +0200

    test:tohlbi: Remove obsolete return type of __builtin_trap.

--- test/vast/Conversion/FromHL/ToHLBI/trap-a.c
+++ test/vast/Conversion/FromHL/ToHLBI/trap-a.c
@@ -1,5 +1,5 @@
 // RUN: %vast-front -vast-emit-mlir=hl -o - %s | %vast-opt --vast-hl-to-hl-builtin | %file-check %s
 void fn() {
-    // CHECK: hlbi.trap : !hl.void
+    // CHECK: hlbi.trap
     __builtin_trap();
 }
