bd05a29f1d8aceb68ae89034cf78d45253f7e5dd
Lukas Korencik <lukas.korencik@trailofbits.com>
2024-05-13 17:06:21 +0200

    conv:tollvm: Fix return of void value.

--- lib/vast/Conversion/ToLLVM/IRsToLLVM.cpp
+++ lib/vast/Conversion/ToLLVM/IRsToLLVM.cpp
@@ -679,7 +679,7 @@ namespace vast::conv::irstollvm
                 op_t ret_op, typename op_t::Adaptor ops,
                 conversion_rewriter &rewriter) const override
         {
-            rewriter.create< LLVM::ReturnOp >(ret_op.getLoc(), ops.getOperands());
+            rewriter.create< LLVM::ReturnOp >(ret_op.getLoc(), this->strip_voids(ops.getOperands()));
             rewriter.eraseOp(ret_op);
             return logical_result::success();
         }
