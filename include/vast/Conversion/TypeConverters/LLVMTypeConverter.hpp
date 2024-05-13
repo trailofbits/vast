03e686c875fbdbfe7e3d8dd3b4273565a2c80710
Lukas Korencik <lukas.korencik@trailofbits.com>
2024-05-13 17:04:28 +0200

    conv:tc: Add materializations of `mlir::LLVM::LLVMVoidType`.

--- include/vast/Conversion/TypeConverters/LLVMTypeConverter.hpp
+++ include/vast/Conversion/TypeConverters/LLVMTypeConverter.hpp
@@ -8,6 +8,7 @@ VAST_RELAX_WARNINGS
 #include <mlir/Conversion/LLVMCommon/TypeConverter.h>
 #include <mlir/Dialect/Func/IR/FuncOps.h>
 #include <mlir/Dialect/LLVMIR/LLVMTypes.h>
+#include <mlir/Dialect/LLVMIR/LLVMDialect.h>
 #include <mlir/IR/Types.h>
 
 #include <llvm/ADT/ScopeExit.h>
@@ -32,6 +33,19 @@ namespace vast::conv::tc {
         using base      = mlir::LLVMTypeConverter;
         using helpers_t = tc::mixins< LLVMTypeConverter >;
 
+        auto ignore_none_materialization() {
+            return [&](mlir::OpBuilder &bld, mlir::NoneType t, mlir::ValueRange inputs,
+                       mlir::Location loc) {
+                return bld.create< mlir::LLVM::ZeroOp >(loc, *this->convert_type_to_type(t));
+            };
+        }
+        auto ignore_void_materialization() {
+            return [&](mlir::OpBuilder &bld, mlir::LLVM::LLVMVoidType t, mlir::ValueRange inputs,
+                       mlir::Location loc) {
+                return bld.create< mlir::LLVM::ZeroOp >(loc, *this->convert_type_to_type(t));
+            };
+        }
+
         template< typename... Args >
         LLVMTypeConverter(Args &&...args) : base(std::forward< Args >(args)...) {
             addConversion([&](hl::LabelType t) { return t; });
@@ -46,6 +60,21 @@ namespace vast::conv::tc {
             addConversion([&](mlir::NoneType t) {
                 return LLVM::LLVMVoidType::get(t.getContext());
             });
+
+            // Since higher level model void values as "real" values in the IR, it can happen
+            // we have `return foo()` where `foo` returns `void`. Now during conversion, because
+            // of type mismatches, materialization callbacks will be invoked. By default, this
+            // means unrealised conversions, but because the value in IR stops existing (as codegen
+            // does not work with void values and LLVM dialect constraints) this means verifier error.
+            // Instead we just emit a dummy constant, so that verifier does not hit unhappy path
+            // that leads to a crash. These should be removed by subsequent passes as a cleanup.
+            addArgumentMaterialization(ignore_none_materialization());
+            addSourceMaterialization(ignore_none_materialization());
+            addTargetMaterialization(ignore_none_materialization());
+
+            addArgumentMaterialization(ignore_void_materialization());
+            addSourceMaterialization(ignore_void_materialization());
+            addTargetMaterialization(ignore_void_materialization());
         }
 
         // Moving the converter caused bugs in the code - since the base class has no comments
