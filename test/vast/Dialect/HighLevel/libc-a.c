cd6f773142c1d36ec28ddb4c12b45789d756fc30
xlauko <xlauko@mail.muni.cz>
2024-05-13 12:02:01 +0200

    test: Update test annotations.

--- test/vast/Dialect/HighLevel/libc-a.c
+++ test/vast/Dialect/HighLevel/libc-a.c
@@ -1,9 +1,9 @@
-// RUN: %vast-cc1 -vast-emit-mlir=hl %s -o - | %file-check %s
-// RUN: %vast-cc1 -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
-// REQUIRES: libc
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %file-check %s
+// RUN: %vast-front -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
 
 #include <stdio.h>
 
 int main() {
+    // CHECK: hl.call @printf
     printf("hello world\n");
 }
