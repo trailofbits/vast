088ab4e0c2a41a7c90e7e23a0efcf2d22a0a11f6
xlauko <xlauko@mail.muni.cz>
2024-05-14 13:11:52 +0200

    cc: Rename emit-mlir-until to emit-mlir-after.

--- lib/vast/Frontend/Action.cpp
+++ lib/vast/Frontend/Action.cpp
@@ -17,7 +17,7 @@ namespace vast::cc {
 
     namespace opt {
         bool emit_only_mlir(const vast_args &vargs) {
-            for (auto arg : { emit_mlir, emit_mlir_until }) {
+            for (auto arg : { emit_mlir, emit_mlir_after }) {
                 if (vargs.has_option(arg)) {
                     return true;
                 }
