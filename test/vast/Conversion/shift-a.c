a809cc640af835a4e4a048e74640b6a4a6805dc7
Robert Konicar <robert.konicar@trailofbits.com>
2024-05-14 16:49:59 +0200

    test: Add test for bit shift conversion to llvm.

---
+++ test/vast/Conversion/shift-a.c
@@ -0,0 +1,15 @@
+// RUN: %vast-front -vast-emit-mlir=llvm -o - %s | %file-check %s
+
+#include <stdint.h>
+
+int main() {
+    int64_t x = 5;
+    uint64_t y = 5;
+    // CHECK: llvm.shl {{.*}} : i64
+    int64_t u = x << 3;
+    // CHECK: llvm.ashr {{.*}} : i64
+    int64_t v = x >> 3;
+    // CHECK: llvm.lshr {{.*}} : i64
+    uint64_t w = y >> 3;
+    return 0;
+}
