4db3293c932105d97a281f92acf57b8e81f6fa67
Robert Konicar <robert.konicar@trailofbits.com>
2024-05-16 14:44:08 +0200

    test: Add test for complex types.

---
+++ test/vast/Dialect/HighLevel/complex-a.c
@@ -0,0 +1,18 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %file-check %s
+// RUN: %vast-front -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
+#include <complex.h>
+
+void fun() {
+// CHECK: "x" : !hl.lvalue<!hl.complex<!hl.double>>
+// CHECK: hl.const.init : !hl.complex<!hl.float>
+    double complex x = I;
+// CHECK: hl.fadd {{.*}} : (!hl.complex<!hl.double>, !hl.complex<!hl.double>)
+    double complex y = x + 3;
+    double c = 3;
+// CHECK: hl.fadd {{.*}} : (!hl.complex<!hl.double>, !hl.double)
+    double complex z = x + c;
+// CHEKC: hl.real {{.*}} : {{.*}}!hl.complex<!hl.double>{{.*}} -> !hl.double
+    double a = __real__ y;
+// CHEKC: hl.imag {{.*}} : {{.*}}!hl.complex<!hl.double>{{.*}} -> !hl.double
+    double b = __imag__ y;
+}
