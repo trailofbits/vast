f8e24cd646062ee60a945997e9996d8e422fcae1
Lukas Korencik <lukas.korencik@trailofbits.com>
2024-05-17 12:31:51 +0200

    test:conv: Improve quality of some tests.

--- test/vast/Conversion/fn-b.c
+++ test/vast/Conversion/fn-b.c
@@ -1,10 +1,10 @@
-// RUN: %vast-cc1 -vast-emit-mlir=hl %s -o - | %vast-opt-irs-to-llvm | %file-check %s
+// RUN: %check-core-to-llvm %s | %file-check %s -check-prefix=C_LLVM
 
-// CHECK: llvm.func @fn() -> i32 {
+// C_LLVM: llvm.func @fn() -> i32 {
+// C_LLVM:   [[V:%[0-9]+]] = llvm.mlir.constant(5 : i32) : i32
+// C_LLVM:   llvm.return [[V]] : i32
+// C_LLVM: }
 int fn()
 {
-    // CHECK: [[V:%[0-9]+]] = llvm.mlir.constant(5 : i32) : i32
-    // CHECK: llvm.return [[V]] : i32
     return 5;
 }
-// CHECK : }
