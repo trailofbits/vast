4b10219b097bb3ac4eb27a4990c1946594672969
xlauko <xlauko@mail.muni.cz>
2024-05-29 10:07:56 +0200

    core: Setup ShadowingSymbolTable.

---
+++ include/vast/Dialect/Core/SymbolTable.hpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2024, Trail of Bits, Inc.
+
+#pragma once
+
+#include "vast/Util/Warnings.hpp"
+
+VAST_RELAX_WARNINGS
+#include <llvm/ADT/DenseMap.h>
+#include <mlir/IR/OpDefinition.h>
+VAST_UNRELAX_WARNINGS
+
+#include "vast/Dialect/Core/CoreTraits.hpp"
+#include "vast/Util/Common.hpp"
+
+namespace vast::core {
+    template< typename... SymbolInterfaces >
+    struct ShadowingSymbolTable
+    {
+        template< typename ConcreteType >
+        struct Impl : op_trait_base< ConcreteType, Impl >
+        {
+            static logical_result verifyRegionTrait(operation /* op */) {
+                return mlir::success();
+            }
+        };
+    };
+} // namespace vast::core
---
+++ lib/vast/Dialect/Core/SymbolTable.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2024, Trail of Bits, Inc.
+
+#include "vast/Dialect/Core/SymbolTable.hpp"
+
+namespace vast::core {
+
+} // namespace vast::core
