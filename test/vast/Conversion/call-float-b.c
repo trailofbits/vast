4feafd15c76412682f74659907f628070bf57e1f
Lukas Korencik <lukas.korencik@trailofbits.com>
2024-05-29 15:41:52 +0200

    test:conv: Add tests of calls with floating arguments.

---
+++ test/vast/Conversion/call-float-b.c
@@ -0,0 +1,45 @@
+// RUN: %check-emit-abi %s | %file-check %s -check-prefix=EMIT_ABI
+// RUN: %check-lower-value-categories %s | %file-check %s -check-prefix=VAL_CAT
+// RUN: %check-core-to-llvm %s | %file-check %s -check-prefix=C_LLVM
+
+// EMIT_ABI:  abi.func @vast.abi.f_float{{.*}}() -> f32 {
+// EMIT_ABI:    [[V0:%[0-9]+]] = hl.const #core.float<1.000000e-01> : f32
+// EMIT_ABI:    ll.return [[V0]] : f32
+// EMIT_ABI:  }
+float f_float() { return 0.1f; }
+
+// EMIT_ABI:  abi.func @vast.abi.f_double{{.*}}() -> f64 {
+// EMIT_ABI:    [[V0:%[0-9]+]] = hl.const #core.float<5.000000e+00> : f64
+// EMIT_ABI:    ll.return [[V0]] : f64
+// EMIT_ABI:  }
+double f_double() { return 5.0; }
+
+// EMIT_ABI:  {{.*}} = abi.call_exec @f_float() {
+// EMIT_ABI:    [[F3:%[0-9]+]] = abi.call @f_float() : () -> f32
+// EMIT_ABI:    [[F4:%[0-9]+]] = abi.call_rets {
+// EMIT_ABI:      {{.*}} = abi.direct [[F3]] : f32 -> f32
+// EMIT_ABI:    } : f32
+// EMIT_ABI:    {{.*}} = abi.yield [[F4]] : f32 -> f32
+// EMIT_ABI:  } : () -> f32
+
+// EMIT_ABI:  {{.*}} = abi.call_exec @f_double() {
+// EMIT_ABI:    [[D3:%[0-9]+]] = abi.call @f_double() : () -> f64
+// EMIT_ABI:    [[D4:%[0-9]+]] = abi.call_rets {
+// EMIT_ABI:      {{.*}} = abi.direct [[D3]] : f64 -> f64
+// EMIT_ABI:    } : f64
+// EMIT_ABI:    {{.*}} = abi.yield [[D4]] : f64 -> f64
+// EMIT_ABI:  } : () -> f64
+
+
+// VAL_CAT:  {{.*}} = hl.call @f_float() : () -> f32
+// VAL_CAT:  {{.*}} = hl.call @f_double() : () -> f64
+
+
+// C_LLVM: {{.*}} = llvm.call @f_float() : () -> f32
+// C_LLVM: {{.*}} = llvm.call @f_double() : () -> f64
+
+int main()
+{
+    f_float();
+    f_double();
+}
---
+++ test/vast/Conversion/call-struct-float-a.c
@@ -0,0 +1,57 @@
+// RUN: %check-emit-abi %s | %file-check %s -check-prefix=EMIT_ABI
+// RUN: %check-lower-value-categories %s | %file-check %s -check-prefix=VAL_CAT
+// RUN: %check-core-to-llvm %s | %file-check %s -check-prefix=C_LLVM
+
+struct data {
+    double x;
+    double y;
+};
+
+// VAL_CAT:  ll.func @fn{{.*}} ([[A0:%.*]]: !hl.record<"data">) -> none {
+// VAL_CAT:    [[V0:%[0-9]+]] = ll.alloca : !hl.ptr<!hl.record<"data">>
+// VAL_CAT:    ll.store [[V0]], [[A0]] : !hl.ptr<!hl.record<"data">>, !hl.record<"data">
+
+// EMIT_ABI:  abi.func @vast.abi.fn{{.*}}([[A0:%.*]]: f64, [[A1:%.*]]: f64) -> none {
+// EMIT_ABI:    {{.*}} = abi.prologue {
+// EMIT_ABI:      [[V3:%[0-9]+]] = abi.direct [[A0]], [[A1]] : f64, f64 -> !hl.record<"data">
+// EMIT_ABI:      {{.*}} = abi.yield [[V3]] : !hl.record<"data"> -> !hl.record<"data">
+// EMIT_ABI:    } : !hl.record<"data">
+
+// C_LLVM:  llvm.func @fn([[A0:%.*]]: f64, [[A1:%.*]]: f64) {
+// C_LLVM:    [[V0:%[0-9]+]] = llvm.mlir.undef : !llvm.struct<"data", (f64, f64)>
+// C_LLVM:    [[V1:%[0-9]+]] = llvm.insertvalue [[A0]], [[V0]][0] : !llvm.struct<"data", (f64, f64)>
+// C_LLVM:    [[V2:%[0-9]+]] = llvm.insertvalue [[A1]], [[V1]][1] : !llvm.struct<"data", (f64, f64)>
+// C_LLVM:    [[V3:%[0-9]+]] = llvm.mlir.constant(1 : index) : i64
+// C_LLVM:    [[V4:%[0-9]+]] = llvm.alloca [[V3]] x !llvm.struct<"data", (f64, f64)> : (i64) -> !llvm.ptr
+// C_LLVM:    llvm.store [[V2]], [[V4]] : !llvm.struct<"data", (f64, f64)>, !llvm.ptr
+
+void fn(struct data d) {
+    d.x + d.y;
+}
+
+// VAL_CAT:  [[V5:%[0-9]+]] = ll.load {{.*}} : (!hl.ptr<!hl.record<"data">>) -> !hl.record<"data">
+// VAL_CAT:  {{.*}} = hl.call @fn([[V5]]) : (!hl.record<"data">) -> none
+
+// EMIT_ABI:  [[V5:%[0-9]+]] = ll.load %1 : (!hl.ptr<!hl.record<"data">>) -> !hl.record<"data">
+// EMIT_ABI:  {{.*}} = abi.call_exec @fn([[V5]]) {
+// EMIT_ABI:    [[V7:%[0-9]+]]:2 = abi.call_args {
+// EMIT_ABI:      [[V10:%[0-9]+]]:2 = abi.direct [[V5]] : !hl.record<"data"> -> f64, f64
+// EMIT_ABI:      [[V11:%[0-9]+]]:2 = abi.yield [[V10]]#0, [[V10]]#1 : f64, f64 -> f64, f64
+// EMIT_ABI:    } : f64, f64
+// EMIT_ABI:    {{.*}} = abi.call @fn([[V7]]#0, [[V7]]#1) : (f64, f64) -> none
+// EMIT_ABI:  } : (!hl.record<"data">) -> none
+
+
+// C_LLVM: {{.*}} = llvm.alloca {{.*}} x !llvm.struct<"data", (f64, f64)> : (i64) -> !llvm.ptr
+// C_LLVM: [[V9:%[0-9]+]] = llvm.alloca {{.*}} x !llvm.struct<"data", (f64, f64)> : (i64) -> !llvm.ptr
+// C_LLVM: llvm.store {{.*}}, [[V9]] : !llvm.struct<"data", (f64, f64)>, !llvm.ptr
+// C_LLVM: [[V10:%[0-9]+]] = llvm.getelementptr [[V9]][0, 0] : (!llvm.ptr) -> !llvm.ptr, !llvm.struct<"data", (f64, f64)>
+// C_LLVM: [[V11:%[0-9]+]] = llvm.load [[V10]] : !llvm.ptr -> f64
+// C_LLVM: [[V12:%[0-9]+]] = llvm.getelementptr [[V9]][0, 1] : (!llvm.ptr) -> !llvm.ptr, !llvm.struct<"data", (f64, f64)>
+// C_LLVM: [[V13:%[0-9]+]] = llvm.load [[V12]] : !llvm.ptr -> f64
+// C_LLVM: llvm.call @fn([[V11]], [[V13]]) : (f64, f64) -> ()
+int main()
+{
+    struct data d = { 0.1, 0.2 };
+    fn(d);
+}
