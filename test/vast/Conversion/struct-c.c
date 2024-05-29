a3010d2e8a124604969bbccd3d977a0457d00f13
Lukas Korencik <lukas.korencik@trailofbits.com>
2024-05-29 14:05:20 +0200

    test:conv: Modernize `struct-*` tests.

--- test/vast/Conversion/struct-c.c
+++ test/vast/Conversion/struct-c.c
@@ -1,18 +1,21 @@
-// RUN: %vast-cc1 -vast-emit-mlir=hl %s -o - | %vast-opt-irs-to-llvm | %file-check %s
+// RUN: %check-lower-value-categories %s | %file-check %s -check-prefix=VAL_CAT
+// RUN: %check-core-to-llvm %s | %file-check %s -check-prefix=C_LLVM
 
 struct Y;
-
-struct X { const int x; struct Y *y; };
-
 struct Y { const float x; };
 
+// VAL_CAT: [[V0:%[0-9]+]] = ll.alloca : !hl.ptr<!hl.record<"Y">>
+// VAL_CAT: [[V1:%[0-9]+]] = hl.const #core.float<2.000000e+00> : f32
+// VAL_CAT: [[V2:%[0-9]+]] = hl.initlist [[V1]] : (f32) -> !hl.record<"Y">
+// VAL_CAT: ll.store [[V0]], [[V2]] : !hl.ptr<!hl.record<"Y">>, !hl.record<"Y">
+
+// C_LLVM: [[V1:%[0-9]+]] = llvm.alloca {{.*}} x !llvm.struct<"Y", (f32)> : (i64) -> !llvm.ptr
+// C_LLVM: [[V2:%[0-9]+]] = llvm.mlir.constant(2.000000e+00 : f32) : f32
+// C_LLVM: [[V3:%[0-9]+]] = llvm.mlir.undef : !llvm.struct<"Y", (f32)>
+// C_LLVM: [[V4:%[0-9]+]] = llvm.insertvalue [[V2]], [[V3]][0] : !llvm.struct<"Y", (f32)>
+// C_LLVM: llvm.store [[V4]], [[V1]] : !llvm.struct<"Y", (f32)>, !llvm.ptr
 int main()
 {
-    // CHECK: {{.*}}  = llvm.alloca {{.*}} x !llvm.struct<"X", (i32, ptr)> : (i64) -> !llvm.ptr
-    struct X x = { 2, 0 };
-
-    // CHECK: {{.*}} = llvm.alloca {{.*}} x !llvm.struct<"Y", (f32)> : (i64) -> !llvm.ptr
     struct Y y = { 2.0f };
-
     return 0;
 }
