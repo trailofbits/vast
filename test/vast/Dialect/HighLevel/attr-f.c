646ccb460bd804b3d58e7942314798fdacaf50c4
Robert Konicar <robert.konicar@trailofbits.com>
2024-05-29 17:48:21 +0200

    test: Add test for transparent_union.

---
+++ test/vast/Dialect/HighLevel/attr-f.c
@@ -0,0 +1,25 @@
+// RUN: %vast-front -vast-emit-mlir=hl -o - %s | %file-check %s
+// RUN: %vast-front -vast-emit-mlir=hl -o - %s > %t && %vast-opt %t | diff -B %t -
+
+// CHECK: hl.union "u" {transparent_union = #hl.transparent_union}
+union u {
+    int *a;
+    char *b;
+} __attribute__ ((transparent_union));
+
+void fun(union u x) {
+    return;
+}
+
+int main() {
+    int x;
+// CHECK: hl.call @fun({{.*}}) : (!hl.elaborated<!hl.record<"u">>) -> !hl.void
+    fun(&x);
+    char y;
+// CHECK: hl.compound_literal : !hl.elaborated<!hl.record<"u">>
+// CHECK: hl.call @fun({{.*}}) : (!hl.elaborated<!hl.record<"u">>) -> !hl.void
+    fun(&y);
+    union u z;
+// CHECK: hl.call @fun({{.*}}) : (!hl.elaborated<!hl.record<"u">>) -> !hl.void
+    fun(z);
+}
