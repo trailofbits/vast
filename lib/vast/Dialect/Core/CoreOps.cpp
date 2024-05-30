7edc0339987f7a344e511c054e5bb07ededfac61
xlauko <xlauko@mail.muni.cz>
2024-05-30 15:37:55 +0200

    core: Introduce ModuleOp.

--- lib/vast/Dialect/Core/CoreOps.cpp
+++ lib/vast/Dialect/Core/CoreOps.cpp
@@ -26,6 +26,58 @@
 namespace vast::core
 {
     GRAPH_REGION_OP(ScopeOp);
+
+    //
+    // ModuleOp
+    //
+
+    void ModuleOp::build(mlir_builder &builder, op_state &state, std::optional< string_ref > name) {
+        state.addRegion()->emplaceBlock();
+
+        if (name.has_value()) {
+            state.addAttribute(symbol_attr_name(), builder.getStringAttr(name.value()));
+        }
+    }
+
+    ModuleOp ModuleOp::create(loc_t loc, std::optional< string_ref > name) {
+        mlir_builder builder(loc->getContext());
+        return builder.create< ModuleOp >(loc, name);
+    }
+
+    mlir::DataLayoutSpecInterface ModuleOp::getDataLayoutSpec() {
+        // Take the first and only (if present) attribute that implements the
+        // interface. This needs a linear search, but is called only once per data
+        // layout object construction that is used for repeated queries.
+        for (auto attr : getOperation()->getAttrs()) {
+            if (auto spec = llvm::dyn_cast< mlir::DataLayoutSpecInterface >(attr.getValue())) {
+                return spec;
+            }
+        }
+        return {};
+    }
+
+    logical_result ModuleOp::verify() {
+        // Check that there is at most one data layout spec attribute.
+        string_ref layout_spec_attr_name;
+        mlir::DataLayoutSpecInterface layout_spec;
+        for (const auto &na : (*this)->getAttrs()) {
+            if (auto spec = llvm::dyn_cast< mlir::DataLayoutSpecInterface >(na.getValue())) {
+                if (layout_spec) {
+                    mlir::InFlightDiagnostic diag = emitOpError()
+                        << "expects at most one data layout attribute";
+                    diag.attachNote()
+                        << "'" << layout_spec_attr_name << "' is a data layout attribute";
+                    diag.attachNote()
+                        << "'" << na.getName().getValue() << "' is a data layout attribute";
+                }
+                layout_spec_attr_name = na.getName().strref();
+                layout_spec         = spec;
+            }
+        }
+
+        return mlir::success();
+    }
+
 } // namespace vast::core
 
 //===----------------------------------------------------------------------===//
