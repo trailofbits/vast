8f6f18aa7e6d24ca35fea99e4c38824ea941d792
xlauko <xlauko@mail.muni.cz>
2024-05-30 15:44:30 +0200

    core: Add lookup methods to shadowing symbol table.

--- include/vast/Dialect/Core/SymbolTable.hpp
+++ include/vast/Dialect/Core/SymbolTable.hpp
@@ -20,8 +20,38 @@ namespace vast::core {
         template< typename ConcreteType >
         struct Impl : op_trait_base< ConcreteType, Impl >
         {
+            using string_attr   = mlir::StringAttr;
+            using symbol_ref    = mlir::SymbolRefAttr;
+
             static logical_result verifyRegionTrait(operation /* op */) {
                 return mlir::success();
+            }
+
+            operation lookup_symbol(string_attr /* name */) {
+                VAST_UNIMPLEMENTED;
+            }
+
+            template< typename T >
+            T lookup_symbol(string_attr name) {
+                return mlir::dyn_cast_or_null< T >(lookup_symbol(name));
+            }
+
+            operation lookup_symbol(symbol_ref /* symbol */) {
+                VAST_UNIMPLEMENTED;
+            }
+
+            template< typename T >
+            T lookup_symbol(symbol_ref symbol) {
+                return dyn_cast_or_null< T >(lookup_symbol(symbol));
+            }
+
+            operation lookup_symbol(string_ref /* name */) {
+                VAST_UNIMPLEMENTED;
+            }
+
+            template< typename T >
+            T lookup_symbol(string_ref name) {
+                return mlir::dyn_cast_or_null< T >(lookup_symbol(name));
             }
         };
     };
