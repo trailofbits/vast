2f7c4f123a8e40b0da0e3519613d4e86a98cffd7
Robert Konicar <robert.konicar@trailofbits.com>
2024-06-03 17:49:37 +0200

    test: Enable indirect goto test.

--- test/vast/Dialect/HighLevel/goto-b.c
+++ test/vast/Dialect/HighLevel/goto-b.c
@@ -1,15 +1,21 @@
-// RUN: %vast-cc1 -vast-emit-mlir=hl %s -o - | %file-check %s
-// RUN: %vast-cc1 -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
-// REQUIRES: indirect-goto
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %file-check %s
+// RUN: %vast-front -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
 
 void foo(int test) {
+
+    // CHECK: [[PTR:%[0-9]+]] = hl.var
     void *ptr;
 
     if (test)
+    // CHECK: hl.labeladdr
         ptr = &&foo;
     else
+    // CHECK: hl.labeladdr
         ptr = &&bar;
 
+    // CHECK: hl.indirect_goto : {
+    // CHECK: hl.ref [[PTR]]
+    // CHECK: }
     goto *ptr;
 
     foo: /* ... */;
