13abbc89e0e8d580a97a021111850571ec8ba1aa
Robert Konicar <robert.konicar@trailofbits.com>
2024-06-03 17:51:43 +0200

    test: Expand complex test with assignment to real/imaginary parts of a complex var.

--- test/vast/Dialect/HighLevel/complex-a.c
+++ test/vast/Dialect/HighLevel/complex-a.c
@@ -15,4 +15,9 @@ void fun() {
     double a = __real__ y;
 // CHEKC: hl.imag {{.*}} : {{.*}}!hl.complex<!hl.double>{{.*}} -> !hl.double
     double b = __imag__ y;
+
+// CHEKC: hl.real {{.*}} : {{.*}}!hl.complex<!hl.double>{{.*}} -> !hl.lvalue<!hl.double>
+// CHEKC: hl.imag {{.*}} : {{.*}}!hl.complex<!hl.double>{{.*}} -> !hl.lvalue<!hl.double>
+    __real__ x = 1;
+    __imag__ x = 1;
 }
