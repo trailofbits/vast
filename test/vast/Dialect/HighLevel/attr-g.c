ee662011925439409c41295b06477a3652681666
Robert Konicar <robert.konicar@trailofbits.com>
2024-06-03 19:40:39 +0200

    test: Add test for MaxFieldAlignmentAttr.

---
+++ test/vast/Dialect/HighLevel/attr-g.c
@@ -0,0 +1,12 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %file-check %s
+// RUN: %vast-front -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
+
+#pragma pack(4)
+
+// CHECK: #hl.max_field_alignment<alignment : {{[0-9]+}}>
+struct foo2 {
+  short a;
+  long  x;
+  short y;
+};
+
