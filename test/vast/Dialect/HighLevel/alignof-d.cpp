c68e17bc589a4a094b08c920f16231ca69113a2d
Robert Konicar <robert.konicar@trailofbits.com>
2024-06-03 17:42:24 +0200

    test: Add preferred_alignof tests.

---
+++ test/vast/Dialect/HighLevel/alignof-d.cpp
@@ -0,0 +1,15 @@
+// RUN: %vast-cc1 -vast-emit-mlir=hl -Wno-gnu-statement-expression %s -o - | %file-check %s
+// RUN: %vast-cc1 -vast-emit-mlir=hl -Wno-gnu-statement-expression %s -o %t && %vast-opt %t | diff -B %t -
+
+int main() {
+    // CHECK: hl.preferred_alignof.type !hl.int -> !hl.long< unsigned >
+    unsigned long si = __alignof__(int);
+
+    // CHECK: [[V1:%[0-9]+]] = hl.var "v" : !hl.lvalue<!hl.int>
+    int v;
+
+    // CHECK: hl.var "sv" : !hl.lvalue<!hl.long< unsigned >>
+    // CHECK: hl.preferred_alignof.expr -> !hl.long< unsigned >
+    // CHECK: hl.ref [[V1]]
+    unsigned long sv = __alignof__ v;
+}
