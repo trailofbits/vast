6ddc6b79bad4d4746435566133e90a8007066c53
xlauko <xlauko@mail.muni.cz>
2024-06-04 10:31:31 +0200

    treewide: Make ShadowingSymbolTable define type list of recognized symbols.

--- include/vast/Dialect/Core/CoreOps.hpp
+++ include/vast/Dialect/Core/CoreOps.hpp
@@ -6,7 +6,9 @@
 #include "vast/Dialect/Core/CoreTraits.hpp"
 #include "vast/Dialect/Core/SymbolTable.hpp"
 #include "vast/Interfaces/SymbolInterface.hpp"
+
 #include "vast/Util/Common.hpp"
+#include "vast/Util/TypeList.hpp"
 
 #include <mlir/Interfaces/FunctionInterfaces.h>
 #include <mlir/Interfaces/CallInterfaces.h>
@@ -14,7 +16,6 @@
 
 #define GET_OP_CLASSES
 #include "vast/Dialect/Core/Core.h.inc"
-
 
 namespace vast::core {
     using module            = core::ModuleOp;
