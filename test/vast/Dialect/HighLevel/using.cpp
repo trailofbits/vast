7aa8baf040098f41f6945751d6d4efe6c33eff9d
xlauko <xlauko@mail.muni.cz>
2024-06-08 10:31:02 +0200

    test: No longer unsupported alias test to high-level tests.

--- test/vast/Dialect/HighLevel/using.cpp
+++ test/vast/Dialect/HighLevel/using.cpp
@@ -1,6 +1,6 @@
 // RUN: %vast-front %s -vast-emit-mlir=hl -o - | %file-check %s
 // RUN: %vast-front %s -vast-emit-mlir=hl -o - > %t && %vast-opt %t | diff -B %t -
 
-// CHECK: unsup.decl "TypeAlias::Int"
+// CHECK: hl.alias "Int" : !hl.int
 using Int = int;
 Int variable = 0;
