265acf1e84105f49de555a10dd77c617fe5c1f24
Robert Konicar <robert.konicar@trailofbits.com>
2024-06-11 11:50:48 +0200

    hl: Add OffsetOfNode printer/parser duo.

--- lib/vast/Dialect/HighLevel/HighLevelAttributes.cpp
+++ lib/vast/Dialect/HighLevel/HighLevelAttributes.cpp
@@ -2,6 +2,8 @@
 
 #include "vast/Dialect/HighLevel/HighLevelAttributes.hpp"
 #include "vast/Dialect/HighLevel/HighLevelTypes.hpp"
+#include "vast/Util/Warnings.hpp"
+#include <gap/core/overloads.hpp>
 
 VAST_RELAX_WARNINGS
 #include <llvm/ADT/TypeSwitch.h>
@@ -14,6 +16,36 @@ VAST_RELAX_WARNINGS
 
 namespace vast::hl
 {
+    mlir_attr OffsetOfNodeAttr::parse(mlir::AsmParser &parser, mlir_type) {
+        if (parser.parseLess()) return {};
+        if (parser.parseOptionalKeyword("index").succeeded()) {
+            if(parser.parseColon()) return {};
+
+            unsigned int value;
+            if (parser.parseInteger(value) || parser.parseGreater()) return {};
+            return get(parser.getContext(), value);
+        }
+
+        if (parser.parseOptionalKeyword("identifier").succeeded()) {
+            if(parser.parseColon()) return {};
+
+            std::string value;
+            if (parser.parseString(&value) || parser.parseGreater()) return {};
+            return get(parser.getContext(), value);
+        }
+
+        return {};
+    }
+
+    void OffsetOfNodeAttr::print(mlir::AsmPrinter &printer) const {
+        auto value = getValue();
+        printer << "<";
+        auto print_identifier = [&](const mlir::StringAttr &str) { printer << "identifier : " << str; };
+        auto print_index      = [&](unsigned int index) { printer << "index : " << index; };
+        std::visit(gap::overloaded{ print_index, print_identifier }, value);
+        printer << ">";
+    }
+
     void HighLevelDialect::registerAttributes()
     {
         addAttributes<
