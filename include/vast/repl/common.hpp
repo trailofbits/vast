a5b8fab1c484c9dee961193db73aebaa9a0da771
Lukas Korencik <lukas.korencik@trailofbits.com>
2024-06-12 18:38:03 +0200

    repl: Throw exceptions instead of killing program on error.

--- include/vast/repl/common.hpp
+++ include/vast/repl/common.hpp
@@ -4,7 +4,13 @@
 
 #include "vast/Util/Common.hpp"
 
+#include <exception>
+
 namespace vast::repl {
 
+    template< typename ... Args >
+    [[noreturn]] void throw_error(Args && ... args) {
+        throw std::runtime_error(llvm::formatv(std::forward< Args >(args) ...).str());
+    }
 
 } // namespace vast::repl
