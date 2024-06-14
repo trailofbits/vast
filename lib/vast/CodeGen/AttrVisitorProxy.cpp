3e7ba32b94fe203b123a7cd5e17426f4ae3be481
xlauko <xlauko@mail.muni.cz>
2024-06-14 11:01:42 +0200

    cg: Create attr visitor proxy.

---
+++ lib/vast/CodeGen/AttrVisitorProxy.cpp
@@ -0,0 +1,52 @@
+// Copyright (c) 2024-present, Trail of Bits, Inc.
+
+#include "vast/CodeGen/AttrVisitorProxy.hpp"
+#include "vast/CodeGen/Util.hpp"
+
+#include "vast/Dialect/Unsupported/UnsupportedDialect.hpp"
+
+#include <ranges>
+#include <functional>
+
+namespace vast::cg
+{
+    operation attr_visitor_proxy::visit_decl_attrs(operation op, const clang_decl *decl, scope_context &scope) {
+        if (!decl->hasAttrs()) {
+            return {};
+        }
+
+        mlir_attr_list attrs = op->getAttrs();
+
+        auto filtered_attrs = decl->getAttrs() | std::ranges::views::filter([&] (auto attr) {
+            return !util::is_one_of< excluded_attr_list >(attr);
+        });
+
+        for (auto attr : filtered_attrs) {
+            auto visited = head.visit(attr, scope);
+            auto is_unsup = mlir::isa< unsup::UnsupportedDialect >(visited.getDialect());
+            auto key = is_unsup ? attr->getSpelling() : visited.getAbstractAttribute().getName();
+
+            if (auto prev = attrs.getNamed(key)) {
+                VAST_CHECK(visited == prev.value().getValue(),
+                    "Conflicting redefinition of attribute {0}", key
+                );
+            }
+
+            attrs.set(key, visited);
+        }
+
+        op->setAttrs(attrs);
+
+        return op;
+    }
+
+
+    operation attr_visitor_proxy::visit(const clang_decl *decl, scope_context &scope) {
+        if (auto op = next->visit(decl, scope)) {
+            return visit_decl_attrs(op, decl, scope);
+        }
+
+        return {};
+    }
+
+} // namespace vast::cg
