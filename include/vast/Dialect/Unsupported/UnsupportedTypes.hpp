af86b778001b057af785ac77e99219976dc0559d
xlauko <xlauko@mail.muni.cz>
2024-06-14 11:45:32 +0200

    unsup: Make unsupported types to implement data layout interface.

--- include/vast/Dialect/Unsupported/UnsupportedTypes.hpp
+++ include/vast/Dialect/Unsupported/UnsupportedTypes.hpp
@@ -2,7 +2,14 @@
 
 #pragma once
 
+#include "vast/Util/Warnings.hpp"
+
+VAST_RELAX_WARNINGS
+#include <mlir/Interfaces/DataLayoutInterfaces.h>
+VAST_UNRELAX_WARNINGS
+
 #include "vast/Dialect/Unsupported/UnsupportedDialect.hpp"
+#include "vast/Interfaces/DefaultDataLayoutTypeInterface.hpp"
 
 #define GET_TYPEDEF_CLASSES
 #include "vast/Dialect/Unsupported/UnsupportedTypes.h.inc"
