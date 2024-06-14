2fa4f664e7ff207bbd6eae7162ed6cffeafdb144
Robert Konicar <robert.konicar@trailofbits.com>
2024-06-14 12:46:43 +0200

    test: Expand complex type test.

--- test/vast/Dialect/HighLevel/complex-d.c
+++ test/vast/Dialect/HighLevel/complex-d.c
@@ -7,10 +7,19 @@ void fun(void) {
     int complex x_i = 0;
     int complex y_i = 0;
     int z_i = 0;
+    int b;
 // CHECK: hl.assign.add {{.*}} : !hl.complex<!hl.int>, !hl.lvalue<!hl.complex<!hl.int>> -> !hl.complex<!hl.int>
     x_i += y_i;
 // CHECK: hl.assign.add {{.*}} : !hl.complex<!hl.int>, !hl.lvalue<!hl.complex<!hl.int>> -> !hl.complex<!hl.int>
     x_i += z_i;
+// CHECK: hl.cmp eq {{.*}} : !hl.complex<!hl.int>, !hl.complex<!hl.int> -> !hl.int
+    b = x_i == y_i;
+// CHECK: hl.cmp eq {{.*}} : !hl.complex<!hl.int>, !hl.complex<!hl.int> -> !hl.int
+    b = x_i == z_i;
+// CHECK: hl.cmp eq {{.*}} : !hl.complex<!hl.int>, !hl.complex<!hl.int> -> !hl.int
+    b = z_i == x_i;
+
+
     unsigned int complex x_u = 0;
     unsigned int complex y_u = 0;
     unsigned int z_u = 0;
@@ -19,6 +28,13 @@ void fun(void) {
 // CHECK: hl.assign.add {{.*}} : !hl.complex<!hl.int< unsigned >>, !hl.lvalue<!hl.complex<!hl.int< unsigned >>> -> !hl.complex<!hl.int< unsigned >>
 
     x_u += z_u;
+// CHECK: hl.cmp eq {{.*}} : !hl.complex<!hl.int< unsigned >>, !hl.complex<!hl.int< unsigned >> -> !hl.int
+    b = x_u == y_u;
+// CHECK: hl.cmp eq {{.*}} : !hl.complex<!hl.int< unsigned >>, !hl.complex<!hl.int< unsigned >> -> !hl.int
+    b = x_u == z_u;
+// CHECK: hl.cmp eq {{.*}} : !hl.complex<!hl.int< unsigned >>, !hl.complex<!hl.int< unsigned >> -> !hl.int
+    b = z_u == x_u;
+
     float complex x_f = 0;
     float complex y_f = 0;
     float z_f = 0;
@@ -26,4 +42,10 @@ void fun(void) {
     x_f += y_f;
 // CHECK: hl.assign.fadd {{.*}} : !hl.float, !hl.lvalue<!hl.complex<!hl.float>> -> !hl.complex<!hl.float>
     x_f += z_f;
+// CHECK: hl.fcmp oeq {{.*}} : !hl.complex<!hl.float>, !hl.complex<!hl.float> -> !hl.int
+    b = x_f == y_f;
+// CHECK: hl.fcmp oeq {{.*}} : !hl.complex<!hl.float>, !hl.float -> !hl.int
+    b = x_f == z_f;
+// CHECK: hl.fcmp oeq {{.*}} : !hl.float, !hl.complex<!hl.float> -> !hl.int
+    b = z_f == x_f;
 }
