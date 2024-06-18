fb93899257cd04dc03faa8c3e8649bb58386be9e
Lukas Korencik <lukas.korencik@trailofbits.com>
2024-06-18 22:07:47 +0200

    tower: Gut original `tower` struct to leave space for new impl.

--- lib/vast/Tower/Tower.cpp
+++ lib/vast/Tower/Tower.cpp
@@ -3,20 +3,6 @@
 #include "vast/Tower/Tower.hpp"
 
 namespace vast::tw {
-    auto default_loc_rewriter_t::insert(mlir::Operation *op) -> void {
-        auto ctx = op->getContext();
-        auto ol  = mlir::OpaqueLoc::get< mlir::Operation  *>(op, ctx);
-        op->setLoc(mlir::FusedLoc::get({ op->getLoc() }, ol, ctx));
-    }
 
-    auto default_loc_rewriter_t::remove(mlir::Operation *op) -> void {
-        auto fl = mlir::cast< mlir::FusedLoc >(op->getLoc());
-        op->setLoc(fl.getLocations().front());
-    }
 
-    auto default_loc_rewriter_t::prev(mlir::Operation *op) -> mlir::Operation * {
-        auto fl = mlir::cast< mlir::FusedLoc >(op->getLoc());
-        auto ol = mlir::cast< mlir::OpaqueLoc >(fl.getMetadata());
-        return mlir::OpaqueLoc::getUnderlyingLocation< mlir::Operation * >(ol);
-    }
 } // namespace vast::tw
