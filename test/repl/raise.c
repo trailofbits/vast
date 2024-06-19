9e2e41b8affd1a57470572e0ce5e89111c926053
Lukas Korencik <lukas.korencik@trailofbits.com>
2024-06-19 15:01:31 +0200

    test:repl: Enable `vast-repl` tests.

--- test/repl/raise.c
+++ test/repl/raise.c
@@ -1,5 +1,4 @@
 // RUN: printf "load %s\n raise vast-hl-to-ll-cf\n show module\n exit" | %vast-repl | %file-check %s
 // CHECK: ll.return %0 : !hl.int
-// REQUIRES: repl
 
 int main(void) { return 0; }
