c5a1932289285f88acbe7f2c88d94a84939d5a53
Lukas Korencik <lukas.korencik@trailofbits.com>
2024-06-19 14:38:06 +0200

    cc: Reflect changes in frontend APIs.

--- tools/vast-front/compiler_invocation.cpp
+++ tools/vast-front/compiler_invocation.cpp
@@ -17,28 +17,31 @@ VAST_UNRELAX_WARNINGS
 
 namespace vast::cc
 {
-    frontend_action_ptr create_frontend_action(const vast_args &vargs) {
+    frontend_action_ptr create_frontend_action(const vast_args &vargs, mcontext_t &mctx) {
         if (opt::emit_only_mlir(vargs)) {
-            return std::make_unique< vast::cc::emit_mlir_action >(vargs);
+            return std::make_unique< vast::cc::emit_mlir_action >(vargs, mctx);
         }
 
         if (vargs.has_option(opt::emit_llvm)) {
-            return std::make_unique< vast::cc::emit_llvm_action >(vargs);
+            return std::make_unique< vast::cc::emit_llvm_action >(vargs, mctx);
         }
 
         if (vargs.has_option(opt::emit_asm)) {
-            return std::make_unique< vast::cc::emit_assembly_action >(vargs);
+            return std::make_unique< vast::cc::emit_assembly_action >(vargs, mctx);
         }
 
         if (vargs.has_option(opt::emit_obj)) {
-            return std::make_unique< vast::cc::emit_obj_action >(vargs);
+            return std::make_unique< vast::cc::emit_obj_action >(vargs, mctx);
         }
 
         return nullptr;
     }
 
-    frontend_action_ptr create_frontend_action(compiler_instance &ci, const vast_args &vargs) {
-        if (auto action = create_frontend_action(vargs)) {
+    frontend_action_ptr create_frontend_action(
+        compiler_instance &ci, const vast_args &vargs,
+        mcontext_t &mctx
+    ) {
+        if (auto action = create_frontend_action(vargs, mctx)) {
             return action;
         }
 
@@ -48,9 +51,9 @@ namespace vast::cc
 
         switch (act) {
             case ASTDump:  return std::make_unique< clang::ASTDumpAction >();
-            case EmitAssembly: return std::make_unique< vast::cc::emit_assembly_action >(vargs);
-            case EmitLLVM: return std::make_unique< vast::cc::emit_llvm_action >(vargs);
-            case EmitObj: return std::make_unique< vast::cc::emit_obj_action >(vargs);
+            case EmitAssembly: return std::make_unique< vast::cc::emit_assembly_action >(vargs, mctx);
+            case EmitLLVM: return std::make_unique< vast::cc::emit_llvm_action >(vargs, mctx);
+            case EmitObj: return std::make_unique< vast::cc::emit_obj_action >(vargs, mctx);
             default: VAST_UNIMPLEMENTED_MSG("unsupported frontend action");
         }
 
@@ -88,8 +91,10 @@ namespace vast::cc
         if (ci->getDiagnostics().hasErrorOccurred())
             return false;
 
+        // TODO: This is probably an internal leakage for this use case.
+        auto mctx = mk_mcontext();
         // Create and execute the frontend action.
-        auto action = create_frontend_action(*ci, vargs);
+        auto action = create_frontend_action(*ci, vargs, *mctx);
         if (!action)
             return false;
 
