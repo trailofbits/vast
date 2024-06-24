cc7e0597c14d0fd1ef23ec594b57fcd61d25a529
xlauko <xlauko@mail.muni.cz>
2024-06-24 21:49:44 +0200

    interfaces: Add SymbolRefInterface.

---
+++ lib/vast/Interfaces/SymbolRefInterface.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2024, Trail of Bits, Inc.
+
+#include "vast/Interfaces/SymbolRefInterface.hpp"
+
+//===----------------------------------------------------------------------===//
+// Symbol Ref Interfaces
+//===----------------------------------------------------------------------===//
+
+/// Include the generated symbol interfaces.
+#include "vast/Interfaces/SymbolRefInterface.cpp.inc"
