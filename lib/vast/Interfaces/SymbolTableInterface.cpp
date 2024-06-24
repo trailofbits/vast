624e1af25586ea22b84cf8f5a5e97b70b110d47f
xlauko <xlauko@mail.muni.cz>
2024-06-24 22:30:34 +0200

    treewide: Update missing core::module uses.

---
+++ lib/vast/Interfaces/SymbolTableInterface.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2024, Trail of Bits, Inc.
+
+#include "vast/Interfaces/SymbolTableInterface.hpp"
+
+//===----------------------------------------------------------------------===//
+// Symbol Interfaces
+//===----------------------------------------------------------------------===//
+
+/// Include the generated symbol interfaces.
+#include "vast/Interfaces/SymbolTableInterface.cpp.inc"
