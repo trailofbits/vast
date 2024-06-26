1fdbc08d3bd65643338a953474989c353b49926c
Robert Konicar <robert.konicar@trailofbits.com>
2024-06-26 12:01:59 +0200

    test: Add test for function declaration that is not a prototype.

---
+++ test/vast/Dialect/HighLevel/func-not-prototype-a.c
@@ -0,0 +1,17 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %file-check %s
+// RUN: %vast-front -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
+
+// CHECK: hl.func @foo external ([[ARG1:%[a-z0-9]+]]: !hl.lvalue<!hl.int>)
+int foo();
+int foo(int x) {
+// CHECK: hl.ref [[ARG1]]
+    if (x != 0) {
+        return 1;
+    }
+    return 2;
+}
+
+int main() {
+// CHECK: hl.call @foo({{%[0-9]+}}) : (!hl.int) -> !hl.int
+    foo(3);
+}
