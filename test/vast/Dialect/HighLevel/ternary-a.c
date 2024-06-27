bbeaed4c58f670bff34b438269e67198e6ec762f
Robert Konicar <robert.konicar@trailofbits.com>
2024-06-27 17:51:20 +0200

    test: Fixup hl condop type position.

--- test/vast/Dialect/HighLevel/ternary-a.c
+++ test/vast/Dialect/HighLevel/ternary-a.c
@@ -4,37 +4,37 @@ typedef int INT;
 typedef INT INT2;
 
 int fun1(int arg1, double arg2) {
-    // CHECK: hl.cond {
+    // CHECK: hl.cond : !hl.double {
     // CHECK: hl.cond.yield [[X:%[0-9]+]] : !hl.int
     // CHECK: } ? {
     // CHECK: hl.value.yield [[X:%[0-9]+]] : !hl.double
     // CHECK: } : {
     // CHECK: hl.value.yield [[X:%[0-9]+]] : !hl.double
-    // CHECK: } : !hl.double
+    // CHECK: }
     int res = arg1 ? arg2 : arg1;
     return res;
 }
 
 INT2 fun2(INT2 arg1, INT arg2) {
-    // CHECK: hl.cond {
+    // CHECK: hl.cond : !hl.elaborated<!hl.typedef<"INT">> {
     // CHECK: hl.cond.yield [[X:%[0-9]+]] : !hl.elaborated<!hl.typedef<"INT2">>
     // CHECK: } ? {
     // CHECK: hl.value.yield [[X:%[0-9]+]] : !hl.elaborated<!hl.typedef<"INT">>
     // CHECK: } : {
     // CHECK: hl.value.yield [[X:%[0-9]+]] : !hl.elaborated<!hl.typedef<"INT2">>
-    // CHECK: } : !hl.elaborated<!hl.typedef<"INT">>
+    // CHECK: }
     INT res = arg1 ? arg2 : arg1;
     return res;
 }
 
 void* fun3(INT2 arg1, INT arg2) {
-    // CHECK: hl.cond {
+    // CHECK: hl.cond : !hl.ptr<!hl.elaborated<!hl.typedef<"INT">>> {
     // CHECK: hl.cond.yield [[X:%[0-9]+]] : !hl.elaborated<!hl.typedef<"INT2">>
     // CHECK: } ? {
     // CHECK: hl.value.yield [[X:%[0-9]+]] : !hl.ptr<!hl.elaborated<!hl.typedef<"INT">>>
     // CHECK: } : {
     // CHECK: hl.value.yield [[X:%[0-9]+]] : !hl.ptr<!hl.elaborated<!hl.typedef<"INT2">>>
-    // CHECK: } : !hl.ptr<!hl.elaborated<!hl.typedef<"INT">>>
+    // CHECK: }
     void* res = arg1 ? &arg2 : &arg1;
     return res;
 }
