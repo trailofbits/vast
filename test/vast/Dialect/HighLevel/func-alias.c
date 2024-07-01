b1dbe448cb9c90582286f130d679099945a649cc
Robert Konicar <robert.konicar@trailofbits.com>
2024-07-01 13:44:59 +0200

    test: Generate alias attributed function.

---
+++ test/vast/Dialect/HighLevel/func-alias.c
@@ -0,0 +1,8 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %file-check %s
+// RUN: %vast-front -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
+
+// CHECK: hl.func @bar external
+void bar(int a) {}
+
+// CHECK: hl.func @foo external {{.*}} attributes {hl.alias = #hl.alias<"bar">}
+void foo(void) __attribute__((__alias__("bar")));
