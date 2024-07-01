f321000eccc3f8a80a3c739f88dfdea30b46d96d
Robert Konicar <robert.konicar@trailofbits.com>
2024-07-01 14:35:30 +0200

    test: Fix function type in function aliasing test.

--- test/vast/Dialect/HighLevel/func-alias.c
+++ test/vast/Dialect/HighLevel/func-alias.c
@@ -2,7 +2,7 @@
 // RUN: %vast-front -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
 
 // CHECK: hl.func @bar external
-void bar(int a) {}
+void bar(void) {}
 
 // CHECK: hl.func @foo external {{.*}} attributes {hl.alias = #hl.alias<"bar">}
 void foo(void) __attribute__((__alias__("bar")));
