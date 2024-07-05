5405b194bffd07e70fe79cb6ae958c32343adba9
xlauko <xlauko@mail.muni.cz>
2024-07-05 10:44:13 +0200

    test: Fix function alias test for darwin.

--- test/vast/Dialect/HighLevel/func-alias.c
+++ test/vast/Dialect/HighLevel/func-alias.c
@@ -1,5 +1,5 @@
-// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %file-check %s
-// RUN: %vast-front -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
+// RUN: %vast-cc1 -triple x86_64-unknown-linux-gnu -vast-emit-mlir=hl %s -o - | %file-check %s
+// RUN: %vast-cc1 -triple x86_64-unknown-linux-gnu -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
 
 // CHECK: hl.func @bar external
 void bar(void) {}
