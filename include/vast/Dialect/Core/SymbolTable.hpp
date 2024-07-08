5e8c7568ace05ae83a545ed007397a480a1e2640
xlauko <xlauko@mail.muni.cz>
2024-07-08 20:07:19 +0200

    core: Setup symbol table initialization.

--- include/vast/Dialect/Core/SymbolTable.hpp
+++ include/vast/Dialect/Core/SymbolTable.hpp
@@ -17,10 +17,16 @@ VAST_UNRELAX_WARNINGS
 #include "vast/Interfaces/SymbolInterface.hpp"
 
 #include <gap/coro/generator.hpp>
+#include <gap/coro/recursive_generator.hpp>
+#include <gap/mlir/views.hpp>
 
 namespace vast::core {
 
-    using string_attr     = mlir::StringAttr;
+    namespace gmw = gap::mlir::views;
+    namespace rns = std::ranges;
+    namespace vws = std::ranges::views;
+
+    using string_attr = mlir::StringAttr;
 
     struct symbol_table;
 
@@ -30,24 +36,88 @@ namespace vast::core {
     template< symbol_op_interface symbol_kind >
     [[nodiscard]] std::optional< symbol_table > get_next_effective_symbol_table_for(operation from);
 
+    template< util::flat_list symbols_list >
+    bool can_hold_symbol_kind(symbol_kind kind) {
+        if constexpr ( symbols_list::empty ) {
+            return false;
+        } else if (is_symbol_kind< typename symbols_list::head >(kind)) {
+            return true;
+        } else {
+            return can_hold_symbol_kind< typename symbols_list::tail >(kind);
+        }
+    }
+
+    template< util::list_of_lists symbols_lists >
+    bool can_hold_symbol_kind(symbol_kind kind) {
+        return can_hold_symbol_kind< util::flatten< symbols_lists > >(kind);
+    }
+
+    template< util::flat_list symbols_list >
+    bool can_hold_operation(operation op) {
+        if constexpr ( symbols_list::empty ) {
+            return false;
+        } else if (mlir::isa< typename symbols_list::head >(op)) {
+            return true;
+        } else {
+            return can_hold_operation< typename symbols_list::tail >(op);
+        }
+    }
+
+    template< util::list_of_lists symbols_lists >
+    bool can_hold_operation(operation op) {
+        return can_hold_operation< util::flatten< symbols_lists > >(op);
+    }
+
+    namespace detail {
+        template< util::flat_list symbols_list >
+        void symbol_kinds(auto &result) {
+            if constexpr ( symbols_list::empty ) {
+                return;
+            } else {
+                result.push_back(get_symbol_kind< typename symbols_list::head >);
+                symbol_kinds< typename symbols_list::tail >(result);
+            }
+        }
+
+        template< util::list_of_lists symbols_lists >
+        void symbol_kinds(auto &result) {
+            symbol_kinds< util::flatten< symbols_lists > >(result);
+        }
+    } // namespace detail
+
+    template< util::list_of_lists symbols_lists >
+    llvm::SmallVector< symbol_kind > symbol_kinds() {
+        constexpr auto size = util::flatten< symbols_lists >::size;
+        llvm::SmallVector< symbol_kind, size > result;
+        detail::symbol_kinds< symbols_lists >(result);
+        return result;
+    }
+
+    static inline auto immediate_nested_symbols(operation table) {
+        return gmw::operations(table) | gmw::filter_cast< symbol_base >;
+    }
+
+    gap::recursive_generator< operation > nested_symbols_unrecognized_by_nested_symbol_tables(operation root);
+
+
     struct symbol_table
     {
-        explicit symbol_table(operation symbol_table_op);
+        using single_symbol_kind_table = llvm::DenseMap< string_ref, operation >;
+
+        template< util::list_of_lists symbols_lists >
+        explicit symbol_table(std::in_place_type_t< symbols_lists >, operation symbol_table_op)
+            : symbol_table_op(symbol_table_op)
+        {
+            using recognized_symbols_list = util::flatten< symbols_lists >;
+            symbol_tables.reserve( recognized_symbols_list::size );
+            setup_symbol_tables< recognized_symbols_list >();
+            insert_nested_symbols< recognized_symbols_list >(symbol_table_op);
+        }
 
         [[nodiscard]] operation lookup(string_ref symbol) const;
         [[nodiscard]] operation lookup(string_attr symbol) const;
 
-        operation symbol_table_op;
-    };
-
-    template< typename recognized_symbols_list >
-    struct constrained_symbol_table : symbol_table {};
-
-
-    // Rename to table that categoriezes symbols by kind
-    struct grouped_symbol_table
-    {
-        explicit grouped_symbol_table(operation symbol_table_op);
+        [[nodiscard]] operation lookup(operation from, string_ref symbol);
 
         template< symbol_op_interface symbol_kind >
         [[nodiscard]] static operation lookup(operation from, string_attr symbol);
@@ -58,18 +128,68 @@ namespace vast::core {
         template< symbol_op_interface symbol_kind >
         [[nodiscard]] operation lookup(string_attr symbol) const;
 
-        template< symbol_op_interface symbol_kind >
-        [[nodiscard]] operation table() const;
+        template< util::flat_list symbols_list >
+        void try_insert(operation op) {
+            if constexpr ( symbols_list::empty ) {
+                return;
+            } else if (mlir::isa< typename symbols_list::head >(op)) {
+                return insert(get_symbol_kind< typename symbols_list::head >, op);
+            } else {
+                return try_insert< typename symbols_list::tail >(op);
+            }
+        }
+
+        template< util::list_of_lists symbol_lists >
+        void try_insert(operation op) {
+            try_insert< util::flatten< symbol_lists > >(op);
+        }
+
+        bool can_hold_symbol_kind(symbol_kind kind) const {
+            return symbol_tables.contains(kind);
+        }
 
         operation symbol_table_op;
 
-      private:
+      protected:
 
-        using constrained_symbol_table = llvm::DenseMap< string_ref, operation >;
+        template< util::flat_list symbols_list >
+        void setup_symbol_tables() {
+            if constexpr ( symbols_list::empty ) {
+                return;
+            } else {
+                // create an empty table for recognized symbol kind
+                symbol_tables.try_emplace(
+                    get_symbol_kind< typename symbols_list::head >,
+                    single_symbol_kind_table{}
+                );
+
+                // continue with the rest of the recognized symbols
+                setup_symbol_tables< typename symbols_list::tail >();
+            }
+        }
+
+        template< util::flat_list symbols_list >
+        void insert_nested_symbols(operation op) {
+            // insert all recognized imediate symbols into the symbol tables
+            for (auto symbol : immediate_nested_symbols(op)) {
+                try_insert< symbols_list >(symbol);
+            }
+
+            // insert all recognized symbols from nested scopes
+            // not recognized by the symbol table of nested scope
+            for (auto symbol : nested_symbols_unrecognized_by_nested_symbol_tables(op)) {
+                try_insert< symbols_list >(symbol);
+            }
+        }
+
+        void insert(symbol_kind kind, operation op);
+
+        llvm::DenseMap< symbol_kind, single_symbol_kind_table > symbol_tables;
     };
 
+
     template< symbol_op_interface symbol_kind >
-    operation grouped_symbol_table::lookup(operation from, string_ref symbol) {
+    operation symbol_table::lookup(operation from, string_ref symbol) {
         auto table = get_effective_symbol_table_for< symbol_kind >(from);
         VAST_CHECK(table, "No effective symbol table found.");
 
@@ -83,12 +203,12 @@ namespace vast::core {
     }
 
     template< symbol_op_interface symbol_kind >
-    operation grouped_symbol_table::lookup(operation from, string_attr symbol) {
+    operation symbol_table::lookup(operation from, string_attr symbol) {
         return lookup< symbol_kind >(from, symbol.getValue());
     }
 
     template< symbol_op_interface symbol_kind >
-    operation grouped_symbol_table::lookup(string_attr symbol) const {
+    operation symbol_table::lookup(string_attr symbol) const {
         return lookup< symbol_kind >(symbol_table_op, symbol);
     }
 
--- lib/vast/Dialect/Core/SymbolTable.cpp
+++ lib/vast/Dialect/Core/SymbolTable.cpp
@@ -2,27 +2,55 @@
 
 #include "vast/Dialect/Core/SymbolTable.hpp"
 
+#include <llvm/ADT/SetOperations.h>
+
 #include "vast/Interfaces/SymbolInterface.hpp"
 #include "vast/Interfaces/SymbolTableInterface.hpp"
 
 namespace vast::core {
 
-    symbol_table::symbol_table(operation symbol_table_op)
-        : symbol_table_op(symbol_table_op)
-    {
-        VAST_ASSERT(symbol_table_op->hasTrait< ShadowingSymbolTable >());
-        // for (auto &region : symbol_table_op->getRegions()) {
-        //     for (auto &block : region) {
-        //         for (auto &op : block) {
-        //             if (auto symbol = mlir::dyn_cast< core::symbol_base >(op)) {
-        //                 auto symbol_name = symbol.getSymbolName();
-        //                 // auto symbol_id = symbol.getSymbolInterfaceID();
-        //                 // split by symbol kinds here
-        //                 // symbol_tables[symbol_id][symbol_name] = op;
-        //             }
-        //         }
-        //     }
-        // }
+    // checks if the symbol table can hold all symbol kinds of other_table
+    bool subsumes(symbol_table_op_interface table, symbol_table_op_interface other_table) {
+        auto self = table.symbol_kinds();
+        return llvm::set_is_subset(
+            other_table.symbol_kinds(), llvm::SmallDenseSet< symbol_kind >(self.begin(), self.end())
+        );
+    }
+
+    auto immediate_unrecognized_nested_symbols(operation table) {
+        auto self = mlir::cast< symbol_table_op_interface >(table);
+
+        auto not_recognized = [&](operation op) {
+            return !self.can_hold_operation(op);
+        };
+
+        return immediate_nested_symbols(table) | vws::filter(not_recognized);
+    }
+
+    auto nested_symbol_tables_with_unrecognized_symbols(operation root) {
+        auto root_table = mlir::cast< symbol_table_op_interface >(root);
+
+        auto can_have_unrecognized_symbols = [&](symbol_table_op_interface st) {
+            return !subsumes(st, root_table);
+        };
+
+        return gmw::operations(root)
+            | gmw::filter_cast< symbol_table_op_interface >
+            | vws::filter(can_have_unrecognized_symbols);
+    }
+
+    gap::recursive_generator< operation > nested_symbols_unrecognized_by_nested_symbol_tables(operation root) {
+        VAST_ASSERT(mlir::isa< symbol_table_op_interface >(root));
+
+        for (auto st : nested_symbol_tables_with_unrecognized_symbols(root)) {
+            for (auto op : immediate_unrecognized_nested_symbols(st)) {
+                co_yield op;
+            }
+
+            for ([[maybe_unused]] auto op : nested_symbols_unrecognized_by_nested_symbol_tables(st)) {
+                VAST_UNIMPLEMENTED_MSG("recursively yield the nested symbol tables of the nested symbol table");
+            }
+        }
     }
 
     operation symbol_table::lookup(string_ref symbol) const {
@@ -34,6 +62,14 @@ namespace vast::core {
         return lookup(symbol.getValue());
     }
 
+    void symbol_table::insert(symbol_kind kind, operation op) {
+        VAST_ASSERT(can_hold_symbol_kind(kind));
+        auto symbol_name = mlir::cast< symbol_base >(op).getSymbolName();
+        VAST_ASSERT(symbol_tables.contains(kind));
+        VAST_ASSERT(!symbol_tables[kind].contains(symbol_name));
+        symbol_tables[kind][symbol_name] = op;
+    }
+
     string_ref symbol_attr_name() {
         return mlir::SymbolTable::getSymbolAttrName();
     }
@@ -42,9 +78,9 @@ namespace vast::core {
         operation from, symbol_kind kind
     ) {
         while (from) {
-            if (auto table = mlir::dyn_cast_if_present< VastSymbolTable >(from)) {
-                if (table.holds_symbol_kind(kind)) {
-                    return symbol_table(from);
+            if (auto table = mlir::dyn_cast_if_present< SymbolTableOpInterface >(from)) {
+                if (table.can_hold_symbol_kind(kind)) {
+                    return table.materialize();
                 }
             }
             from = from->getParentOp();
@@ -52,6 +88,5 @@ namespace vast::core {
 
         return std::nullopt;
     }
-
 
 } // namespace vast::core
