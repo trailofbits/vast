731c03200bd50eb866fbe132e705f6fc4b587027
Vladimir Uhlik <vladimir.uhlik@trailofbits.com>
2024-07-09 13:10:31 +0200

    interfaces:ast: Add corresponding .cpp files for Clang AST interfaces.

---
+++ lib/vast/Interfaces/ASTContextInterface.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2024-present, Trail of Bits, Inc.
+
+#include "vast/Interfaces/ASTContextInterface.hpp"
+
+//===----------------------------------------------------------------------===//
+// ASTNodes Op Interface
+//===----------------------------------------------------------------------===//
+
+/// Include the generated interface.
+#include "vast/Interfaces/ASTContextInterface.cpp.inc"
+
