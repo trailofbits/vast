50eeb4134ce51282f9fd35e11f4f6d0d609e168d
Vladimir Uhlik <vladimir.uhlik@trailofbits.com>
2024-07-09 13:19:54 +0200

    interfaces:ast: Provide template implementations for .hpp and .td files.

---
+++ include/vast/Interfaces/ASTTypeInterface.hpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2024-present, Trail of Bits, Inc.
+
+#pragma once
+
+#include "vast/Util/Warnings.hpp"
+
+VAST_RELAX_WARNINGS
+#include <mlir/IR/BuiltinOps.h>
+#include <mlir/IR/BuiltinTypes.h>
+#include <mlir/IR/Dialect.h>
+#include <mlir/IR/OperationSupport.h>
+VAST_RELAX_WARNINGS
+
+
+/// Include the generated interface declarations.
+#include "vast/Interfaces/ASTTypeInterface.h.inc"
