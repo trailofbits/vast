f73c4a72becc609d7d9a0ab92d8b22b0633329ad
Lukas Korencik <lukas.korencik@trailofbits.com>
2024-07-10 13:17:22 +0200

    test:repl: Update test to reflect changes in driver.

--- test/repl/raise.c
+++ test/repl/raise.c
@@ -1,4 +1,4 @@
 // RUN: printf "load %s\n raise vast-hl-to-ll-cf\n show module\n exit" | %vast-repl | %file-check %s
-// CHECK: ll.return %0 : !hl.int
+// CHECK: hl.return %0 : !hl.int
 
 int main(void) { return 0; }
