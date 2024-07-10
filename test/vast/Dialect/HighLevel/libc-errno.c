0fbc260a3ac0eca13aa539a43e06841d28a68fe3
Robert Konicar <robert.konicar@trailofbits.com>
2024-07-10 14:41:13 +0200

    test: Add libc errno test.

---
+++ test/vast/Dialect/HighLevel/libc-errno.c
@@ -0,0 +1,6 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %file-check %s
+// RUN: %vast-front -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
+
+// CHECK: hl.func @__errno_location
+
+#include <errno.h>
