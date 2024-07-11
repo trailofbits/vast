0dde8073e9661e8dbaa2184e5391e180f2c4f924
Lukas Korencik <lukas.korencik@trailofbits.com>
2024-07-11 15:58:45 +0200

    test:repl: Disable `raise.c` due to unresolved problem (Issue #668).

--- test/repl/raise.c
+++ test/repl/raise.c
@@ -1,4 +1,6 @@
 // RUN: printf "load %s\n raise vast-hl-to-ll-cf\n show module\n exit" | %vast-repl | %file-check %s
 // CHECK: hl.return %0 : !hl.int
 
+// REQUIRES: clone-memory-leak
+
 int main(void) { return 0; }
