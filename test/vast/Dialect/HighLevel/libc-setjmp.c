0891f79399870abe324d8396c2154ad7ae70e2cf
Robert Konicar <robert.konicar@trailofbits.com>
2024-07-11 12:10:21 +0200

    test: Add libc setjmp test.

---
+++ test/vast/Dialect/HighLevel/libc-setjmp.c
@@ -0,0 +1,7 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %file-check %s
+// RUN: %vast-front -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
+
+// CHECK-DAG: hl.func @setjmp
+// CHECK-DAG: hl.func @longjmp
+// CHECK-DAG: hl.typedef "jmp_buf"
+#include <setjmp.h>
