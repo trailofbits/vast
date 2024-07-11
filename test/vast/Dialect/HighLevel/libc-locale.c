82576ba08be14952cd8df139dd49015a9659adeb
Robert Konicar <robert.konicar@trailofbits.com>
2024-07-11 12:10:08 +0200

    test: Add libc locale test.

---
+++ test/vast/Dialect/HighLevel/libc-locale.c
@@ -0,0 +1,7 @@
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %file-check %s
+// RUN: %vast-front -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
+
+// CHECK-DAG: hl.func @setlocale
+// CHECK-DAG: hl.func @localeconv
+// CHECK-DAG: hl.struct "lconv"
+#include <locale.h>
