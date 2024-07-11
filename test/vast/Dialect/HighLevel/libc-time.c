19f67dbcc0db33d2665a10d35e6c624a6b9ac4f6
Robert Konicar <robert.konicar@trailofbits.com>
2024-07-11 16:43:02 +0200

    test: Add libc time test.

---
+++ test/vast/Dialect/HighLevel/libc-time.c
@@ -0,0 +1,26 @@
+// RUN: %vast-front -vast-emit-mlir=hl -std=c23 %s -o - | %file-check %s
+// RUN: %vast-front -vast-emit-mlir=hl %s -o - | %file-check %s --check-prefix PRE-C23
+// RUN: %vast-front -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
+
+// CHECK-DAG: hl.func @difftime
+// CHECK-DAG: hl.func @time
+// CHECK-DAG: hl.func @clock
+// CHECK-DAG: hl.func @timespec_get
+// CHECK-DAG: hl.func @timespec_getres
+
+// CHECK-DAG: hl.func @asctime
+// CHECK-DAG: hl.func @ctime
+// CHECK-DAG: hl.func @strftime
+// CHECK-DAG: hl.func @gmtime
+// CHECK-DAG: hl.func @localtime
+
+// CHECK-DAG: hl.func @mktime
+
+// CHECK-DAG: hl.struct "tm"
+// CHECK-DAG: hl.struct "timespec"
+// CHECK-DAG: hl.typedef "time_t"
+// CHECK-DAG: hl.typedef "clock_t"
+
+// PRE-C23-NOT: hl.fun &timespec_getres
+
+#include <time.h>
