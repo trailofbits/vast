1b18fb65ab0ec4975e63d6f1d0566a83a5bd8269
Robert Konicar <robert.konicar@trailofbits.com>
2024-07-22 14:03:59 +0200

    test: Fix path to vast-front in miamcu test toggle.

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -162,7 +162,8 @@ uchar_test = subprocess.run(["cc", "-std=c2x", "-x", "c", "-", "-o", "/dev/null"
 if uchar_test.returncode == 0:
     config.available_features.add("ucharc23")
 
-miamcu_test = subprocess.run(["vast-front", "-vast-emit-mlir=hl", "-x", "c", "-", "-o", "/dev/null", "-m32", "-miamcu"],
+vast_front = os.path.join(config.vast_tools_dir,'vast-front', config.vast_build_type, 'vast-front')
+miamcu_test = subprocess.run([vast_front, "-vast-emit-mlir=hl", "-x", "c", "-", "-o", "/dev/null", "-m32", "-miamcu"],
                              input=b'int main() {}')
 if miamcu_test.returncode == 0:
     config.available_features.add("miamcu")
