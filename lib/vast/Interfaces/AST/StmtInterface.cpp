49828d677464f76ecc1b5887ee12610f07c9e57d
Vladimir Uhlik <vladimir.uhlik@trailofbits.com>
2024-07-29 14:03:21 +0200

    interfaces:ast: Add new interfaces.

---
+++ lib/vast/Interfaces/AST/StmtInterface.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2024-present, Trail of Bits, Inc.
+
+#include "vast/Interfaces/AST/StmtInterface.hpp"
+
+//===----------------------------------------------------------------------===//
+// ASTNodes Op Interface
+//===----------------------------------------------------------------------===//
+
+/// Include the generated interface.
+#include "vast/Interfaces/AST/StmtInterface.cpp.inc"
