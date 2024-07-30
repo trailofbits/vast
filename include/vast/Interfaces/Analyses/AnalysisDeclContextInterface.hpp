11eeccf6d167010a3da5c0409ef98aa2b1948e23
Vladimir Uhlik <vladimir.uhlik@trailofbits.com>
2024-07-30 17:55:14 +0200

    interfaces:analyses: Add AnalysisDeclContextInterface.

---
+++ include/vast/Interfaces/Analyses/AnalysisDeclContextInterface.hpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2024-present, Trail of Bits, Inc.
+
+#pragma once
+
+#include "vast/Util/Warnings.hpp"
+
+VAST_RELAX_WARNINGS
+#include <mlir/IR/BuiltinOps.h>
+#include <mlir/IR/BuiltinTypes.h>
+#include <mlir/IR/Dialect.h>
+#include <mlir/IR/OperationSupport.h>
+VAST_RELAX_WARNINGS
+
+#include "vast/Interfaces/AST/DeclInterface.hpp"
+
+/// Include the generated interface declarations.
+#include "vast/Interfaces/Analyses/AnalysisDeclContextInterface.h.inc"
