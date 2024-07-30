97e996650a0cb93ed95eba98322daf0c202a9dcd
Vladimir Uhlik <vladimir.uhlik@trailofbits.com>
2024-07-30 17:55:14 +0200

    interfaces:analyses: Add AnalysisDeclContextInterface.

---
+++ lib/vast/Interfaces/Analyses/AnalysisDeclContextInterface.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2024-present, Trail of Bits, Inc.
+
+#include "vast/Interfaces/Analyses/AnalysisDeclContextInterface.hpp"
+
+//===----------------------------------------------------------------------===//
+// ASTNodes Op Interface
+//===----------------------------------------------------------------------===//
+
+/// Include the generated interface.
+#include "vast/Interfaces/Analyses/AnalysisDeclContextInterface.cpp.inc"
