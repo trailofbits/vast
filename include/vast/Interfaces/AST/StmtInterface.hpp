4fb8b4104a704679c23d8f4e1155deff4f900e47
Vladimir Uhlik <vladimir.uhlik@trailofbits.com>
2024-07-30 16:18:59 +0200

    interfaces:ast: Add new interfaces and use :: before return types.

--- include/vast/Interfaces/AST/StmtInterface.hpp
+++ include/vast/Interfaces/AST/StmtInterface.hpp
@@ -11,5 +11,7 @@ VAST_RELAX_WARNINGS
 #include <mlir/IR/OperationSupport.h>
 VAST_RELAX_WARNINGS
 
+#include "clang/AST/Stmt.h"
+
 /// Include the generated interface declarations.
 #include "vast/Interfaces/AST/StmtInterface.h.inc"
