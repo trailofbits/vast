a4d339f596bad2a385e2668fa72d1854c84aa1bd
Robert Konicar <robert.konicar@trailofbits.com>
2024-07-31 12:14:18 +0200

    test: Expand linakge test.

--- test/vast/Dialect/HighLevel/linkage-a.c
+++ test/vast/Dialect/HighLevel/linkage-a.c
@@ -3,3 +3,29 @@
 
 // CHECK: hl.func @foo internal
 static void foo(void) {}
+
+// CHECK: hl.func @bar available_externally
+inline void bar(void) {}
+
+// CHECK: hl.func @baz available_externally
+inline void baz(void);
+inline void baz(void) {}
+
+// CHECK: hl.func @qux available_externally
+inline void qux(void) {}
+inline void qux(void);
+
+// CHECK: hl.func @quux external
+inline void quux(void) {}
+void quux(void);
+
+// CHECK: hl.func @corge external
+inline extern void corge(void);
+inline void corge(void) {}
+
+// CHECK: hl.func @grault external
+inline void grault(void) {}
+inline extern void grault(void);
+
+// CHECK: hl.func @graply internal
+static inline void graply(void) {}
