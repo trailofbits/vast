be17044708cf71ba46a8b6cd3a4d8bd8faef1ea5
Vladimir Uhlik <vladimir.uhlik@trailofbits.com>
2024-08-08 17:15:42 +0200

    analyses: Add new iterators.

--- lib/vast/Analyses/Iterators.cpp
+++ lib/vast/Analyses/Iterators.cpp
@@ -1,8 +1,10 @@
 #include "vast/Analyses/Iterators.hpp"
+#include "vast/Interfaces/AST/DeclInterface.hpp"
+#include "vast/Interfaces/CFG/CFGInterface.hpp"
 
 namespace vast::analyses {
 
-    ast::DeclInterface decl_interface_iterator::operator*() const { return *Current; }
+    ast::DeclInterface &decl_interface_iterator::operator*() const { return *Current; }
     ast::DeclInterface *decl_interface_iterator::operator->() const { return Current; }
 
     decl_interface_iterator &decl_interface_iterator::operator++() {
@@ -16,16 +18,29 @@ namespace vast::analyses {
         return tmp;
     }
 
-    mlir::Operation *get_current_op() const {
+    mlir::Operation *decl_interface_iterator::get_current_op() const {
         return Current->getOperation();
     }
 
-    friend bool decl_interface_iterator::operator==(decl_interface_iterator x, decl_interface_iterator y) {
+    bool operator==(const decl_interface_iterator &x, const decl_interface_iterator &y) {
         return x.Current == y.Current;
     }
 
-    friend bool decl_interface_iterator::operator!=(decl_interface_iterator x, decl_interface_iterator y) {
+    bool operator!=(const decl_interface_iterator &x, const decl_interface_iterator &y) {
         return x.Current != y.Current;
     }
 } // namespace vast::analyses
 
+namespace vast::cfg {
+
+    AdjacentBlock::AdjacentBlock(cfg::CFGBlockInterface *B, bool isReachable)
+        : ReachableBlock(isReachable ? B->getOperation() : nullptr),
+          UnreachableBlock(!isReachable ? B->getOperation() : nullptr,
+                           B && isReachable ? Kind::AB_Normal : Kind::AB_Unreachable) {}
+
+    AdjacentBlock::AdjacentBlock(cfg::CFGBlockInterface *B, cfg::CFGBlockInterface *AlternateBlock)
+        : ReachableBlock(B->getOperation()),
+          UnreachableBlock(B == AlternateBlock ? nullptr : AlternateBlock->getOperation(),
+                           B == AlternateBlock ? Kind::AB_Alternate : Kind::AB_Normal) {}
+
+} // namespace vast::cfg
