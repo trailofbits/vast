50df128b94d6134fdf0e9bc2ae9479e838e500c0
Vladimir Uhlik <vladimir.uhlik@trailofbits.com>
2024-08-08 17:15:42 +0200

    analyses: Add new iterators.

--- include/vast/Analyses/Iterators.hpp
+++ include/vast/Analyses/Iterators.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
+#include "vast/Util/Warnings.hpp"
+
+VAST_RELAX_WARNINGS
+#include <clang/Analysis/Support/BumpVector.h>
+#include <llvm/ADT/iterator.h>
+#include <llvm/ADT/iterator_range.h>
+#include <mlir/IR/Operation.h>
+VAST_RELAX_WARNINGS
+
+#include <iterator>
+
 namespace vast::ast {
 
     class DeclInterface;
+    class ExprInterface;
+    class StmtInterface;
 }
 
 namespace vast::analyses {
@@ -14,12 +27,13 @@ namespace vast::analyses {
         decl_interface_iterator() = default;
         explicit decl_interface_iterator(ast::DeclInterface *C) : Current(C) {}
 
-        ast::DeclInterface &operator*() const;
+        ast::DeclInterface &operator*() const; 
         ast::DeclInterface *operator->() const;
         decl_interface_iterator &operator++();
         decl_interface_iterator operator++(int);
-        friend bool operator==(decl_interface_iterator, decl_interface_iterator);
-        friend bool operator!=(decl_interface_iterator, decl_interface_iterator);
+
+        friend bool operator==(const decl_interface_iterator &, const decl_interface_iterator &);
+        friend bool operator!=(const decl_interface_iterator &, const decl_interface_iterator &);
         mlir::Operation *get_current_op() const;
     };
 
@@ -65,4 +79,91 @@ namespace vast::analyses {
             return x.Current != y.Current;
         }
     };
+
+    template< typename T, typename TPtr = T *, typename StmtPtr = ast::StmtInterface * >
+    struct CastIterator
+        : llvm::iterator_adaptor_base< CastIterator< T, TPtr, StmtPtr >, StmtPtr *,
+                                       std::random_access_iterator_tag, T > {
+        using Base = typename CastIterator::iterator_adaptor_base;
+
+        CastIterator() : Base(nullptr) {}
+        CastIterator(StmtPtr *I) : Base(I) {}
+
+        typename Base::value_type operator*() const {
+            return cast_or_null< T >((*this->I)->getOperation());
+        }
+    };
+
+    using ExprInterfaceIterator = CastIterator< ast::ExprInterface >;
+    using call_expr_arg_iterator = ExprInterfaceIterator;
 } // namespace vast::analyses
+
+namespace vast::cfg {
+
+    class CFGBlockInterface;
+    class CFGElementInterface;
+} 
+
+namespace vast::cfg {
+
+    class AdjacentBlock {
+        enum Kind {
+            AB_Normal,
+            AB_Unreachable,
+            AB_Alternate
+        };
+
+        mlir::Operation *ReachableBlock;
+        llvm::PointerIntPair< mlir::Operation *, 2 > UnreachableBlock;
+
+    public:
+        /// Construct an AdjacentBlock with a possibly unreachable block.
+        AdjacentBlock(cfg::CFGBlockInterface *B, bool isReachable);
+
+        /// Construct an AdjacentBlock with a reachable block and an alternate
+        /// unreachable block.
+        AdjacentBlock(cfg::CFGBlockInterface *B, cfg::CFGBlockInterface *AlternateBlock);
+
+        /// Get the reachable block, if one exists.
+        mlir::Operation *getReachableBlock() const {
+            return ReachableBlock;
+        }
+
+        /// Get the potentially unreachable block.
+        mlir::Operation *getPossiblyUnreachableBlock() const {
+            return UnreachableBlock.getPointer();
+        }
+
+        /// Provide an implicit conversion to cfg::CFGBlockInterface so that
+        /// AdjacentBlock can be substituted for cfg::CFGBlockInterface.
+        /*
+        operator mlir::Operation*() const {
+            return getReachableBlock();
+        }
+        */
+
+        mlir::Operation *operator*() const {
+            return getReachableBlock();
+        }
+
+        mlir::Operation *operator->() const {
+            return getReachableBlock();
+        }
+
+        bool isReachable() const {
+            Kind K = (Kind) UnreachableBlock.getInt();
+            return K == Kind::AB_Normal || K == Kind::AB_Alternate;
+        }
+    };
+
+    using AdjacentBlocks = clang::BumpVector< AdjacentBlock >;
+    using pred_iterator = AdjacentBlocks::iterator;
+
+    using CFGBlockListTy = clang::BumpVector< CFGBlockInterface >;
+    using CFGIterator = CFGBlockListTy::iterator;
+
+    using succ_iterator = AdjacentBlocks::iterator;
+    using succ_range = llvm::iterator_range< succ_iterator >;
+
+    using CFGBlockIterator = std::reverse_iterator< clang::BumpVector< CFGElementInterface >::iterator >;
+} // namespace vast::cfg
