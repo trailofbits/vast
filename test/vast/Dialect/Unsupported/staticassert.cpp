656590515f31942931872a1c40946e0e2d54adec
Robert Konicar <robert.konicar@trailofbits.com>
2024-08-30 13:50:52 +0200

    test: Remove static assert test from unsupported dialect.

--- test/vast/Dialect/Unsupported/staticassert.cpp
+++
@@ -1,5 +0,0 @@
-// RUN: %vast-front %s -vast-emit-mlir=hl -o - | %file-check %s
-// RUN: %vast-front %s -vast-emit-mlir=hl -o - > %t && %vast-opt %t | diff -B %t -
-
-// CHECK: unsup.decl "StaticAssert"
-static_assert(1, "Test static assert 1");
