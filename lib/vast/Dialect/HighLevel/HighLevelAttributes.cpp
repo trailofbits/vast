dd97d1bd82d16468df85038de6a8923ac29e307d
Robert Konicar <robert.konicar@trailofbits.com>
2024-09-03 17:15:31 +0200

    util: Rename field parsing utils.

--- lib/vast/Dialect/HighLevel/HighLevelAttributes.cpp
+++ lib/vast/Dialect/HighLevel/HighLevelAttributes.cpp
@@ -2,7 +2,7 @@
 
 #include "vast/Dialect/HighLevel/HighLevelAttributes.hpp"
 #include "vast/Dialect/HighLevel/HighLevelTypes.hpp"
-#include "vast/Util/FieldAdditions.hpp"
+#include "vast/Util/FieldParser.hpp"
 #include "vast/Util/Warnings.hpp"
 #include <gap/core/overloads.hpp>
 
