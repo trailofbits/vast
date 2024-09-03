d4b3c1968b56c0320d7606c4cbd114ea81486921
Robert Konicar <robert.konicar@trailofbits.com>
2024-09-03 16:20:53 +0200

    hl: Create a base tablegen class for conditional operator-like ops.

--- lib/vast/Dialect/HighLevel/HighLevelOps.cpp
+++ lib/vast/Dialect/HighLevel/HighLevelOps.cpp
@@ -569,18 +569,6 @@ namespace vast::hl
         build_region(bld, st, else_builder);
     }
 
-    void CondOp::build(
-        Builder &bld, State &st, Type type,
-        builder_callback_ref cond,
-        builder_callback_ref then_builder,
-        builder_callback_ref else_builder
-    ) {
-        InsertionGuard guard(bld);
-        build_region(bld, st, cond);
-        build_region(bld, st, then_builder);
-        build_region(bld, st, else_builder);
-        st.addTypes(type);
-    }
     void BinaryCondOp::build(
         Builder &bld, State &st, Type type,
         builder_callback_ref common_builder,
