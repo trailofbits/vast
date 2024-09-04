f7ae2aeb777e6e72cf89ba2656f5f0812efe26d2
Robert Konicar <robert.konicar@trailofbits.com>
2024-09-04 15:43:51 +0200

    hl: Add tablegen base class for TypeTraitExpr.

--- include/vast/Dialect/HighLevel/HighLevelOps.hpp
+++ include/vast/Dialect/HighLevel/HighLevelOps.hpp
@@ -20,6 +20,7 @@ VAST_UNRELAX_WARNINGS
 
 #include "vast/Dialect/Core/Interfaces/SymbolInterface.hpp"
 #include "vast/Interfaces/AggregateTypeDefinitionInterface.hpp"
+#include "vast/Interfaces/TypeTraitExprInterface.hpp"
 #include "vast/Interfaces/AST/DeclInterface.hpp"
 
 
