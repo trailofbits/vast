08a10c02ede7b847026a784ec7741cb86bd1be3e
Robert Konicar <robert.konicar@trailofbits.com>
2024-09-04 15:42:13 +0200

    interface: Add TypeTraitExpr interface.

---
+++ lib/vast/Interfaces/TypeTraitExprInterface.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2024-present, Trail of Bits, Inc.
+
+#include "vast/Interfaces/TypeTraitExprInterface.hpp"
+
+//===----------------------------------------------------------------------===//
+// Alias Type Interface
+//===----------------------------------------------------------------------===//
+
+/// Include the generated interface.
+#include "vast/Interfaces/TypeTraitExprInterface.cpp.inc"
