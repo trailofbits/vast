432ed1fe6374f052ea059d8ee9d81826c5e539f8
Robert Konicar <robert.konicar@trailofbits.com>
2024-09-04 11:15:30 +0200

    test: Update test for choose expr.

--- test/vast/Dialect/HighLevel/choose-expr-a.c
+++ test/vast/Dialect/HighLevel/choose-expr-a.c
@@ -3,9 +3,11 @@
 
 void fn() {
     int x, y;
-    // CHECK: hl.choose_expr : !hl.lvalue<!hl.int> {
+    // CHECK: hl.choose_expr cond false : !hl.lvalue<!hl.int> {
     // CHECK: hl.cond.yield {{%.*}} : !hl.int
     // CHECK: hl.value.yield {{%.*}} : !hl.lvalue<!hl.int>
     // CHECK: hl.value.yield {{%.*}} : !hl.lvalue<!hl.int>
     int z = __builtin_choose_expr(0, x, y);
+    // CHECK: hl.choose_expr cond true : !hl.lvalue<!hl.int> {
+    int w = __builtin_choose_expr(1, x, y);
 }
