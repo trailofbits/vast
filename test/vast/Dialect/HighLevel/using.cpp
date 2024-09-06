fc29b8cd083a9db4a50a8ef05d8365b1f21d36e5
xlauko <xlauko@mail.muni.cz>
2024-09-06 21:42:44 +0200

    hl: Let TypeAlias use SymbolNameAttr.

--- test/vast/Dialect/HighLevel/using.cpp
+++ test/vast/Dialect/HighLevel/using.cpp
@@ -1,6 +1,6 @@
 // RUN: %vast-front %s -vast-emit-mlir=hl -o - | %file-check %s
 // RUN: %vast-front %s -vast-emit-mlir=hl -o - > %t && %vast-opt %t | diff -B %t -
 
-// CHECK: hl.alias "Int" : !hl.int
+// CHECK: hl.alias @Int : !hl.int
 using Int = int;
 Int variable = 0;
