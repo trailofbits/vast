6f951379d3662e6b57b445122cc0c0f3a08c7ca0
Robert Konicar <robert.konicar@trailofbits.com>
2024-09-10 10:12:28 +0200

    test: Enable quirk tests that are no longer unsupported.

--- test/vast/Dialect/HighLevel/quirks-b.c
+++ test/vast/Dialect/HighLevel/quirks-b.c
@@ -1,6 +1,5 @@
 // RUN: %vast-cc1 -vast-emit-mlir=hl %s -o - | %file-check %s
 // RUN: %vast-cc1 -vast-emit-mlir=hl %s -o %t && %vast-opt %t | diff -B %t -
-// REQUIRES: compund-literal
 
 // adapted from https://gist.github.com/fay59/5ccbe684e6e56a7df8815c3486568f01
 
@@ -10,9 +9,15 @@ struct foo {
 
 void baz() {
     // compound literal: https://en.cppreference.com/w/c/language/compound_literal
+    // CHECK: hl.compound_literal : !hl.lvalue<!hl.elaborated<!hl.record<"foo">>>
     (struct foo){};
 
     // these are actually lvalues
+    // CHECK: hl.compound_literal : !hl.lvalue<!hl.elaborated<!hl.record<"foo">>>
+    // CHECK: [[V1:%[0-9]+]] = hl.member {{.*}} at @bar : !hl.lvalue<!hl.elaborated<!hl.record<"foo">>> -> !hl.lvalue<!hl.int>
+    // CHECK: hl.assign {{.*}} to [[V1]] : !hl.int, !hl.lvalue<!hl.int> -> !hl.int
     ((struct foo){}).bar = 4;
+    // CHECK: [[V2:%[0-9]+]] = hl.compound_literal : !hl.lvalue<!hl.elaborated<!hl.record<"foo">>>
+    // CHECK: hl.addressof [[V2]]
     &(struct foo){};
 }
