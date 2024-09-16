08c7d031ea7160262df1025896c13dedd4ffc18f
xlauko <xlauko@mail.muni.cz>
2024-09-16 14:23:54 +0200

    test: Update ll.var to ll.cell tests.

--- test/vast/Conversion/vars-a.c
+++ test/vast/Conversion/vars-a.c
@@ -1,12 +1,12 @@
-// RUN: %vast-cc1 -vast-emit-mlir=hl %s -o - | %vast-opt --vast-hl-dce --vast-hl-lower-types --vast-hl-to-ll-vars | %file-check %s
+// RUN: %vast-cc1 -vast-emit-mlir-after=vast-refs-to-ssa %s -o - | %file-check %s
 
 int main()
 {
-    // CHECK: [[V0:%[0-9]+]] = ll.uninitialized_var : !hl.lvalue<si32>
+    // CHECK: [[V0:%[0-9]+]] = ll.cell @a : !hl.lvalue<si32>
     int a;
 
-    // CHECK: [[V1:%[0-9]+]] = ll.uninitialized_var : !hl.lvalue<si32>
+    // CHECK: [[V1:%[0-9]+]] = ll.cell @b : !hl.lvalue<si32>
     // CHECK: [[V2:%[0-9]+]] = hl.const #core.integer<1> : si32
-    // CHECK: [[V3:%[0-9]+]] = ll.initialize [[V1]], [[V2]] : (!hl.lvalue<si32>, si32) -> !hl.lvalue<si32>
+    // CHECK: [[V3:%[0-9]+]] = ll.cell_init [[V1]], [[V2]] : (!hl.lvalue<si32>, si32) -> !hl.lvalue<si32>
     int b = 1;
 }
