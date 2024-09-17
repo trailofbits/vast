48e6057acefbe907f3b31a37dc05f23f079fb863
xlauko <xlauko@mail.muni.cz>
2024-09-17 15:18:05 +0200

    test: Fix filecheck output parameters.

--- test/vast/Conversion/signs.c
+++ test/vast/Conversion/signs.c
@@ -1,13 +1,13 @@
-// RUN: %vast-front -vast-emit-mlir=llvm %s -o - %s | %file-check %s
+// RUN: %vast-front -vast-emit-mlir=llvm %s -o - | %file-check %s
 
 int main() {
     int a = 5;
-    // CHECK: {{.* = llvm.load.*}}
-    // CHECK-NEXT: {{.* = llvm.mlir.constant.*}}
-    // CHECK-NEXT: {{.* = llvm.sub.*}}
-    // CHECK-NEXT: {{.*llvm.store.*}}
-    // CHECK: {{.* = llvm.load.*}}
-    // CHECK-NEXT: {{.*llvm.store.*}}
+    // CHECK: llvm.load
+    // CHECK-NEXT: llvm.mlir.constant
+    // CHECK-NEXT: llvm.sub
+    // CHECK-NEXT: llvm.store
+    // CHECK: llvm.load
+    // CHECK-NEXT: llvm.store
     int b = -a;
     int c = +a;
     return 0;
