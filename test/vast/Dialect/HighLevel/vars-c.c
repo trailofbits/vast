2073ac18c7070c234d433547ce7872e636eabe8d
Robert Konicar <robert.konicar@trailofbits.com>
2024-09-20 09:46:25 +0200

    treewide: Obliterate graph regions.

--- test/vast/Dialect/HighLevel/vars-c.c
+++ test/vast/Dialect/HighLevel/vars-c.c
@@ -1,38 +1,38 @@
 // RUN: %vast-front -vast-emit-mlir=hl -o - %s | %file-check %s
-// REQUIRES: non-graph-regions
+// REQUIRES: loop-init-section
 
 #include <stdlib.h>
 
 int main() {
-    // CHECK: hl.var @y : !hl.lvalue<!hl.ptr<!hl.int>>
+    // CHECK: hl.var [[Y:@y]] : !hl.lvalue<!hl.ptr<!hl.int>>
     // CHECK: hl.sizeof.expr
     // CHECK: hl.ref [[Y]]
     int *y = malloc(sizeof(*y));
     if(1) {
-    // CHECK: hl.var @x : !hl.lvalue<!hl.ptr<!hl.int>>
+    // CHECK: hl.var [[X:@x]] : !hl.lvalue<!hl.ptr<!hl.int>>
     // CHECK: hl.sizeof.expr
     // CHECK: hl.ref [[X]]
         int *x = malloc(sizeof(*x));
     }
     while(1) {
-    // CHECK: hl.var @x : !hl.lvalue<!hl.ptr<!hl.int>>
+    // CHECK: hl.var [[X:@x]] : !hl.lvalue<!hl.ptr<!hl.int>>
     // CHECK: hl.sizeof.expr
     // CHECK: hl.ref [[X]]
         int *x = malloc(sizeof(*x));
     }
     do{
-    // CHECK: hl.var @x : !hl.lvalue<!hl.ptr<!hl.int>>
+    // CHECK: hl.var [[X:@x]] : !hl.lvalue<!hl.ptr<!hl.int>>
     // CHECK: hl.sizeof.expr
     // CHECK: hl.ref [[X]]
         int *x = malloc(sizeof(*x));
     }while(0);
     // CHECK: core.scope
-    // CHECK-NEXT: hl.var @x : !hl.lvalue<!hl.ptr<!hl.int>>
+    // CHECK-NEXT: hl.var [[X:@x]] : !hl.lvalue<!hl.ptr<!hl.int>>
     // CHECK: hl.sizeof.expr
     // CHECK: hl.ref [[X]]
     for(int *x = malloc(sizeof(*x)); *x<100; *x++) {
     // CHECK: do
-    // CHECK-NEXT: hl.var @f : !hl.lvalue<!hl.ptr<!hl.int>>
+    // CHECK-NEXT: hl.var [[F:@f]] : !hl.lvalue<!hl.ptr<!hl.int>>
     // CHECK: hl.sizeof.expr
     // CHECK: hl.ref [[F]]
         int *f = malloc(sizeof(*f));
@@ -43,7 +43,7 @@ int main() {
     if (*y) {
         case 2: case 3: case 5: case 7: {
     // CHECK: hl.case
-    // CHECK: hl.var @x : !hl.lvalue<!hl.ptr<!hl.int>>
+    // CHECK: hl.var [[X:@x]] : !hl.lvalue<!hl.ptr<!hl.int>>
     // CHECK: hl.sizeof.expr
     // CHECK: hl.ref [[X]]
             int *x = malloc(sizeof(*x));
@@ -57,13 +57,13 @@ int main() {
         case 1:
         default: {
     // CHECK: hl.default
-    // CHECK: hl.var @z : !hl.lvalue<!hl.ptr<!hl.int>>
+    // CHECK: hl.var [[Z:@z]] : !hl.lvalue<!hl.ptr<!hl.int>>
     // CHECK: hl.sizeof.expr
     // CHECK: hl.ref [[Z]]
             int *z = malloc(sizeof(*z));
         }
     }
-    // CHECK: hl.var @g : !hl.lvalue<!hl.ptr<!hl.int>>
+    // CHECK: hl.var [[G:@g]] : !hl.lvalue<!hl.ptr<!hl.int>>
     // CHECK: hl.cond
     // CHECK: ?
     // CHECK: hl.sizeof.expr
@@ -73,7 +73,7 @@ int main() {
     int *g = y ? malloc(sizeof(*g)) : y ;
     ({
     // CHECK: hl.stmt.expr
-    // CHECK: hl.var @z : !hl.lvalue<!hl.ptr<!hl.int>>
+    // CHECK: hl.var [[Z:@z]] : !hl.lvalue<!hl.ptr<!hl.int>>
     // CHECK: hl.sizeof.expr
     // CHECK: hl.ref [[Z]]
             int *z = malloc(sizeof(*z));
