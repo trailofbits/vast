66e5138dd89db361dd3a4f95b8d11ea6c48cb6ea
Robert Konicar <robert.konicar@trailofbits.com>
2024-09-23 17:35:21 +0200

    core: Add DeclStorageInterface.

---
+++ lib/vast/Dialect/Core/Interfaces/DeclStorageInterface.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2024-present, Trail of Bits, Inc.
+
+#include "vast/Dialect/Core/Interfaces/DeclStorageInterface.hpp"
+
+//===----------------------------------------------------------------------===//
+// Decl Storage Interfaces
+//===----------------------------------------------------------------------===//
+
+/// Include the generated symbol interfaces.
+#include "vast/Dialect/Core/Interfaces/DeclStorageInterface.cpp.inc"
