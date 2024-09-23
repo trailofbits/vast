e13814ea20dd97d186c44d963044496e4cce98ab
Robert Konicar <robert.konicar@trailofbits.com>
2024-09-23 17:35:21 +0200

    core: Add DeclStorageInterface.

---
+++ include/vast/Dialect/Core/Interfaces/DeclStorageInterface.hpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2024, Trail of Bits, Inc.
+
+#pragma once
+
+#include "vast/Util/Warnings.hpp"
+
+VAST_RELAX_WARNINGS
+#include <mlir/Interfaces/FunctionInterfaces.h>
+#include <mlir/IR/BuiltinOps.h>
+#include <mlir/IR/BuiltinTypes.h>
+#include <mlir/IR/Dialect.h>
+#include <mlir/IR/OperationSupport.h>
+VAST_RELAX_WARNINGS
+
+#include "vast/Dialect/Core/CoreAttributes.hpp"
+#include "vast/Dialect/Core/CoreOps.hpp"
+#include "vast/Dialect/Core/SymbolTable.hpp"
+
+#define GET_OP_FWD_DEFINES
+#include "vast/Dialect/HighLevel/HighLevel.h.inc"
+/// Include the generated interface declarations.
+#include "vast/Dialect/Core/Interfaces/DeclStorageInterface.h.inc"
