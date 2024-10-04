843fbb6023e7882e72e178fb2cc1c366191edfb3
Robert Konicar <robert.konicar@trailofbits.com>
2024-10-04 17:30:05 +0200

    test: Add test for external and weak linakge of global variables.

---
+++ test/vast/Dialect/HighLevel/glob-linkage-a.c
@@ -0,0 +1,16 @@
+// RUN: %vast-front %s -vast-emit-mlir=hl -o - | %file-check %s
+//
+// CHECK: hl.var @ew, <extern_weak>
+extern int __attribute__((weak)) ew;
+// CHECK: hl.var @wundef, <weak>
+int __attribute__((weak)) wundef;
+// CHECK: hl.var @wdef, <weak>
+int __attribute__((weak)) wdef = 5;
+// CHECK: hl.var @ewdef, <weak>
+extern int __attribute__((weak)) ewdef = 5;
+// CHECK: hl.var @edef, <external>
+extern int edef = 5;
+// CHECK: hl.var @undef, <common>
+int undef;
+// CHECK: hl.var @def, <external>
+int def = 5;
