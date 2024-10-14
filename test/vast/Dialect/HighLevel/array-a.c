283d2a082acc4f1b95962a4a6bcc85a5312a5b1e
Robert Konicar <robert.konicar@trailofbits.com>
2024-10-14 21:46:14 +0200

    test: Add const qualifier to vars where it belongs.

--- test/vast/Dialect/HighLevel/array-a.c
+++ test/vast/Dialect/HighLevel/array-a.c
@@ -4,16 +4,16 @@
 // CHECK: hl.var @ai, <external> : !hl.lvalue<!hl.array<10, !hl.int>>
 int ai[10];
 
-// CHECK: hl.var @aci, <external> : !hl.lvalue<!hl.array<5, !hl.int< const >>>
+// CHECK: hl.var @aci, <external> constant : !hl.lvalue<!hl.array<5, !hl.int< const >>>
 const int aci[5];
 
 // CHECK: hl.var @avi, <external> : !hl.lvalue<!hl.array<5, !hl.int< volatile >>>
 volatile int avi[5];
 
-// CHECK: hl.var @acvi, <external> : !hl.lvalue<!hl.array<5, !hl.int< const, volatile >>>
+// CHECK: hl.var @acvi, <external> constant : !hl.lvalue<!hl.array<5, !hl.int< const, volatile >>>
 const volatile int acvi[5];
 
-// CHECK: hl.var @acvui, <external> : !hl.lvalue<!hl.array<5, !hl.int< unsigned, const, volatile >>>
+// CHECK: hl.var @acvui, <external> constant : !hl.lvalue<!hl.array<5, !hl.int< unsigned, const, volatile >>>
 const volatile unsigned int acvui[5];
 
 // CHECK: hl.var @af, <external> : !hl.lvalue<!hl.array<10, !hl.float>>
