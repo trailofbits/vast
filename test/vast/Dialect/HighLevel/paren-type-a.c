4a5e2c9c4e5fd3c7532a24561d58bfa14253745c
Robert Konicar <robert.konicar@trailofbits.com>
2024-10-17 17:22:08 +0200

    test: Update ParenType test.

--- test/vast/Dialect/HighLevel/paren-type-a.c
+++ test/vast/Dialect/HighLevel/paren-type-a.c
@@ -3,8 +3,11 @@
 
 void fun() {
     int (a) = 0;
-    // CHECK: hl.add %{{[0-9]+}}, %{{[0-9]+}} : (!hl.int, !hl.int)
+    // CHECK: hl.add %{{[0-9]+}}, %{{[0-9]+}} : (!hl.paren<!hl.int>, !hl.int)
     a + 0;
+    float (b) = 0;
+    // CHECK: hl.fadd %{{[0-9]+}}, %{{[0-9]+}} : (!hl.paren<!hl.float>, !hl.float)
+    b + 0.0f;
 }
 
 enum FOO { BAR };
@@ -12,6 +15,6 @@ enum FOO { BAR };
 void fun2 () {
     int a[1]    = { 0 };
     enum FOO (b) = BAR;
-    // CHECK: hl.subscript %{{[0-9]+}} at [%{{[0-9]+}} : !hl.elaborated<!hl.enum<"FOO">>]
+    // CHECK: hl.subscript %{{[0-9]+}} at [%{{[0-9]+}} : !hl.paren<!hl.elaborated<!hl.enum<@FOO>>>]
     a[b];
 }
