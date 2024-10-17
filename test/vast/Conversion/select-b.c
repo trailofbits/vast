b06f02be808a24b791dec413ecc83cdcd860d82c
xlauko <xlauko@mail.muni.cz>
2024-10-17 15:05:38 +0200

    test: Update lazy op conversion expected output.

--- test/vast/Conversion/select-b.c
+++ test/vast/Conversion/select-b.c
@@ -10,12 +10,8 @@ int main(int argc, char** argv)
 {
     // I_LLVM: [[V6:%[0-9]+]] = core.lazy.op {
     // I_LLVM:   llvm.call @foo() : () -> ()
-    // I_LLVM:   [[V11:%[0-9]+]] = llvm.mlir.zero : !llvm.void
-    // I_LLVM:   hl.value.yield [[V11]] : !llvm.void
     // I_LLVM: } : !llvm.void
     // I_LLVM: [[V7:%[0-9]+]] = core.lazy.op {
-    // I_LLVM:   [[V12:%[0-9]+]] = llvm.mlir.zero : !llvm.void
-    // I_LLVM:   hl.value.yield [[V12]] : !llvm.void
     // I_LLVM: } : !llvm.void
     // I_LLVM: {{.*}} = core.select {{.*}}, [[V6]], [[V7]] : (i32, !llvm.void, !llvm.void) -> !llvm.void
 
