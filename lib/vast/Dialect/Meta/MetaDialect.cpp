1d4c1253ac01572a94f60ef2b61cd24c74e4b69c
xlauko <xlauko@mail.muni.cz>
2024-10-18 11:35:01 +0200

    meta: Update symbols walk.

--- lib/vast/Dialect/Meta/MetaDialect.cpp
+++ lib/vast/Dialect/Meta/MetaDialect.cpp
@@ -3,6 +3,8 @@
 #include "vast/Dialect/Meta/MetaDialect.hpp"
 #include "vast/Dialect/Meta/MetaAttributes.hpp"
 
+#include "vast/Dialect/Core/Interfaces/SymbolInterface.hpp"
+
 namespace vast::meta
 {
     void MetaDialect::initialize() {
@@ -17,17 +19,17 @@ namespace vast::meta
 
     static constexpr std::string_view identifier_name = "meta_identifier";
 
-    void add_identifier(mlir::Operation *op, identifier_t id) {
+    void add_identifier(operation op, identifier_t id) {
         auto ctx = op->getContext();
         auto attr = IdentifierAttr::get(ctx, id);
         op->setAttr(identifier_name, attr);
     }
 
-    void remove_identifier(mlir::Operation *op) {
+    void remove_identifier(operation op) {
         op->removeAttr(identifier_name);
     }
 
-    bool has_identifier(mlir::Operation *op, identifier_t id) {
+    bool has_identifier(operation op, identifier_t id) {
         if (auto attr = op->getAttr(identifier_name)) {
             return mlir::cast< IdentifierAttr >(attr).getValue() == id;
         }
@@ -35,19 +37,19 @@ namespace vast::meta
         return false;
     }
 
-    std::vector< mlir::Operation * > get_with_identifier(mlir::Operation *scope, identifier_t id) {
-        std::vector< mlir::Operation * > result;
-        util::symbols(scope, [&] (auto symbol) {
-            if (has_identifier(symbol, id)) {
-                result.push_back(symbol);
+    std::vector< operation  > get_with_identifier(operation scope, identifier_t id) {
+        std::vector< operation  > result;
+        core::symbols< core::symbol >(scope, [&](operation op) {
+            if (has_identifier(op, id)) {
+                result.push_back(op);
             }
         });
         return result;
     }
 
-    std::vector< mlir::Operation * > get_with_meta_location(mlir::Operation *scope, IdentifierAttr id) {
-        std::vector< mlir::Operation * > result;
-        scope->walk([&](mlir::Operation *op) {
+    std::vector< operation  > get_with_meta_location(operation scope, IdentifierAttr id) {
+        std::vector< operation  > result;
+        scope->walk([&](operation op) {
             if (auto loc = mlir::dyn_cast< mlir::FusedLoc >(op->getLoc())) {
                 if (id == loc.getMetadata()) {
                     result.push_back(op);
@@ -57,7 +59,7 @@ namespace vast::meta
         return result;
     }
 
-    std::vector< mlir::Operation * > get_with_meta_location(mlir::Operation *scope, identifier_t id) {
+    std::vector< operation  > get_with_meta_location(operation scope, identifier_t id) {
         auto ctx = scope->getContext();
         return get_with_meta_location(scope, IdentifierAttr::get(ctx, id));
     }
