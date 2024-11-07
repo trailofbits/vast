27919989e470447cc358fbb795bf0c8be38f293e
Robert Konicar <robert.konicar@trailofbits.com>
2024-11-07 10:23:07 +0100

    test: Move atomic expr test from unsup to hl.

--- test/vast/Dialect/HighLevel/atomicexpr.cpp
+++ test/vast/Dialect/HighLevel/atomicexpr.cpp
@@ -2,7 +2,7 @@
 // RUN: %vast-front %s -vast-emit-mlir=hl -o - > %t && %vast-opt %t | diff -B %t -
 
 int load(int* p) {
-    // CHECK: unsup.stmt "AtomicExpr"
+    // CHECK: hl.atomic_expr "__atomic_load_n" : !hl.int
     // CHECK: hl.ref @p
     // CHECK: hl.const #core.integer<5>
     int q = __atomic_load_n (p, __ATOMIC_SEQ_CST);
