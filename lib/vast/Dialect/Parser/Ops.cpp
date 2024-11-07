3faafc21c38fd34e7323259f0d7e62e9a03a5385
xlauko <xlauko@mail.muni.cz>
2024-11-07 14:00:11 +0100

    pr: Setup noparse folding.

--- lib/vast/Dialect/Parser/Ops.cpp
+++ lib/vast/Dialect/Parser/Ops.cpp
@@ -2,26 +2,23 @@
 
 #include "vast/Util/Warnings.hpp"
 
-VAST_RELAX_WARNINGS
-#include <mlir/IR/Builders.h>
-#include <mlir/IR/OpImplementation.h>
-#include <mlir/IR/OperationSupport.h>
-#include <mlir/IR/SymbolTable.h>
-
-#include <mlir/Interfaces/FunctionImplementation.h>
-#include <mlir/Interfaces/CallInterfaces.h>
-
-#include <mlir/Support/LLVM.h>
-#include <mlir/Support/LogicalResult.h>
-
-#include <llvm/Support/ErrorHandling.h>
-VAST_UNRELAX_WARNINGS
-
 #include "vast/Dialect/Parser/Dialect.hpp"
 #include "vast/Dialect/Parser/Ops.hpp"
 #include "vast/Dialect/Parser/Types.hpp"
+
+#include "vast/Util/Common.hpp"
 
 using namespace vast::pr;
 
 #define GET_OP_CLASSES
 #include "vast/Dialect/Parser/Parser.cpp.inc"
+
+namespace vast::pr {
+
+    using fold_result_t = ::llvm::SmallVectorImpl< ::mlir::OpFoldResult >;
+
+    logical_result NoParse::fold(FoldAdaptor adaptor, fold_result_t &results) {
+        return mlir::failure();
+    }
+
+} // namespace vast::pr
