b3999eeb4f03841bb1725deea91d4a5087ee8a9b
xlauko <xlauko@mail.muni.cz>
2024-11-20 10:38:56 +0100

    core: Add arithmetic binary op interface.

---
+++ lib/vast/Dialect/Core/Interfaces/OperationInterfaces.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2024-present, Trail of Bits, Inc.
+
+#include "vast/Dialect/Core/Interfaces/OperationInterfaces.hpp"
+
+//===----------------------------------------------------------------------===//
+// Operation Interfaces
+//===----------------------------------------------------------------------===//
+
+/// Include the generated operation interfaces.
+#include "vast/Dialect/Core/Interfaces/OperationInterfaces.cpp.inc"
