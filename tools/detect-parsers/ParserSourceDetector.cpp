ff7d5595c0a6d0ef11e624af31b0f39c6403c632
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2024-11-20 16:53:14 +0100

    pr: Add pass to emit SARIF for each parser source.

---
+++ tools/detect-parsers/ParserSourceDetector.cpp
@@ -0,0 +1,29 @@
+// Copyright (c) 2021-present, Trail of Bits, Inc.
+
+#include "SarifPasses.hpp"
+
+#include "vast/Dialect/Parser/Ops.hpp"
+#include "vast/Frontend/Sarif.hpp"
+
+namespace vast {
+    void ParserSourceDetector::runOnOperation() {
+        getOperation().walk([&](pr::Source op) {
+            gap::sarif::result result{
+                .ruleId{ "pr-source" },
+                .ruleIndex = 0,
+                .kind      = gap::sarif::kind::kInformational,
+                .level     = gap::sarif::level::kNote,
+                .message{
+                        .text{ { "Parser source detected" } },
+                        },
+                .locations{},
+            };
+            if (auto loc = cc::sarif::mk_location(op.getLoc());
+                loc.physicalLocation.has_value())
+            {
+                result.locations.push_back(std::move(loc));
+            }
+            results.push_back(result);
+        });
+    }
+} // namespace vast
--- tools/detect-parsers/main.cpp
+++ tools/detect-parsers/main.cpp
@@ -1,12 +1,18 @@
 // Copyright (c) 2021-present, Trail of Bits, Inc.
 
+#include <string>
+
 #include "vast/Util/Warnings.hpp"
 
 VAST_RELAX_WARNINGS
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/InitAllDialects.h"
+#include "mlir/Pass/PassOptions.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
+
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/raw_ostream.h>
 VAST_UNRELAX_WARNINGS
 
 #include "vast/Conversion/Parser/Passes.hpp"
@@ -14,14 +20,72 @@ VAST_UNRELAX_WARNINGS
 
 #include "vast/Dialect/Parser/Dialect.hpp"
 
-int main(int argc, char **argv)
-{
+#include "SarifPasses.hpp"
+
+namespace vast {
+    struct SarifWriter
+        : mlir::PassWrapper<
+              SarifWriter, mlir::OperationPass< mlir::ModuleOp > >
+    {
+        std::vector< gap::sarif::result > results;
+        std::string path;
+
+        SarifWriter(std::string path) : path(path) {}
+
+        void runOnOperation() override {}
+
+        ~SarifWriter() {
+            std::error_code ec;
+            llvm::raw_fd_ostream os(path, ec, llvm::sys::fs::OF_None);
+            if (ec) {
+                VAST_FATAL("Failed to open file for SARIF output: {}", ec.message());
+            }
+            gap::sarif::root root{
+                .runs{
+                      {
+                        {
+                            .tool{
+                                .driver{
+                                    .name{ "detect-parsers" },
+                                },
+                            },
+                            .results{ results },
+                        },
+                    }, },
+            };
+
+            nlohmann::json root_json = root;
+
+            os << root_json.dump(2);
+        }
+    };
+
+    struct SarifOptions : mlir::PassPipelineOptions< SarifOptions >
+    {
+        Option< std::string > out_path{ *this, "output",
+                                        llvm::cl::desc("Output SARIF file path.") };
+    };
+
+    void registerSarifPasses() {
+        mlir::PassPipelineRegistration< SarifOptions >(
+            "parser-source-to-sarif", "Dumps all pr.source locations to a SARIF file.",
+            [](mlir::OpPassManager &pm, const SarifOptions &opts) {
+                auto writer = std::make_unique< SarifWriter >(opts.out_path);
+                pm.addPass(std::make_unique< vast::ParserSourceDetector >(writer->results));
+                pm.addPass(std::move(writer));
+            }
+        );
+    }
+} // namespace vast
+
+int main(int argc, char **argv) {
     mlir::DialectRegistry registry;
     // register dialects
     vast::registerAllDialects(registry);
     mlir::registerAllDialects(registry);
 
     vast::registerParserConversionPasses();
+    vast::registerSarifPasses();
     registry.insert< vast::pr::ParserDialect >();
 
     return mlir::asMainReturnCode(
