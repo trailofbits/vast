58d855f76e84e31d5c0060aea457dbbee2e81176
xlauko <xlauko@mail.muni.cz>
2025-03-27 12:06:14 +0100

    test: Add parser conversion test to expr-a

--- test/parsers/expr-a.c
+++ test/parsers/expr-a.c
@@ -1,9 +1,12 @@
 // RUN: %vast-front -vast-emit-mlir=hl %s -o - | %file-check %s -check-prefix=HL
+// RUN: %vast-front -vast-show-locs -vast-loc-attrs -vast-emit-mlir=hl %s -o - | %vast-opt -vast-hl-to-lazy-regions -o %t.mlir
+// RUN: %vast-detect-parsers -vast-hl-to-parser -vast-parser-reconcile-casts -reconcile-unrealized-casts %t.mlir -o - | %file-check %s -check-prefix=PARSER
 
 #include <stdio.h>
 #include <ctype.h>
 
 // HL: hl.func @parse_number
+// PARSER: hl.func @parse_number
 int parse_number(const char **input) {
     int value = 0;
     while (isdigit(**input)) {
@@ -14,6 +17,7 @@ int parse_number(const char **input) {
 }
 
 // HL: hl.func @add_two_numbers
+// PARSER: hl.func @add_two_numbers
 int add_two_numbers(const char *input) {
     int num1 = parse_number(&input);
     while (isspace(*input)) input++; // Skip spaces
