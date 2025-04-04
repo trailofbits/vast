3c480e30d2261658405a65ce247eaf5a6343b241
xlauko <xlauko@mail.muni.cz>
2025-04-04 10:45:57 +0200

    pr: Setup parser refine pass

---
+++ lib/vast/Conversion/Parser/Refine.cpp
@@ -0,0 +1,71 @@
+#include "vast/Util/Warnings.hpp"
+
+#include "vast/Conversion/Parser/Passes.hpp"
+
+VAST_RELAX_WARNINGS
+#include <mlir/IR/PatternMatch.h>
+#include <mlir/Transforms/DialectConversion.h>
+#include <mlir/Transforms/GreedyPatternRewriteDriver.h>
+VAST_UNRELAX_WARNINGS
+
+#include "PassesDetails.hpp"
+#include "Utils.hpp"
+
+#include "vast/Conversion/Common/Mixins.hpp"
+#include "vast/Conversion/Common/Patterns.hpp"
+
+#include "vast/Dialect/Parser/Ops.hpp"
+#include "vast/Dialect/Parser/Types.hpp"
+
+namespace vast::conv {
+
+    namespace pattern {
+
+        struct DeadNoParseElimination : operation_conversion_pattern< pr::NoParse >
+        {
+            using op_t = pr::NoParse;
+            using base = operation_conversion_pattern< op_t >;
+            using base::base;
+
+            using adaptor_t = typename op_t::Adaptor;
+
+            logical_result matchAndRewrite(
+                op_t op, adaptor_t adaptor, conversion_rewriter &rewriter
+            ) const override {
+                if (op->getUsers().empty()) {
+                    rewriter.eraseOp(op);
+                    return mlir::success();
+                }
+
+                return mlir::failure();
+            }
+
+            static void legalize(base_conversion_config &cfg) {
+                cfg.target.addDynamicallyLegalOp< op_t >([](op_t op) {
+                    return !op->getUsers().empty();
+                });
+            }
+        };
+
+        using refines = util::type_list< DeadNoParseElimination >;
+
+    } // namespace pattern
+
+    struct ParserRefinePass : ConversionPassMixin< ParserRefinePass, ParserRefineBase >
+    {
+        using base = ConversionPassMixin< ParserRefinePass, ParserRefineBase >;
+
+        static conversion_target create_conversion_target(mcontext_t &mctx) {
+            return conversion_target(mctx);
+        }
+
+        static void populate_conversions(auto &cfg) {
+            base::populate_conversions< pattern::refines >(cfg);
+        }
+    };
+
+} // namespace vast::conv
+
+std::unique_ptr< mlir::Pass > vast::createParserRefinePass() {
+    return std::make_unique< vast::conv::ParserRefinePass >();
+}
