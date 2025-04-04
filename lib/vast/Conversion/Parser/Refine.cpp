f4536bc729f26842bce21c364f9990564e1eb9e2
xlauko <xlauko@mail.muni.cz>
2025-04-04 12:47:37 +0200

    pr: Implement NoParseFold refinement

--- lib/vast/Conversion/Parser/Refine.cpp
+++ lib/vast/Conversion/Parser/Refine.cpp
@@ -28,6 +28,114 @@ namespace vast::conv {
             using base::base;
         };
 
+        template< typename op_t >
+        struct NoParseFold : operation_conversion_pattern< op_t >
+        {
+            using base = operation_conversion_pattern< op_t >;
+            using base::base;
+
+            using adaptor_t = typename op_t::Adaptor;
+
+            logical_result matchAndRewrite(
+                op_t op, adaptor_t adaptor, conversion_rewriter &rewriter
+            ) const override {
+                if (op->getNumResults() == 0) {
+                    rewriter.eraseOp(op);
+                    return mlir::success();
+                } else {
+                    rewriter.replaceOpWithNewOp< pr::NoParse >(
+                        op, op->getResultTypes(), adaptor.getOperands()
+                    );
+                    return mlir::success();
+                }
+
+                return mlir::failure();
+            }
+
+            static bool is_nodata(mlir_type type) { return mlir::isa< pr::NoDataType >(type); }
+
+            static bool is_nodata(mlir_value value) { return is_nodata(value.getType()); }
+
+            static bool is_nodata(mlir::ValueRange values) {
+                for (auto value : values) {
+                    if (!is_nodata(value)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            static bool is_noparse_op(mlir::Operation &op) {
+                if (mlir::isa< pr::NoParse >(op)) {
+                    return true;
+                }
+
+                if (mlir::isa< hl::NullStmt >(op)) {
+                    return true;
+                }
+
+                if (mlir::isa< hl::BreakOp >(op)) {
+                    return true;
+                }
+
+                if (mlir::isa< hl::ContinueOp >(op)) {
+                    return true;
+                }
+
+                if (auto yield = mlir::dyn_cast< hl::CondYieldOp >(op)) {
+                    if (is_nodata(yield.getResult())) {
+                        return true;
+                    }
+                }
+
+                if (auto yield = mlir::dyn_cast< hl::ValueYieldOp >(op)) {
+                    if (is_nodata(yield.getResult())) {
+                        return true;
+                    }
+                }
+
+                if (auto ret = mlir::dyn_cast< hl::ReturnOp >(op)) {
+                    if (is_nodata(ret.getResult())) {
+                        return true;
+                    }
+                }
+
+                if (auto call = mlir::dyn_cast< hl::CallOp >(op)) {
+                    return is_nodata(call.getArgOperands()) && is_nodata(call.getResults());
+                }
+
+                return false;
+            }
+
+            static bool is_noparse_region(mlir::Region *region) {
+                if (region->empty()) {
+                    return true;
+                }
+
+                for (auto &block : *region) {
+                    for (auto &op : block) {
+                        if (!is_noparse_op(op)) {
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            }
+
+            static void legalize(base_conversion_config &cfg) {
+                cfg.target.addDynamicallyLegalOp< op_t >([](op_t op) {
+                    for (auto region : op.getRegions()) {
+                        if (!is_noparse_region(region)) {
+                            return true;
+                        }
+                    }
+
+                    return false;
+                });
+            }
+        };
+
         struct DeadNoParseElimination : operation_conversion_pattern< pr::NoParse >
         {
             using op_t = pr::NoParse;
@@ -56,6 +164,12 @@ namespace vast::conv {
 
         using refines = util::type_list<
             DeadNoParseElimination,
+            NoParseFold< hl::IfOp >,
+            NoParseFold< hl::WhileOp >,
+            NoParseFold< hl::ForOp >,
+            NoParseFold< hl::DoOp >,
+            NoParseFold< hl::ChooseExprOp >,
+            NoParseFold< hl::BinaryCondOp >,
             DefinitionElimination< hl::EnumDeclOp >,
             DefinitionElimination< hl::StructDeclOp >,
             DefinitionElimination< hl::UnionDeclOp >,
